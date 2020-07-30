bef0f337f041d494dc8575020da80167a4b90701
Troy Swanson <2troy2020@gmail.com>
2020-07-30 17:51:52 -0500

    logz

--- app/src/main/java/com/example/budgetvalue/App.kt
+++ app/src/main/java/com/example/budgetvalue/App.kt
@@ -3,8 +3,14 @@ package com.example.budgetvalue
 import android.app.Application
 import com.example.budgetvalue.dependency_injection.AppModule
 import com.example.budgetvalue.dependency_injection.DaggerAppComponent
+import com.example.tmcommonkotlin.logz
 
 class App : Application() {
+    override fun onCreate() {
+        logz("START")
+        super.onCreate()
+    }
+
     val appComponent by lazy {
         DaggerAppComponent.builder().appModule(AppModule(this)).build()
     }
