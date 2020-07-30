5c540c80e2e73564ff1f30df291e74acb60a0871
Troy Swanson <2troy2020@gmail.com>
2020-07-30 14:31:00 -0500

    top_menu`Import Transactions`halfway

--- app/src/main/java/com/example/budgetvalue/layers/z_ui/HostActivity.kt
+++ app/src/main/java/com/example/budgetvalue/layers/z_ui/HostActivity.kt
@@ -2,11 +2,17 @@ package com.example.budgetvalue.layers.z_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.budgetvalue.R
 
 class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu);
+        return true;
     }
 }
