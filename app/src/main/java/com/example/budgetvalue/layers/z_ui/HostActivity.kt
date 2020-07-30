25595b91d8e5ea2bd4db9572b2e708106f47a149
Troy Swanson <2troy2020@gmail.com>
2020-07-30 14:15:33 -0500

    renaming, restructuring

--- app/src/main/java/com/example/budgetvalue/layers/z_ui/HostActivity.kt
+++ app/src/main/java/com/example/budgetvalue/layers/z_ui/HostActivity.kt
@@ -1,11 +1,12 @@
-package com.example.budgetvalue
+package com.example.budgetvalue.layers.z_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.budgetvalue.R
 
-class MainActivity : AppCompatActivity() {
+class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_host)
     }
 }
