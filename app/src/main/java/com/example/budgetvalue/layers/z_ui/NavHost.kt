1a5c0565dcea6755b3c3fdeeadf29ef24c1bf0af
Troy Swanson <2troy2020@gmail.com>
2020-07-31 14:46:25 -0500

    comments

--- app/src/main/java/com/example/budgetvalue/layers/z_ui/NavHost.kt
+++ app/src/main/java/com/example/budgetvalue/layers/z_ui/NavHost.kt
@@ -2,4 +2,5 @@ package com.example.budgetvalue.layers.z_ui
 
 import androidx.navigation.fragment.NavHostFragment
 
+// TODO might be able to just remove this part
 class NavHost : NavHostFragment()
