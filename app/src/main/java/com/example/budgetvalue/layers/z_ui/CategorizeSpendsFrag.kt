02d0e33ef053e716f241e6b1780ee9c3bcaf3ac7
Troy Swanson <2troy2020@gmail.com>
2020-07-31 14:15:52 -0500

    bottom navigation

---
+++ app/src/main/java/com/example/budgetvalue/layers/z_ui/CategorizeSpendsFrag.kt
@@ -0,0 +1,12 @@
+package com.example.budgetvalue.layers.z_ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.budgetvalue.R
+
+class CategorizeSpendsFrag : Fragment(R.layout.frag_categorize_spends) {
+
+}
---
+++ app/src/main/java/com/example/budgetvalue/layers/z_ui/NavHost.kt
@@ -0,0 +1,5 @@
+package com.example.budgetvalue.layers.z_ui
+
+import androidx.navigation.fragment.NavHostFragment
+
+class NavHost : NavHostFragment()
