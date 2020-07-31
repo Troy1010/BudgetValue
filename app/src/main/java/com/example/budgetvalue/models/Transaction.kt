864282ce644b2bbb12520908c5d707e4e80e8cd7
Troy Swanson <2troy2020@gmail.com>
2020-07-31 17:10:42 -0500

    Transaction`categoryAmounts

--- app/src/main/java/com/example/budgetvalue/models/Transaction.kt
+++ app/src/main/java/com/example/budgetvalue/models/Transaction.kt
@@ -2,12 +2,15 @@ package com.example.budgetvalue.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.Gson
+import java.math.BigDecimal
 
 @Entity
 data class Transaction(
     var date: String?,
     var description: String,
     var amount: String,
+    var categoryAmounts: String = Gson().toJson(HashMap<String, BigDecimal>()),
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0
 )
