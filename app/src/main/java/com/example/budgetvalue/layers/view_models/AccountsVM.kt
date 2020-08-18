0511e6f0392b530b1363f200d9d791d642595474
Troy Swanson <2troy2020@gmail.com>
2020-08-17 22:00:06 -0500

    working on using Accounts table

--- app/src/main/java/com/example/budgetvalue/layers/view_models/AccountsVM.kt
+++ app/src/main/java/com/example/budgetvalue/layers/view_models/AccountsVM.kt
@@ -1,7 +1,8 @@
 package com.example.budgetvalue.layers.view_models
 
 import androidx.lifecycle.ViewModel
+import com.example.budgetvalue.layers.data_layer.Repo
 
-class AccountsVM: ViewModel() {
-
+class AccountsVM(val repo: Repo): ViewModel() {
+    val accounts = repo.getAccounts()
 }
--- app/src/main/java/com/example/budgetvalue/layers/z_ui/AccountsFrag.kt
+++ app/src/main/java/com/example/budgetvalue/layers/z_ui/AccountsFrag.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.budgetvalue.App
 import com.example.budgetvalue.R
+import com.example.budgetvalue.layers.view_models.AccountsVM
 import com.example.tmcommonkotlin.GenericRecyclerViewAdapter
+import com.example.tmcommonkotlin.vmFactoryFactory
 import kotlinx.android.synthetic.main.frag_accounts.*
+import kotlinx.android.synthetic.main.item_account.view.*
 
 class AccountsFrag: Fragment(), GenericRecyclerViewAdapter.Callbacks {
+    val appComponent by lazy { (requireActivity().application as App).appComponent }
+    val accountsVM : AccountsVM by viewModels { vmFactoryFactory { AccountsVM(appComponent.getRepo()) } }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,10 +31,11 @@ class AccountsFrag: Fragment(), GenericRecyclerViewAdapter.Callbacks {
     }
 
     override fun bindRecyclerItemView(view: View, i: Int) {
-        TODO("Not yet implemented")
+        view.editText_name?.setText(accountsVM.accounts.value?.get(i)?.name ?: "")
+        view.editText_amount?.setText(accountsVM.accounts.value?.get(i)?.amount ?: "")
     }
 
     override fun getRecyclerDataSize(): Int {
-        TODO("Not yet implemented")
+        return accountsVM.accounts.value?.size?:0
     }
 }
