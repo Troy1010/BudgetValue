300c7fe6df3fec0f48fae92de28c410f19054d64
Troy <troyswanson1010@gmail.com>
2020-08-26 23:46:12 -0500

    Util`toBehaviorSubject

--- app/src/main/java/com/example/budgetvalue/util/Util.kt
+++ app/src/main/java/com/example/budgetvalue/util/Util.kt
@@ -1,9 +1,17 @@
-package com.example.budgetvalue
+package com.example.budgetvalue.util
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 
+// This might be buggy..
+fun <T> Observable<T>.toBehaviorSubject(): BehaviorSubject<T> {
+    val behaviorSubject = BehaviorSubject.create<T>()
+    this.subscribe(behaviorSubject)
+    return behaviorSubject
+}
 
 
 fun <T> LiveData<T>.observeOnce(action: (T?) -> Unit) {
