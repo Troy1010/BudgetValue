e82f66fb17ea93ed332c558c896b19f01982be42
Troy <troyswanson1010@gmail.com>
2020-08-30 18:49:59 -0500

    removed parts using TableView library

--- app/src/main/java/com/example/budgetvalue/layers/z_ui/table_view/models/ColumnHeaderModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.budgetvalue.layers.z_ui.table_view.models
-
-import io.reactivex.rxjava3.subjects.BehaviorSubject
-import java.math.BigDecimal
-
-data class ColumnHeaderModel(val title: String, val amount: BigDecimal? =null)
--- app/src/main/java/com/example/budgetvalue/layers/z_ui/table_view/models/RowHeaderModel.kt
+++
@@ -1,3 +0,0 @@
-package com.example.budgetvalue.layers.z_ui.table_view.models
-
-data class RowHeaderModel(val data: String)
