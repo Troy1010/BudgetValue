f232782344d14cab85e405710ec4a41018d43d28
Troy <troyswanson1010@gmail.com>
2020-09-04 16:10:18 -0500

    MyTableView`cleanup

--- app/src/main/java/com/example/budgetvalue/layers/z_ui/views/MyTableView.kt
+++ app/src/main/java/com/example/budgetvalue/layers/z_ui/views/MyTableView.kt
@@ -11,6 +11,7 @@ import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.get
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL
 import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
 import androidx.recyclerview.widget.RecyclerView
@@ -32,10 +33,7 @@ class MyTableView @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr) {
     val mainView: View = View.inflate(context, R.layout.tableview_layout, this)
     var columnCount = 0
-    var dataZ = listOf<String>()
-    var headers = listOf<String>()
     var bInitialized = false
-    var firstPass = true
     val minColWidths = BehaviorSubject.createDefault<List<Int>>(listOf<Int>())
     val intrinsicColWidths = BehaviorSubject.createDefault<List<Int>>(listOf<Int>())
     val tableViewWidth = BehaviorSubject.createDefault<Int>(0)
@@ -52,15 +50,15 @@ class MyTableView @JvmOverloads constructor(
         .publish().refCount()
         .logSubscribe2("columnWidthsObservable")
         .toBehaviorSubjectWithDefault(listOf())
+        .also {
+            it.subscribe {
+                recyclerview_column_header.adapter?.notifyDataSetChanged()
+            }
+        }
 
     @SuppressLint("InflateParams")
     val defaultHeaderFactory = {
         val view = LayoutInflater.from(context).inflate(R.layout.tableview_header, null, false)
-//        view.layoutParams = LinearLayout.LayoutParams(
-//            300,
-//            100
-//        )
-//        val width = columnWidths.getOrNull(i) ?: 0
         view
     }
     val defaultHeaderBindAction = { view: View, data: Any ->
@@ -85,7 +83,7 @@ class MyTableView @JvmOverloads constructor(
     lateinit var cellViewFactory__: () -> View
     lateinit var cellViewBindAction__: (View, Any) -> Unit
     fun finishInit(
-        mHeaders: List<String>,
+        headers: List<String>,
         data: List<String>,
         headerViewFactory_: (() -> View) = defaultHeaderFactory,
         headerViewBindAction_: ((View, Any) -> Unit) = defaultHeaderBindAction,
@@ -98,36 +96,7 @@ class MyTableView @JvmOverloads constructor(
         cellViewFactory__ = cellViewFactory_
         cellViewBindAction__ = cellViewBindAction_
         // initialize
-        mainView.recyclerview_data.requestLayout()
-        //  find width
-        (mainView.parent as View).requestLayout()
-//        (mainView.parent as View).measure(MeasureSpec.EXACTLY, MeasureSpec.EXACTLY)
-//        logz("dsfgsd${(mainView.parent as View).measuredWidth}")
-//        (mainView.parent as View).measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
-//        logz("dsfgtrewtrtysd${(mainView.parent as View).measuredWidth}")
-//        (mainView.parent as View).addOnLayoutChangeListener(object: View.OnLayoutChangeListener {
-//            override fun onLayoutChange(
-//                p0: View?,
-//                p1: Int,
-//                p2: Int,
-//                p3: Int,
-//                p4: Int,
-//                p5: Int,
-//                p6: Int,
-//                p7: Int,
-//                p8: Int
-//            ) {
-//                mainView.requestLayout()
-//                logz("Layout is changing..")
-//            }
-//
-//        })
-//        mainView.parent.requestLayout()
-//        logz("widthhhdhh:${(mainView.parent as View).measuredWidth}")
-//        logz("widthhhhgh:${(mainView.parent as View).intrinsicWidth2}")
-//        logz("widthhhfhh:${(mainView.parent as View).exactWidth}")
-        //
-        setColumnHeaderData(mHeaders)
+        columnCount = headers.size
         mainView.recyclerview_column_header.adapter = GenericRecyclerViewAdapter5(
             headerFactory,
             { holder, view->
@@ -140,17 +109,12 @@ class MyTableView @JvmOverloads constructor(
             },
             { headers.size }
         )
-        //
-        setTableData(data)
-        mainView.recyclerview_column_header.layoutManager = MyTableViewLayoutManager2(
-            this,
-            HORIZONTAL
+        mainView.recyclerview_column_header.layoutManager = LinearLayoutManager(
+            context,
+            HORIZONTAL,
+            false
         )
-//        mainView.recyclerview_column_header.layoutManager = LinearLayoutManager(
-//            context,
-//            HORIZONTAL,
-//            false
-//        )
+        //
         val dividerItemDecoration = TableViewDecoration(
             context as Activity,
             VERTICAL,
@@ -160,9 +124,9 @@ class MyTableView @JvmOverloads constructor(
         mainView.recyclerview_data.adapter = MyTableViewDataRecyclerViewAdapter(
             rowFactory,
             cellBindAction,
-            { dataZ.size / columnCount + if ((dataZ.size % columnCount) == 0) 0 else 1 },
+            { data.size / columnCount + if ((data.size % columnCount) == 0) 0 else 1 },
             { columnCount },
-            { dataZ })
+            { data })
         mainView.recyclerview_data.layoutManager = MyTableViewLayoutManager(this)
         //
         bInitialized = true
@@ -173,6 +137,7 @@ class MyTableView @JvmOverloads constructor(
         minColWidths.onNext(
             generateMinWidths(headerFactory, headerBindAction, headers)
         )
+        //
         logz("finishInit`Close")
     }
 
@@ -196,32 +161,14 @@ class MyTableView @JvmOverloads constructor(
 
     class InitializationNotFinished(msg: String = "${MyTableView::class.simpleName}`finishInit() must be called before layout is completed") : Exception(msg)
 
-//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-//        logz("``MyTableView`onMeasure`measuredWidth:${measuredWidth}")
-//    }
-//    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-//        logz("onLayout`measuredWidth:${measuredWidth}")
-//        if (!bInitialized) throw InitializationNotFinished()
-//        if (firstPass) {
-//            firstPass=false
-//            recyclerview_column_header.adapter?.notifyDataSetChanged()
-//        } else {
-//            super.onLayout(changed, left, top, right, bottom)
-//        }
-//    }
-
-//    override fun onAttachedToWindow() {
-//        super.onAttachedToWindow()
-//        logz("OnAttachToWindow`measuredWidth:${measuredWidth}")
-//    }
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        if (!bInitialized) throw InitializationNotFinished()
+        super.onLayout(changed, left, top, right, bottom)
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        logz("onSizeChanged`Open`w:${w}")
         super.onSizeChanged(w, h, oldw, oldh)
         tableViewWidth.onNext(w)
-        // trigger another layout pass after tableViewWidth has been determined.
-        recyclerview_column_header.adapter?.notifyDataSetChanged()
     }
 
     // Give a list of strings
@@ -229,16 +176,6 @@ class MyTableView @JvmOverloads constructor(
     // How about: List: Any. If it's a string, cool. If it's a view, also cool.
     //    Okay, but then how can you have compile-time errors?
     // How about: 2 inputs. List<String> and HashMap<Position, View>
-    fun setColumnHeaderData(mHeaders: List<String>) {
-        headers = mHeaders
-        mainView.recyclerview_column_header.adapter?.notifyDataSetChanged()
-        columnCount = mHeaders.size
-    }
-
-    fun setTableData(dataZZ: List<String>) {
-        dataZ = dataZZ
-        mainView.recyclerview_data.adapter?.notifyDataSetChanged()
-    }
 
     fun generateIntrinsicWidths(
         rowFactory: () -> LinearLayout,
