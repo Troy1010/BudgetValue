d47579a4a9bb8416a602aae9fb19e6a2663e6fcf
Troy <troyswanson1010@gmail.com>
2020-09-05 01:13:53 -0500

    MyTableViewDataRecyclerViewAdapter`removed unused methods

--- app/src/main/java/com/example/budgetvalue/layers/z_ui/misc/MyTableViewDataRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/budgetvalue/layers/z_ui/misc/MyTableViewDataRecyclerViewAdapter.kt
@@ -19,25 +19,6 @@ class MyTableViewDataRecyclerViewAdapter(
     val getColumnCount: () -> Int,
     val getData: () -> List<String>
 ): RecyclerView.Adapter<MyTableViewDataRecyclerViewAdapter.ViewHolder>() {
-
-    val streamDataChanged = PublishSubject.create<Unit>().also {
-        this.registerAdapterDataObserver(object: RecyclerView.AdapterDataObserver() {
-            override fun onChanged() {
-                it.onNext(Unit)
-            }
-        })
-    }
-
-    fun setLayoutParams(rowView:LinearLayout, columnCount: Int, columnWidths: List<Int>) {
-        for (i in 0 until columnCount) {
-            val width = columnWidths.getOrNull(i) ?: 0
-            rowView[i].layoutParams = LinearLayout.LayoutParams(
-                width,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-        }
-    }
-
     class ViewHolder (view: View) : RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
