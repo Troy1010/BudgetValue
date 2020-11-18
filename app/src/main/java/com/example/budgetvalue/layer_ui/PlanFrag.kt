6ea88dbf5271cf90fc064e268321ac2142820156
Troy <troye668@gmail.com>
2020-11-17 20:28:21 -0600

    PlanFrag`looks okay, but header has width:0

--- app/src/main/java/com/example/budgetvalue/layer_ui/PlanFrag.kt
+++ app/src/main/java/com/example/budgetvalue/layer_ui/PlanFrag.kt
@@ -2,15 +2,31 @@ package com.example.budgetvalue.layer_ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.budgetvalue.App
 import com.example.budgetvalue.R
+import com.example.budgetvalue.layer_ui.TMTableView.CellRecipeBuilder
+import com.example.budgetvalue.layer_ui.misc.rxBind
+import com.example.budgetvalue.layer_ui.misc.rxBindOneWay
+import com.example.budgetvalue.reflectXY
+import com.example.budgetvalue.toBigDecimal2
 import com.tminus1010.tmcommonkotlin.misc.createVmFactory
+import com.tminus1010.tmcommonkotlin_rx.observe
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import kotlinx.android.synthetic.main.frag_plan.*
+import kotlinx.android.synthetic.main.frag_reconcile.*
+import kotlinx.android.synthetic.main.tableview_header_income.view.*
+import java.math.BigDecimal
 
 class PlanFrag: Fragment(R.layout.frag_plan) {
     val appComponent by lazy { (requireActivity().application as App).appComponent }
-    val planVM : PlanVM by viewModels { createVmFactory { PlanVM(appComponent.getRepo()) } }
+    val categoriesVM : CategoriesVM by viewModels { createVmFactory { CategoriesVM() } }
+    val planVM : PlanVM by viewModels { createVmFactory { PlanVM(categoriesVM) } }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,5 +42,22 @@ class PlanFrag: Fragment(R.layout.frag_plan) {
     }
 
     private fun setupObservers() {
+        val cellRecipeBuilder = CellRecipeBuilder(requireContext(), CellRecipeBuilder.DefaultType.CELL)
+        val headerRecipeBuilder = CellRecipeBuilder(requireContext(), CellRecipeBuilder.DefaultType.HEADER)
+        val incomeRecipeBuilder = CellRecipeBuilder<EditText, BehaviorSubject<BigDecimal>>(
+            { View.inflate(context, R.layout.item_text_edit, null) as EditText },
+            { v, bs -> v.rxBind(bs, { it.toBigDecimal2() } )}
+        )
+        planVM.planCategoryAmounts.observable
+            .observe(this) {
+                myTableView_plan.setRecipes(
+                    listOf(
+                        headerRecipeBuilder.buildOne("Category")
+                                + cellRecipeBuilder.buildMany(it.keys.toList()),
+                        headerRecipeBuilder.buildOne("Plan")
+                                + incomeRecipeBuilder.buildMany(it.values.toList())
+                    ).reflectXY()
+                )
+            }
     }
 }
--- app/src/main/java/com/example/budgetvalue/layer_ui/PlanVM.kt
+++ app/src/main/java/com/example/budgetvalue/layer_ui/PlanVM.kt
@@ -1,6 +1,24 @@
 package com.example.budgetvalue.layer_ui
 
 import androidx.lifecycle.ViewModel
+import com.example.budgetvalue.SourceArrayList
+import com.example.budgetvalue.SourceHashMap
 import com.example.budgetvalue.layer_data.Repo
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import java.math.BigDecimal
 
-class PlanVM(repo: Repo): ViewModel()
+class PlanVM(categoriesVM: CategoriesVM): ViewModel() {
+    val planCategoryAmounts = SourceHashMap<String, BehaviorSubject<BigDecimal>>()
+    init {
+        categoriesVM.categories.subscribe { categories ->
+            for ((categoryName, amountObservable) in planCategoryAmounts) {
+                if (categoryName !in categories.map { it.name })
+                    planCategoryAmounts.remove(categoryName)
+            }
+            for (categoryName in categories.map { it.name }) {
+                if (categoryName !in planCategoryAmounts)
+                    planCategoryAmounts[categoryName] = BehaviorSubject.createDefault(BigDecimal.ZERO)
+            }
+        }
+    }
+}
