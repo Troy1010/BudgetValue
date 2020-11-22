d71b80da3460271314d93bba21f26e7eacc19680
Troy <troye668@gmail.com>
2020-11-22 09:52:07 -0600

    comments

--- app/src/main/java/com/example/budgetvalue/layer_data/Repo.kt
+++ app/src/main/java/com/example/budgetvalue/layer_data/Repo.kt
@@ -2,6 +2,10 @@ package com.example.budgetvalue.layer_data
 
 import javax.inject.Inject
 
+/**
+ * A Repo is the facade to the data layer.
+ * If you ever change how the data is written/retrieved, you don't need to change the ui_layer.
+ */
 class Repo @Inject constructor(
     transactionParser: TransactionParser,
     sharedPrefWrapper: ISharedPrefWrapper,
