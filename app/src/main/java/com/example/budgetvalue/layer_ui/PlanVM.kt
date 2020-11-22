05a876ba2d6f211a127c0ebcb27a8372c9d8b211
Troy <troye668@gmail.com>
2020-11-22 14:10:54 -0600

    PlanVM`minor

--- app/src/main/java/com/example/budgetvalue/layer_ui/PlanVM.kt
+++ app/src/main/java/com/example/budgetvalue/layer_ui/PlanVM.kt
@@ -12,7 +12,7 @@ import java.math.BigDecimal
 
 class PlanVM(repo: Repo, categoriesVM: CategoriesVM): ViewModel() {
     val planCategoryAmounts = SourceHashMap<String, BehaviorSubject<BigDecimal>>()
-    var repoLoadComplete = BehaviorSubject.createDefault(false)
+    private var repoLoadComplete = BehaviorSubject.createDefault(false) // TODO("Simplify this")
     init {
         // # Sync planCategoryAmounts with Repo
         // ## Bind once Repo -> planCategoryAmounts
