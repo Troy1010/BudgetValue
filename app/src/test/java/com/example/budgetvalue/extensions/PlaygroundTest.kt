894b91f7a8751a758ae6c1304a3f2f399f1023b6
Troy <troye668@gmail.com>
2020-11-22 22:04:12 -0600

    PlaygroundTest

---
+++ app/src/test/java/com/example/budgetvalue/extensions/PlaygroundTest.kt
@@ -0,0 +1,18 @@
+package com.example.budgetvalue.extensions
+
+import org.junit.Test
+import io.reactivex.rxjava3.core.Observable
+import org.junit.Assert.assertEquals
+
+class PlaygroundTest() {
+    @Test
+    fun test1() {
+        // # Stimulate
+        val result = Observable.just(4, 6, 8, 4, 2)
+            .scan(0) { acc, y -> acc + y }
+            .toList()
+            .blockingGet()
+        // # Verify
+        assertEquals(listOf(0, 4, 10, 18, 22, 24), result)
+    }
+}
