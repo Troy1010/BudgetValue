7261a943b83e45b6926a0269389e41fcab581233
Troy <troye668@gmail.com>
2020-11-22 23:33:13 -0600

    PlanVM`simplified, not using Thread.sleep()

--- app/src/main/java/com/example/budgetvalue/layer_ui/PlanVM.kt
+++ app/src/main/java/com/example/budgetvalue/layer_ui/PlanVM.kt
@@ -8,6 +8,7 @@ import com.example.budgetvalue.layer_data.Repo
 import com.example.budgetvalue.model_data.PlanCategoryAmounts
 import com.tminus1010.tmcommonkotlin.logz.logz
 import com.tminus1010.tmcommonkotlin_rx.toBehaviorSubject
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import java.math.BigDecimal
@@ -17,23 +18,21 @@ class PlanVM(repo: Repo, categoriesVM: CategoriesVM): ViewModel() {
     private var repoLoadComplete = BehaviorSubject.createDefault(false) // TODO("Simplify this")
     val uncategorizedPlan = planCategoryAmounts.itemObservablesObservable
         .map { it.map { it.value } }
-        .flatMap {
-            val pairwiseDifference = BehaviorSubject.createDefault(BigDecimal.ZERO)
-            val x = pairwiseDifference
-                .scan(BigDecimal.ZERO) { acc, y -> acc + y }
-                .toBehaviorSubject() // TODO("Simplify")
-            it.forEach { it.pairwise(BigDecimal.ZERO).map { it.second - it.first }.subscribe(pairwiseDifference) } // TODO("Are these subscriptions safe?")
-            x
-        }
+        .flatMap(::getTotalObservable)
         .toBehaviorSubject()
+    fun getTotalObservable(it: Iterable<BehaviorSubject<BigDecimal>>): BehaviorSubject<BigDecimal> {
+        val pairwiseDifference = BehaviorSubject.createDefault(BigDecimal.ZERO)
+        val returning = pairwiseDifference
+            .scan(BigDecimal.ZERO) { acc, y -> acc + y }
+            .toBehaviorSubject() // TODO("Simplify")
+        it.forEach { it.pairwise(BigDecimal.ZERO).map { it.second - it.first }.subscribe(pairwiseDifference) } // TODO("Are these subscriptions safe?")
+        return returning
+    }
     init {
         // # Bind once Repo <-> planCategoryAmounts
         // ## Bind once Repo -> planCategoryAmounts
         repo.getPlanCategoryAmounts().take(1).observeOn(Schedulers.io()).subscribe {
-            logz("Repo -> planCategoryAmounts")
-            for (x in it) {
-                planCategoryAmounts[x.category] = x.amount
-            }
+            planCategoryAmounts.putAll(it.associate { Pair(it.category, it.amount) })
             repoLoadComplete.onNext(true) // TODO("Simplify this")
         }
         // ## Bind planCategoryAmounts -> Repo
@@ -41,10 +40,8 @@ class PlanVM(repo: Repo, categoriesVM: CategoriesVM): ViewModel() {
             .observeOn(Schedulers.io())
             .filter { it.second }
             .map { it.first }
+            .doOnNext { Completable.fromAction { repo.clearPlanCategoryAmounts() }.blockingAwait() }
             .subscribe {
-                logz("planCategoryAmounts -> Repo")
-                repo.clearPlanCategoryAmounts()
-                Thread.sleep(1000) // TODO("this should be unnecessary")
                 for ((categoryName, amountBehaviorSubject) in it) {
                     repo.addPlanCategoryAmounts(PlanCategoryAmounts(
                         categoryName,
