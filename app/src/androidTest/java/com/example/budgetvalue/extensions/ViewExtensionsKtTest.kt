639d9813191e4df829a5eddef687d7b06b6bdfa0
Troy <troye668@gmail.com>
2020-11-25 23:48:07 -0600

    ViewExtensions`setWidth

---
+++ app/src/androidTest/java/com/example/budgetvalue/extensions/ViewExtensionsKtTest.kt
@@ -0,0 +1,24 @@
+package com.example.budgetvalue.extensions
+
+import android.view.LayoutInflater
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.budgetvalue.AppMock
+import com.example.budgetvalue.R
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+
+class ViewExtensionsKtTest {
+    val app by lazy { InstrumentationRegistry.getInstrumentation().targetContext.applicationContext as AppMock }
+
+    @Test
+    fun setWidth() {
+        // # Given
+        val view = LayoutInflater.from(app).inflate(R.layout.blank_view, null, false)
+        assertNotEquals(50, view.layoutParams?.width)
+        // # Stimulate
+        view.setWidth(50)
+        // # Verify
+        assertEquals(50, view.layoutParams?.width)
+    }
+}
