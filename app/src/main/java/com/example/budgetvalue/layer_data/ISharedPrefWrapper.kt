04d4e4208cbf92cfec2a77b0455d12d86c985e29
Troy <troye668@gmail.com>
2020-11-27 15:53:05 -0600

    linting

--- app/src/main/java/com/example/budgetvalue/layer_data/ISharedPrefWrapper.kt
+++ app/src/main/java/com/example/budgetvalue/layer_data/ISharedPrefWrapper.kt
@@ -1,10 +1,8 @@
 package com.example.budgetvalue.layer_data
 
 import com.example.budgetvalue.model_app.Category
-import com.example.budgetvalue.model_data.ReconcileCategoryAmount
 import io.reactivex.rxjava3.core.Observable
 import java.math.BigDecimal
-import java.time.DayOfWeek
 
 interface ISharedPrefWrapper {
     fun fetchReconcileCategoryAmounts(): Observable<Map<Category, BigDecimal>>
