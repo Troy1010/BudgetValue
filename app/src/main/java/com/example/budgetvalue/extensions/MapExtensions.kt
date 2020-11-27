5200a3868823c6549c6fda08b5078af964e9e54f
Troy <troye668@gmail.com>
2020-11-27 15:29:00 -0600

    MapExtensions.toSourceHashMap`uses putAll

--- app/src/main/java/com/example/budgetvalue/extensions/MapExtensions.kt
+++ app/src/main/java/com/example/budgetvalue/extensions/MapExtensions.kt
@@ -3,9 +3,7 @@ package com.example.budgetvalue.extensions
 import com.example.budgetvalue.SourceHashMap
 
 fun <K, V> Map<K, V>.toSourceHashMap(): SourceHashMap<K, V> {
-    // I'm not sure why .apply { putAll(this) } doesn't work here..
-//    return SourceHashMap<K, V>().apply { putAll(this) }
-    return SourceHashMap<K, V>().also { this.forEach { (k, v) -> it[k] = v } }
+    return SourceHashMap<K, V>().also { it.putAll(this) }
 }
 
 fun <K, V> Map<K, V>.toHashMap(): HashMap<K, V> {
--- app/src/test/java/com/example/budgetvalue/PlaygroundTest.kt
+++ app/src/test/java/com/example/budgetvalue/PlaygroundTest.kt
@@ -1,5 +1,6 @@
 package com.example.budgetvalue
 
+import com.example.budgetvalue.extensions.toSourceHashMap
 import com.example.budgetvalue.extensions.withLatestFrom
 import org.junit.Test
 import io.reactivex.rxjava3.core.Observable
@@ -9,13 +10,18 @@ import org.junit.Assert.assertEquals
 class PlaygroundTest() {
     @Test
     fun test1() {
+        // # Given
+        val feedingMap = hashMapOf(0 to 10, 3 to 30, 9 to 90)
         // # Stimulate
-        val observable = Observable.just(4, 6, 8, 4, 2)
-            .compose { observable ->
-                observable.skip(1).zipWith(observable, BiFunction<Int, Int, Pair<Int, Int>> { a, b -> Pair(a,b)})
-            }
-        val result = observable.toList().blockingGet()
+//        val sourceHashMap = SourceHashMap<Int, Int>().apply { putAll(feedingMap) }
+        val sourceHashMap = feedingMap.toSourceHashMap()
         // # Verify
-//        assertEquals(listOf(0, 4, 10, 18, 22, 24), result)
+        assertEquals(sourceHashMap[0], 10)
+        assertEquals(sourceHashMap[3], 30)
+        assertEquals(sourceHashMap[9], 90)
+        Thread.sleep(1000) // TODO("use RxThreadsRule")
+        assertEquals(sourceHashMap.itemObservablesObservable.value[0]!!.value, 10)
+        assertEquals(sourceHashMap.itemObservablesObservable.value[3]!!.value, 30)
+        assertEquals(sourceHashMap.itemObservablesObservable.value[9]!!.value, 90)
     }
 }
