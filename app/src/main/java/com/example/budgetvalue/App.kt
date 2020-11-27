0d0af43e211d3b1d6d66e5e1246392a354562da5
Troy <troye668@gmail.com>
2020-11-27 15:09:24 -0600

    App`Configure Rx

--- app/src/main/java/com/example/budgetvalue/App.kt
+++ app/src/main/java/com/example/budgetvalue/App.kt
@@ -4,11 +4,15 @@ import android.app.Application
 import com.example.budgetvalue.dependency_injection.DaggerAppComponent
 import com.example.budgetvalue.dependency_injection.MiscModule
 import com.tminus1010.tmcommonkotlin.logz.logz
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
 
 open class App : Application() {
     override fun onCreate() {
         logz("!*!*! START")
         super.onCreate()
+
+        // # Configure Rx
+        RxJavaPlugins.setErrorHandler { throw it.cause?:it }
     }
     open val appComponent by lazy {
         DaggerAppComponent.builder()
