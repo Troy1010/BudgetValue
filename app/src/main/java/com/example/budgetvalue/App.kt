f5c98193f13486d147ad99431030aee3aac34d62
Troy <troye668@gmail.com>
2020-11-27 15:08:20 -0600

    linting

--- app/src/main/java/com/example/budgetvalue/App.kt
+++ app/src/main/java/com/example/budgetvalue/App.kt
@@ -1,9 +1,8 @@
 package com.example.budgetvalue
 
 import android.app.Application
-import com.example.budgetvalue.dependency_injection.MiscModule
-import com.example.budgetvalue.dependency_injection.BudgetValueDBModule
 import com.example.budgetvalue.dependency_injection.DaggerAppComponent
+import com.example.budgetvalue.dependency_injection.MiscModule
 import com.tminus1010.tmcommonkotlin.logz.logz
 
 open class App : Application() {
