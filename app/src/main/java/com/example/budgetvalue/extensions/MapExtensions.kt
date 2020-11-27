d00f7faf6d6a16d8212c77d493383dbf002a7d86
Troy <troye668@gmail.com>
2020-11-27 16:54:25 -0600

    MapExtensions`associate

--- app/src/main/java/com/example/budgetvalue/extensions/MapExtensions.kt
+++ app/src/main/java/com/example/budgetvalue/extensions/MapExtensions.kt
@@ -9,3 +9,7 @@ fun <K, V> Map<K, V>.toSourceHashMap(): SourceHashMap<K, V> {
 fun <K, V> Map<K, V>.toHashMap(): HashMap<K, V> {
     return HashMap(this)
 }
+
+fun <K, V, K2, V2> Map<K, V>.associate(action: (Map.Entry<K, V>) -> Pair<K2, V2>): Map<K2, V2> {
+    return this.entries.associate(action)
+}
