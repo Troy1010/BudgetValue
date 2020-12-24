cd9281ed14c37f71e3b34dc4409362fe1737b4c4
Troy <troye668@gmail.com>
2020-12-24 11:42:12 -0600

    removed logz, comments

--- app/src/main/java/com/example/budgetvalue/layer_ui/TMTableView2/InnerRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/budgetvalue/layer_ui/TMTableView2/InnerRecyclerViewAdapter.kt
@@ -9,14 +9,11 @@ import com.tminus1010.tmcommonkotlin.logz.logz
 class InnerRecyclerViewAdapter(
     val context: Context,
     val recipeGrid: RecipeGrid,
-    val j: Int
+    val j: Int,
 ) : RecyclerView.Adapter<InnerRecyclerViewAdapter.ViewHolder>() {
-    init {
-        logz("recipeGrid xSize:${recipeGrid.size} ySize:${recipeGrid[0].size} ySizeIsSame:${recipeGrid.map { it.size }.all { it == recipeGrid[0].size }}")
-    }
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
     override fun onCreateViewHolder(parent: ViewGroup, i: Int): ViewHolder {
-        logz("i:$i j:$j")
         return ViewHolder(recipeGrid.createResizedView(i, j))
     }
 
@@ -26,12 +23,5 @@ class InnerRecyclerViewAdapter(
         recipeGrid[j][holder.adapterPosition].bindView(holder.itemView)
     }
 
-    override fun getItemCount() = recipeGrid[0].size.also { logz("recipeGrid[0].size:${it}") }
-//    override fun onViewAttachedToWindow(holder: ViewHolder) {
-//        super.onViewAttachedToWindow(holder)
-//        recipe2D!!.value
-//            .map { try { it.toList()[holder.adapterPosition].intrinsicWidth } catch (e:Exception) { 0 } }
-//            .fold(0) { acc, v -> max(acc, v) }
-//            .also { holder.itemView.updateLayoutParams { width = it } }
-//    }
+    override fun getItemCount() = recipeGrid[0].size
 }
