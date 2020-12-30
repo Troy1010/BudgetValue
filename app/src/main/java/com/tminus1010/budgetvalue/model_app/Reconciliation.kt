daed7dc2341bebb8699be8daec19daa433736d4c
Troy <troye668@gmail.com>
2020-12-29 19:00:24 -0600

    Reconciliation`toReconciliationReceived

--- app/src/main/java/com/tminus1010/budgetvalue/model_app/Reconciliation.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/model_app/Reconciliation.kt
@@ -1,10 +1,18 @@
 package com.tminus1010.budgetvalue.model_app
 
 import com.tminus1010.budgetvalue.SourceHashMap
+import com.tminus1010.budgetvalue.model_data.ReconciliationReceived
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class Reconciliation(
     val localDate: LocalDate,
     val categoryAmounts: SourceHashMap<Category, BigDecimal>
-)
+) {
+    fun toReconciliationReceived(amount: BigDecimal): ReconciliationReceived {
+        return ReconciliationReceived(
+            localDate = localDate,
+            categoryAmounts = categoryAmounts.mapKeys { it.key.name }.toMap(),
+            amount = amount)
+    }
+}
