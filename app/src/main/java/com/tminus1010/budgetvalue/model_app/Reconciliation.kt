c6041001b841ca616208077203ddce3bfad1f846
Troy <troye668@gmail.com>
2020-12-30 19:00:06 -0600

    Reconciliation`categoryAmounts is not a SourceHashMap

--- app/src/main/java/com/tminus1010/budgetvalue/model_app/Reconciliation.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/model_app/Reconciliation.kt
@@ -1,6 +1,5 @@
 package com.tminus1010.budgetvalue.model_app
 
-import com.tminus1010.budgetvalue.SourceHashMap
 import com.tminus1010.budgetvalue.layer_data.TypeConverter
 import com.tminus1010.budgetvalue.model_data.ReconciliationReceived
 import java.math.BigDecimal
@@ -8,7 +7,7 @@ import java.time.LocalDate
 
 data class Reconciliation(
     val localDate: LocalDate,
-    val categoryAmounts: SourceHashMap<Category, BigDecimal>
+    val categoryAmounts: Map<Category, BigDecimal>
 ) {
     fun toReconciliationReceived(typeConverter: TypeConverter, amount: BigDecimal): ReconciliationReceived {
         return ReconciliationReceived(
