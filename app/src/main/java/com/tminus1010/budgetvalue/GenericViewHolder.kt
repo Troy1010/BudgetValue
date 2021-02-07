3ee7c1b3d2b2c7595c16456f09389e73645c98f0
Troy <troye668@gmail.com>
2021-02-07 14:25:51 -0600

    GenericViewHolder

---
+++ app/src/main/java/com/tminus1010/budgetvalue/GenericViewHolder.kt
@@ -0,0 +1,6 @@
+package com.tminus1010.budgetvalue
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GenericViewHolder<T:View>(v: T) : RecyclerView.ViewHolder(v)
