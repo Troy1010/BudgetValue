bcd0355bc06507651dd1d433a5a303e480ef9fc4
Troy <troye668@gmail.com>
2021-02-07 12:49:13 -0600

    minor

--- app/src/main/java/com/tminus1010/budgetvalue/App.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/App.kt
@@ -1,6 +1,7 @@
 package com.tminus1010.budgetvalue
 
 import android.app.Application
+import com.tminus1010.budgetvalue.dependency_injection.AppComponent
 import com.tminus1010.budgetvalue.dependency_injection.DaggerAppComponent
 import com.tminus1010.budgetvalue.dependency_injection.MiscModule
 import com.tminus1010.tmcommonkotlin.logz.logz
@@ -14,7 +15,7 @@ open class App : Application() {
         // # Configure Rx
         RxJavaPlugins.setErrorHandler { throw it.cause?:it }
     }
-    open val appComponent by lazy {
+    open val appComponent: AppComponent by lazy {
         DaggerAppComponent.builder()
             .miscModule(MiscModule(this))
             .build()
