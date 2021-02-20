75a934ce7d97ed527765153502856180fa6fbb90
Troy <troye668@gmail.com>
2021-02-20 15:33:09 -0600

    click back

--- app/src/main/java/com/tminus1010/budgetvalue/layer_ui/CategoryCustomizationFrag.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/layer_ui/CategoryCustomizationFrag.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.tminus1010.budgetvalue.R
+import com.tminus1010.budgetvalue.extensions.nav
+import com.tminus1010.budgetvalue.extensions.v
+import kotlinx.android.synthetic.main.frag_category_customization.view.*
 
 class CategoryCustomizationFrag: Fragment(R.layout.frag_category_customization) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        v.btn_cc_done.setOnClickListener { nav.navigateUp() }
     }
 }
