66664581d91e3acd66a43cda95aee44d9c21d667
Troy <troye668@gmail.com>
2021-02-26 04:05:13 -0600

    AppInitializer`new categories

--- app/src/main/java/com/tminus1010/budgetvalue/layer_domain/AppInitializer.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/layer_domain/AppInitializer.kt
@@ -19,19 +19,16 @@ class AppInitializer @Inject constructor(
     val initCategories
         get() = listOf(
             Category("Food", Category.Type.Always),
-            Category("Drinks", Category.Type.Always),
+            Category("Vanity Food", Category.Type.Reservoir),
             Category("Improvements", Category.Type.Always),
             Category("Dentist", Category.Type.Always),
             Category("Medical Supplies", Category.Type.Always),
-            Category("Leli gifts/activities", Category.Type.Always),
             Category("Misc", Category.Type.Always),
-            Category("Gas", Category.Type.Always),
-            Category("Vanity Food", Category.Type.Reservoir),
+            Category("Commute", Category.Type.Always),
             Category("Emergency", Category.Type.Reservoir),
             Category("Charity", Category.Type.Reservoir),
             Category("Trips", Category.Type.Reservoir),
             Category("Christmas", Category.Type.Reservoir),
-            Category("Gifts", Category.Type.Reservoir),
             Category("Activities", Category.Type.Reservoir),
         )
 }
