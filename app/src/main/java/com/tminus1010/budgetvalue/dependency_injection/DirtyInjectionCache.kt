d91bc41bbb864fedb793d89e8fe8764c22a1162a
Troy <troye668@gmail.com>
2021-02-26 00:31:57 -0600

    DirtyInjectionCache`no reflection

--- app/src/main/java/com/tminus1010/budgetvalue/dependency_injection/DirtyInjectionCache.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/dependency_injection/DirtyInjectionCache.kt
@@ -1,19 +1,19 @@
 package com.tminus1010.budgetvalue.dependency_injection
 
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
-import com.tminus1010.budgetvalue.dependency_injection.view_model_provision.IVMComponent
-import com.tminus1010.budgetvalue.extensions.viewModelsWithDagger
+import com.tminus1010.tmcommonkotlin.view.createViewModelFactory
 
-class DirtyInjectionCache(val activity: AppCompatActivity, val vmComponent: IVMComponent) {
-    constructor(activity: FragmentActivity, vmComponent: IVMComponent) :
+class DirtyInjectionCache(val activity: AppCompatActivity, val component: AppComponent) {
+    constructor(activity: FragmentActivity, vmComponent: AppComponent) :
             this(activity as AppCompatActivity, vmComponent)
 
     val cache = hashMapOf<Class<*>, Lazy<ViewModel>>()
 
-    inline fun <reified VM : ViewModel> get(): VM =
-        (cache[VM::class.java] ?: activity.viewModelsWithDagger<VM>(vmComponent)
-            .also { cache[VM::class.java] = it })
-            .value as VM
+    inline fun <reified VM : ViewModel> get(noinline action: () -> VM): VM =
+        cache[VM::class.java]?.value as? VM
+            ?: activity.viewModels<VM> { createViewModelFactory(action) }
+                .also { cache[VM::class.java] = it }.value
 }
--- app/src/main/java/com/tminus1010/budgetvalue/dependency_injection/IDirtyInjection.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/dependency_injection/IDirtyInjection.kt
@@ -4,31 +4,32 @@ import com.tminus1010.budgetvalue.layer_ui.*
 
 interface IDirtyInjection {
     val dirtyInjectionCache: DirtyInjectionCache
+    private val component get() = dirtyInjectionCache.component
 
     val accountsVM: AccountsVM
-        get() = dirtyInjectionCache.get()
+        get() = dirtyInjectionCache.get { component.getAccountsVM() }
 
     val activePlanVM: ActivePlanVM
-        get() = dirtyInjectionCache.get()
+        get() = dirtyInjectionCache.get { component.getActivePlanVM() }
 
     val activeReconciliationVM: ActiveReconciliationVM
-        get() = dirtyInjectionCache.get()
+        get() = dirtyInjectionCache.get { component.getActiveReconciliationVM() }
 
     val activeReconciliationVM2: ActiveReconciliationVM2
-        get() = dirtyInjectionCache.get()
+        get() = dirtyInjectionCache.get { component.getActiveReconciliationVM2() }
 
     val advancedCategorizeVM: AdvancedCategorizeVM
-        get() = dirtyInjectionCache.get()
+        get() = dirtyInjectionCache.get { component.getAdvancedCategorizeVM() }
 
     val budgetedVM: BudgetedVM
-        get() = dirtyInjectionCache.get()
+        get() = dirtyInjectionCache.get { component.getBudgetedVM() }
 
     val categorizeVM: CategorizeVM
-        get() = dirtyInjectionCache.get()
+        get() = dirtyInjectionCache.get { component.getCategorizeVM() }
 
     val historyVM: HistoryVM
-        get() = dirtyInjectionCache.get()
+        get() = dirtyInjectionCache.get { component.getHistoryVM() }
 
     val transactionsVM: TransactionsVM
-        get() = dirtyInjectionCache.get()
+        get() = dirtyInjectionCache.get { component.getTransactionsVM() }
 }
