2e0b6a3a319561b8f65feedeb00d46319c95c019
Troy <troyswanson1010@gmail.com>
2021-02-27 04:49:06 -0600

    TypeConverter:bugfix

--- app/src/main/java/com/tminus1010/budgetvalue/layer_data/TypeConverter.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/layer_data/TypeConverter.kt
@@ -1,9 +1,9 @@
 package com.tminus1010.budgetvalue.layer_data
 
 
+import com.google.gson.Gson
 import com.tminus1010.budgetvalue.getTypeForGson
 import com.tminus1010.budgetvalue.model_data.Category
-import com.google.gson.Gson
 import com.tminus1010.tmcommonkotlin.rx.extensions.associate
 import java.math.BigDecimal
 import javax.inject.Inject
@@ -14,7 +14,7 @@ class TypeConverter @Inject constructor(
     fun categoryAmounts(s: String?): Map<Category, BigDecimal> =
         if (s == null) emptyMap() else
             Gson().fromJson<Map<String, String>>(s, getTypeForGson<HashMap<String, String>>())
-                .associate { categoryParser.parseCategory(it.key) to it.value.toBigDecimal() }
+                .associate { categoryParser.parseCategory(it.key) to (it.value as Any).toString().toBigDecimal() }
 
     fun string(categoryAmounts: Map<Category, BigDecimal>?): String? =
         categoryAmounts
