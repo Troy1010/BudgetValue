55f086f02188c5d81c42fadd5ef27dd9984570cd
Troy <troyswanson1010@gmail.com>
2021-02-27 13:55:56 -0600

    non-static TypeConverterForRoom

--- app/src/main/java/com/tminus1010/budgetvalue/dependency_injection/RepoRoomDBModule.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/dependency_injection/RepoRoomDBModule.kt
@@ -3,6 +3,7 @@ package com.tminus1010.budgetvalue.dependency_injection
 import androidx.room.Room
 import com.tminus1010.budgetvalue.App
 import com.tminus1010.budgetvalue.layer_data.DB
+import com.tminus1010.budgetvalue.layer_data.TypeConverterForRoom
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -13,6 +14,7 @@ class RepoRoomDBModule {
     @Singleton
     fun providesDatabase(app: App): DB {
         return Room.databaseBuilder(app, DB::class.java, "BudgetValueDB")
+            .addTypeConverter(TypeConverterForRoom())
             .fallbackToDestructiveMigration()
             .build()
     }
