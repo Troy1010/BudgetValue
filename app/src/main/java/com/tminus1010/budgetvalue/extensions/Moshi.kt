b13c52abee1eaa8e316d88ae5a5704e8b466e0c9
Troy <troyswanson1010@gmail.com>
2021-02-27 16:32:19 -0600

    starting to replace GSon with moshi, using @OptIn with compiler option

--- app/src/main/java/com/tminus1010/budgetvalue/extensions/Moshi.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/extensions/Moshi.kt
@@ -3,10 +3,10 @@ package com.tminus1010.budgetvalue.extensions
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapter
 
-@ExperimentalStdlibApi
+@OptIn(ExperimentalStdlibApi::class)
 inline fun <reified T> Moshi.toJson(x: T): String =
     this.adapter<T>().toJson(x)
 
-@ExperimentalStdlibApi
+@OptIn(ExperimentalStdlibApi::class)
 inline fun <reified T> Moshi.fromJson(s: String): T =
     this.adapter<T>().fromJson(s)!!
--- app/src/main/java/com/tminus1010/budgetvalue/layer_data/RoomTypeConverter.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/layer_data/RoomTypeConverter.kt
@@ -2,9 +2,14 @@ package com.tminus1010.budgetvalue.layer_data
 
 import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
+import com.tminus1010.budgetvalue.extensions.fromJson
+import com.tminus1010.budgetvalue.extensions.toJson
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.tminus1010.budgetvalue.extensions.toJson
 import com.tminus1010.budgetvalue.model_data.Category
+import com.tminus1010.budgetvalue.moshi
+import com.tminus1010.budgetvalue.moshi
 import java.lang.reflect.Type
 import java.math.BigDecimal
 import java.time.LocalDate
@@ -30,15 +35,12 @@ class RoomTypeConverter {
         s.toBigDecimal()
 
     @TypeConverter
-    fun fromCategoryAmountsToString(x: Map<String, BigDecimal>): String {
-        return Gson().toJson(x)
-    }
+    fun fromCategoryAmountsToString(x: Map<String, BigDecimal>): String =
+        moshi.toJson(x)
 
     @TypeConverter
-    fun toCategoryAmounts(s: String): HashMap<String, BigDecimal> {
-        val type: Type = object : TypeToken<HashMap<String, BigDecimal>>() {}.type
-        return Gson().fromJson<HashMap<String, BigDecimal>>(s, type)
-    }
+    fun toCategoryAmounts(s: String): HashMap<String, BigDecimal> =
+        moshi.fromJson(s)
 
     @TypeConverter
     fun fromDateToString(x: LocalDate): String =
