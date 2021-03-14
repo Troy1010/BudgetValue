8c3f8b9581594dea12bb5b250197a8b6d1e6f73c
Troy <troyswanson1010@gmail.com>
2021-03-14 16:04:19 -0500

    AppInitializer`initCategories method in companion obj

--- app/src/main/java/com/tminus1010/budgetvalue/layer_domain/AppInitializer.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/layer_domain/AppInitializer.kt
@@ -18,19 +18,21 @@ class AppInitializer @Inject constructor(
         }
     }
 
-    val initCategories
-        get() = listOf(
-            Category("Food", Category.Type.Always),
-            Category("Vanity Food", Category.Type.Reservoir),
-            Category("Improvements", Category.Type.Always),
-            Category("Dentist", Category.Type.Always),
-            Category("Medical Supplies", Category.Type.Always),
-            Category("Misc", Category.Type.Always),
-            Category("Commute", Category.Type.Always),
-            Category("Emergency", Category.Type.Reservoir),
-            Category("Charity", Category.Type.Reservoir),
-            Category("Trips", Category.Type.Reservoir),
-            Category("Christmas", Category.Type.Reservoir),
-            Category("Activities", Category.Type.Reservoir),
-        )
+    companion object {
+        val initCategories
+            get() = listOf(
+                Category("Food", Category.Type.Always),
+                Category("Vanity Food", Category.Type.Reservoir),
+                Category("Improvements", Category.Type.Always),
+                Category("Dentist", Category.Type.Always),
+                Category("Medical Supplies", Category.Type.Always),
+                Category("Misc", Category.Type.Always),
+                Category("Commute", Category.Type.Always),
+                Category("Emergency", Category.Type.Reservoir),
+                Category("Charity", Category.Type.Reservoir),
+                Category("Trips", Category.Type.Reservoir),
+                Category("Christmas", Category.Type.Reservoir),
+                Category("Activities", Category.Type.Reservoir),
+            )
+    }
 }
