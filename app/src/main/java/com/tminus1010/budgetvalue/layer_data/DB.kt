e5d6af9bbe9bf00cdc22d129c7f25f91bc7c10aa
Troy <troyswanson1010@gmail.com>
2021-03-14 04:25:46 -0500

    cleanup

--- app/src/main/java/com/tminus1010/budgetvalue/layer_data/DB.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/layer_data/DB.kt
@@ -6,8 +6,8 @@ import androidx.room.TypeConverters
 import com.tminus1010.budgetvalue.model_data.*
 
 @TypeConverters(RoomTypeConverter::class)
-@Database(entities = [TransactionDTO::class, AccountDTO::class, ReconciliationDTO::class, PlanCategoryAmount::class, PlanDTO::class, CategoryDTO::class],
-    version = 31)
+@Database(entities = [TransactionDTO::class, AccountDTO::class, ReconciliationDTO::class, PlanDTO::class, CategoryDTO::class],
+    version = 32)
 abstract class DB : RoomDatabase() {
     abstract fun miscDAO(): MiscDAO
     abstract fun activeCategoryDAO(): UserCategoriesDAO
