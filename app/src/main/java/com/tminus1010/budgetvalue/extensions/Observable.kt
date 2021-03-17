f114b8d85d3112fb0822bad6014701e6705a11c2
Troy <troyswanson1010@gmail.com>
2021-03-17 04:46:26 -0500

    minor

--- app/src/main/java/com/tminus1010/budgetvalue/extensions/Observable.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/extensions/Observable.kt
@@ -6,6 +6,6 @@ import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-fun <T> Observable<T>.io() = observeOn(Schedulers.io())
+fun <T> Observable<T>.io(): Observable<T> = observeOn(Schedulers.io())
 fun <T> Observable<T>.launch(scheduler: Scheduler = Schedulers.io(), completableProvider: (T) -> Completable): Disposable =
     this.observeOn(scheduler).flatMapCompletable { completableProvider(it) }.subscribe()
