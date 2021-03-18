33c8cb2c4160f770651db3ed291606935ebcdbd8
Troy <troyswanson1010@gmail.com>
2021-03-17 20:51:44 -0500

    GenViewHolder2

--- app/src/main/java/com/tminus1010/budgetvalue/GenViewHolder.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/GenViewHolder.kt
@@ -2,5 +2,7 @@ package com.tminus1010.budgetvalue
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 
 class GenViewHolder(v: View) : RecyclerView.ViewHolder(v)
+class GenViewHolder2<T:ViewBinding>(val binding: T) : RecyclerView.ViewHolder(binding.root)
