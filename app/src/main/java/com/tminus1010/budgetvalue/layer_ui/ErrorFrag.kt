3472a64a5e118d7a57dfb8debe80e7081330844a
Troy <troyswanson1010@gmail.com>
2021-03-17 20:52:19 -0500

    ErrorFrag`viewBinding

--- app/src/main/java/com/tminus1010/budgetvalue/layer_ui/ErrorFrag.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/layer_ui/ErrorFrag.kt
@@ -7,45 +7,48 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.tminus1010.budgetvalue.GenViewHolder
+import com.tminus1010.budgetvalue.GenViewHolder2
 import com.tminus1010.budgetvalue.R
+import com.tminus1010.budgetvalue.databinding.FragErrorBinding
+import com.tminus1010.budgetvalue.databinding.ItemButtonBinding
 import com.tminus1010.budgetvalue.dependency_injection.ViewModelProviders
 import com.tminus1010.budgetvalue.dependency_injection.injection_extensions.appComponent
+import com.tminus1010.budgetvalue.layer_ui.misc.viewBinding
 import com.tminus1010.tmcommonkotlin.rx.extensions.observe
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import kotlinx.android.synthetic.main.frag_error.*
-import kotlinx.android.synthetic.main.item_button.view.*
 
 class ErrorFrag : Fragment(R.layout.frag_error), IViewModels {
     override val viewModelProviders by lazy { ViewModelProviders(requireActivity(), appComponent) }
+    val binding by viewBinding(FragErrorBinding::bind)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // # RecyclerView
-        recyclerview_buttons.apply {
+        binding.recyclerviewButtons.apply {
             layoutManager = GridLayoutManager(requireContext(), 2)
-            adapter = object : RecyclerView.Adapter<GenViewHolder>() {
-                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenViewHolder =
-                    LayoutInflater.from(requireContext()).inflate(R.layout.item_button, parent, false)
-                        .let { GenViewHolder(it) }
+            adapter = object : RecyclerView.Adapter<GenViewHolder2<ItemButtonBinding>>() {
+                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+                    ItemButtonBinding.inflate(LayoutInflater.from(requireContext()), parent, false)
+                        .let { GenViewHolder2(it) }
 
-                override fun onBindViewHolder(holder: GenViewHolder, position: Int) {
-                    holder.itemView.btn_item.setOnClickListener { errorVM.buttons.value[holder.adapterPosition].action() }
-                    holder.itemView.btn_item.text = errorVM.buttons.value[position].title
+                override fun onBindViewHolder(holder: GenViewHolder2<ItemButtonBinding>, position: Int) {
+                    holder.binding.btnItem.setOnClickListener { errorVM.buttons.value[holder.adapterPosition].action() }
+                    holder.binding.btnItem.text = errorVM.buttons.value[position].title
                 }
 
                 override fun getItemCount(): Int = errorVM.buttons.value.size
             }
         }
-        // # Observe
+        // # Message
         errorVM.message
             .observeOn(AndroidSchedulers.mainThread())
             .observe(viewLifecycleOwner) {
-                textview_message.text = it
+                binding.textviewMessage.text = it
             }
+        // # Buttons
         errorVM.buttons
             .observeOn(AndroidSchedulers.mainThread())
             .observe(viewLifecycleOwner) {
-                recyclerview_buttons.adapter?.notifyDataSetChanged()
+                binding.recyclerviewButtons.adapter?.notifyDataSetChanged()
             }
     }
 }
