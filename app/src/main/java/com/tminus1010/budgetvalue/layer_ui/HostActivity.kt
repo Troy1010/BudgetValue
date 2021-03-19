43ae8bd43ccd52c18a3fac00a32f83bac87e3559
Troy <troyswanson1010@gmail.com>
2021-03-18 23:57:34 -0500

    HostActivity`viewBinding

--- app/src/main/java/com/tminus1010/budgetvalue/layer_ui/HostActivity.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/layer_ui/HostActivity.kt
@@ -10,26 +10,28 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.tminus1010.budgetvalue.CODE_PICK_TRANSACTIONS_FILE
 import com.tminus1010.budgetvalue.R
+import com.tminus1010.budgetvalue.databinding.ActivityHostBinding
 import com.tminus1010.budgetvalue.dependency_injection.ViewModelProviders
 import com.tminus1010.budgetvalue.dependency_injection.injection_extensions.appComponent
 import com.tminus1010.budgetvalue.dependency_injection.injection_extensions.domain
 import com.tminus1010.budgetvalue.dependency_injection.injection_extensions.flavorIntersection
 import com.tminus1010.budgetvalue.extensions.add
+import com.tminus1010.budgetvalue.layer_ui.misc.viewBinding
 import com.tminus1010.tmcommonkotlin.view.extensions.toast
-import kotlinx.android.synthetic.main.activity_host.*
 
 class HostActivity : AppCompatActivity(), IViewModels {
     override val viewModelProviders by lazy { ViewModelProviders(this, appComponent) }
-    val hostFrag by lazy { fragNavHost as HostFrag }
+    val binding by viewBinding(ActivityHostBinding::inflate)
+    val hostFrag by lazy { supportFragmentManager.findFragmentById(R.id.fragNavHost) as HostFrag }
     val nav by lazy { findNavController(R.id.fragNavHost) }
     val menuItemPartials by lazy { flavorIntersection.getExtraMenuItemPartials(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         domain.appInit()
-        setContentView(R.layout.activity_host)
+        setContentView(binding.root)
         // # Bind bottom menu to navigation.
         // In order for NavigationUI.setupWithNavController to work, the ids in R.menu.* must exactly match R.navigation.*
-        NavigationUI.setupWithNavController(bottom_navigation, nav)
+        NavigationUI.setupWithNavController(binding.bottomNavigation, nav)
     }
 
     override fun onPrepareOptionsMenu(menu: Menu): Boolean {
