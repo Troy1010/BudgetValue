95c063d1c425839bb6b253ee01cbfeadad9ed000
Troy <troyswanson1010@gmail.com>
2021-03-22 21:28:14 -0500

    cleanup

--- app/src/main/java/com/tminus1010/budgetvalue/Constants.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/Constants.kt
@@ -2,7 +2,6 @@ package com.tminus1010.budgetvalue
 
 import com.tminus1010.budgetvalue.features.categories.Category
 
-const val CODE_PICK_TRANSACTIONS_FILE = 3486
 const val SHARED_PREF_FILE_NAME = "SharedPref"
 // * TODO("Give this to a VM, and allow the user to change it.")
 val categoryComparator = compareBy<Category>({ category -> category.type }, { category -> category.name })
