6457f26c7ebc1f40e23dc794f05279b2e5876293
Troy <troyswanson1010@gmail.com>
2021-03-23 17:35:50 -0500

    CategoryDeletionVM`intent

--- app/src/main/java/com/tminus1010/budgetvalue/features/categories/CategoryDeletionVM.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/features/categories/CategoryDeletionVM.kt
@@ -1,21 +1,26 @@
 package com.tminus1010.budgetvalue.features.categories
 
 import androidx.lifecycle.ViewModel
+import com.tminus1010.budgetvalue.extensions.launch
 import com.tminus1010.budgetvalue.features.plans.ActivePlanVM
 import com.tminus1010.budgetvalue.features.plans.PlanUseCases
 import com.tminus1010.budgetvalue.features_shared.Domain
 import com.tminus1010.budgetvalue.middleware.Rx
-import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.subjects.PublishSubject
 
 class CategoryDeletionVM(
     private val domain: Domain,
     private val planUseCases: PlanUseCases,
     private val activePlanVM: ActivePlanVM,
 ): ViewModel() {
-    fun deleteFromActive(category: Category): Completable =
-        Rx.merge(
-            activePlanVM.activePlan.flatMapCompletable { planUseCases.updatePlanCA(it, Pair(category, null)) },
-            domain.pushActiveReconciliationCA(Pair(category, null)),
-            domain.delete(category),
-        )
+    val intentDeleteCategoryFromActive = PublishSubject.create<Category>()
+        .also {
+            it.launch { category ->
+                Rx.merge(
+                    activePlanVM.activePlan.flatMapCompletable { planUseCases.updatePlanCA(it, Pair(category, null)) },
+                    domain.pushActiveReconciliationCA(Pair(category, null)),
+                    domain.delete(category),
+                )
+            }
+        }
 }
