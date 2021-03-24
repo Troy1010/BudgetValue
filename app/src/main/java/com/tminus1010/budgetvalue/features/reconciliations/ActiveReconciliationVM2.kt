f0437ebb102235bc120837bebaca44420d7e7cd9
Troy <troyswanson1010@gmail.com>
2021-03-23 19:53:32 -0500

    linting

--- app/src/main/java/com/tminus1010/budgetvalue/features/reconciliations/ActiveReconciliationVM2.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/features/reconciliations/ActiveReconciliationVM2.kt
@@ -1,11 +1,11 @@
 package com.tminus1010.budgetvalue.features.reconciliations
 
 import androidx.lifecycle.ViewModel
-import com.tminus1010.budgetvalue.middleware.Rx
 import com.tminus1010.budgetvalue.extensions.launch
-import com.tminus1010.budgetvalue.features_shared.Domain
 import com.tminus1010.budgetvalue.features.transactions.TransactionsVM
+import com.tminus1010.budgetvalue.features_shared.Domain
 import com.tminus1010.budgetvalue.features_shared.budgeted.BudgetedVM
+import com.tminus1010.budgetvalue.middleware.Rx
 import com.tminus1010.tmcommonkotlin.rx.extensions.toBehaviorSubject
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
