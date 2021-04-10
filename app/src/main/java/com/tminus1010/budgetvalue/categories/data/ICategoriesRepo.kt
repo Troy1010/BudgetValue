648531c876914eda495173b84731098c3b5ef4d2
Troy <troyswanson1010@gmail.com>
2021-04-10 00:51:12 -0500

    linting

--- app/src/main/java/com/tminus1010/budgetvalue/categories/data/ICategoriesRepo.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/categories/data/ICategoriesRepo.kt
@@ -3,7 +3,6 @@ package com.tminus1010.budgetvalue.categories.data
 import com.tminus1010.budgetvalue.categories.models.Category
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
-import java.math.BigDecimal
 
 interface ICategoriesRepo {
     fun fetchUserCategories(): Observable<List<Category>>
