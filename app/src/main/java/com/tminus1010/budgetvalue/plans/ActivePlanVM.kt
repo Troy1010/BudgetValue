b3e53f6ad5fe4d565986217b299657f51c17392b
Troy <troyswanson1010@gmail.com>
2021-04-10 22:22:47 -0500

    ActivePlanVM`bugfix

--- app/src/main/java/com/tminus1010/budgetvalue/plans/ActivePlanVM.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/plans/ActivePlanVM.kt
@@ -4,21 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.tminus1010.budgetvalue._core.extensions.launch
 import com.tminus1010.budgetvalue._core.extensions.toLiveData
-import com.tminus1010.budgetvalue._core.extensions.withLatestFrom2
-import com.tminus1010.budgetvalue._core.middleware.nullIfZero
 import com.tminus1010.budgetvalue._core.middleware.toMoneyBigDecimal
 import com.tminus1010.budgetvalue.categories.models.Category
 import com.tminus1010.budgetvalue.plans.data.IPlansRepo
 import com.tminus1010.budgetvalue.plans.domain.ActivePlanDomain
-import com.tminus1010.budgetvalue.plans.domain.IActivePlanDomain
-import com.tminus1010.budgetvalue.plans.models.Plan
-import com.tminus1010.tmcommonkotlin.misc.extensions.associate
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.subjects.BehaviorSubject
-import io.reactivex.rxjava3.subjects.PublishSubject
 import io.reactivex.rxjava3.subjects.Subject
-import java.math.BigDecimal
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,8 +19,6 @@ class ActivePlanVM @Inject constructor(
     private val activePlanDomain: ActivePlanDomain,
     private val plansRepo: IPlansRepo,
 ) : ViewModel() {
-    val intentPushActivePlanCA: PublishSubject<Pair<Category, BigDecimal?>> = TODO()
-
     // # State
     val defaultAmount: LiveData<String> = activePlanDomain.defaultAmount
         .map { it.toString() }
@@ -36,12 +26,14 @@ class ActivePlanVM @Inject constructor(
     val expectedIncome: LiveData<String> = activePlanDomain.expectedIncome
         .map { it.toString() }
         .toLiveData(errorSubject)
-    val activePlanCAs: Observable<Map<String, LiveData<String>>> = activePlanDomain.activePlanCAs
-        .map { it.associate { it.key.name to it.value.map { it.toString() }.toLiveData(errorSubject) } }
+    val activePlanCAs: Observable<Map<Category, LiveData<String>>> = activePlanDomain.activePlanCAs
+        .map { it.mapValues { it.value.map { it.toString() }.toLiveData(errorSubject) } }
+
     // # Intents
     fun pushExpectedIncome(s: String) {
         activePlanDomain.activePlan.launch { plansRepo.updatePlanAmount(it, s.toMoneyBigDecimal()) }
     }
+
     fun pushActivePlanCA(category: Category, s: String) {
         activePlanDomain.activePlan.launch { plansRepo.updatePlanCA(it, category, s.toMoneyBigDecimal()) }
     }
