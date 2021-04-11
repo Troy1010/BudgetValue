9ae666628eacd23af123286e31a48e8017875b49
Troy <troyswanson1010@gmail.com>
2021-04-11 17:56:00 -0500

    deprecation comments

--- app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/Rx.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/Rx.kt
@@ -144,6 +144,7 @@ object Rx {
     fun merge(vararg completables: Completable) = Completable.merge(completables.toList())
     fun merge(completables: List<Completable>): Completable = merge(*completables.toTypedArray())
 
+    @Deprecated("This is a combine latest or something, not a merge.")
     inline fun <reified A, reified B> merge(a: ObservableSource<A>, b: ObservableSource<B>): Observable<Pair<A?, B?>> {
         return Observable.merge(a, b)
             .map { Pair(it as? A, it as? B) }
