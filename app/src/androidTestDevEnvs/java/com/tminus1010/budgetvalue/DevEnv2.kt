338b794e761d12adba6cc881260e0754d48ad193
Troy <troyswanson1010@gmail.com>
2021-04-12 02:40:18 -0500

    DevEnv2`removed Print Spents, Print Transactions

--- app/src/androidTestDevEnvs/java/com/tminus1010/budgetvalue/DevEnv2.kt
+++ app/src/androidTestDevEnvs/java/com/tminus1010/budgetvalue/DevEnv2.kt
@@ -7,12 +7,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.tminus1010.budgetvalue._core.GetExtraMenuItemPartialsUC
 import com.tminus1010.budgetvalue._core.LaunchImportUC
 import com.tminus1010.budgetvalue._core.TestException
-import com.tminus1010.budgetvalue._core.middleware.getBlocks
-import com.tminus1010.budgetvalue._core.middleware.reflectXY
 import com.tminus1010.budgetvalue._core.middleware.ui.MenuItemPartial
 import com.tminus1010.budgetvalue._core.ui.HostActivity
 import com.tminus1010.budgetvalue._core.ui.MockImportSelectionActivity
-import com.tminus1010.tmcommonkotlin.core.logz
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -22,7 +19,6 @@ import dagger.hilt.components.SingletonComponent
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.math.BigDecimal
 import javax.inject.Singleton
 
 @HiltAndroidTest
@@ -52,58 +48,6 @@ class DevEnv2 {
                     },
                     MenuItemPartial("Throw Error") {
                         hostFrag.handle(Exception("Zip zoop an error"))
-                    },
-                    MenuItemPartial("Print Transactions") {
-                        transactionsVM.transactions.take(1)
-                            .subscribe { logz("transactions:${it?.joinToString(",")}") }
-                    },
-                    MenuItemPartial("Print Spends") {
-                        // define transactionBlocks
-                        val transactionBlocks = transactionsVM.transactions.blockingFirst().getBlocks(2)
-                        // define stringBlocks
-                        val stringBlocks = arrayListOf<HashMap<String, String>>()
-                        for (transactionBlock in transactionBlocks) {
-                            val curStringBlock = HashMap<String, String>()
-                            stringBlocks.add(curStringBlock)
-                            for (category in categoriesVM.userCategories.value) {
-                                curStringBlock[category.name] = transactionBlock.value
-                                    .map { it.categoryAmounts[category] ?: BigDecimal.ZERO }
-                                    .fold(BigDecimal.ZERO, BigDecimal::add)
-                                    .toString()
-                            }
-                        }
-                        //
-                        logz("stringBlocks:${stringBlocks}")
-                        logz("stringBlocks.reflectXY():${stringBlocks.reflectXY()}")
-                        val spends = HashMap<String, String>()
-                        for (x in stringBlocks.reflectXY()) {
-                            spends[x.key] = x.value.joinToString(",")
-                        }
-                        logz("spends:${spends}")
-                        //
-                        val column = listOf(
-                            "",
-                            "",
-                            "",
-                            spends["Default"] ?: "",
-                            "",
-                            "",
-                            spends["Food"] ?: "",
-                            spends["Drinks"] ?: "",
-                            spends["Vanity Food"] ?: "",
-                            spends["Improvements"] ?: "",
-                            spends["Dentist"] ?: "",
-                            spends["Diabetic Supplies"] ?: "",
-                            spends["Leli"] ?: "",
-                            spends["Misc"] ?: "",
-                            spends["Gas"] ?: "",
-                            "",
-                            spends["Vanity Food"] ?: "",
-                            spends["Emergency"] ?: ""
-                        )
-                        //
-                        val spendsString = column.joinToString("\n")
-                        logz("spendsString:${spendsString}")
                     },
                 )
             }
