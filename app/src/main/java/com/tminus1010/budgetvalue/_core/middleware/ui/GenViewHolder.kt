3289e2d9d13ebdcf05497a444ddd0702c6445e66
Troy <troyswanson1010@gmail.com>
2021-04-14 00:31:01 -0500

    cleanup

--- app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/ui/GenViewHolder.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/ui/GenViewHolder.kt
@@ -1,19 +1,9 @@
 package com.tminus1010.budgetvalue._core.middleware.ui
 
 import android.view.View
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LifecycleRegistry
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
 
 class GenViewHolder(v: View) : RecyclerView.ViewHolder(v)
-class GenViewHolder2<T : ViewBinding>(val vb: T) : RecyclerView.ViewHolder(vb.root), LifecycleOwner {
-    private val lifecycleRegistry = LifecycleRegistry(this)
-    override fun getLifecycle(): Lifecycle = lifecycleRegistry
-    fun onStart() { lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START) }
-    fun onStop() { lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP) }
-    fun onCreate() { lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE) }
-    fun onDestroy() { lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY) }
-}
+class GenViewHolder2<T : ViewBinding>(val vb: T) : RecyclerView.ViewHolder(vb.root)
