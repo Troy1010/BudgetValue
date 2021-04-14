36ccd2282c139eebeebd5b21166f358359423a5a
Troy <troyswanson1010@gmail.com>
2021-04-14 00:46:16 -0500

    cleanup flavor_contracts

--- app/src/main/java/com/tminus1010/budgetvalue/_core/GetExtraMenuItemPartialsUC.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/GetExtraMenuItemPartialsUC.kt
@@ -1,10 +1,9 @@
 package com.tminus1010.budgetvalue._core
 
-import com.tminus1010.budgetvalue._core.flavor_contracts.development_production.IGetExtraMenuItemPartialsUC
 import com.tminus1010.budgetvalue._core.middleware.ui.MenuItemPartial
 import com.tminus1010.budgetvalue._core.ui.HostActivity
 import javax.inject.Inject
 
-open class GetExtraMenuItemPartialsUC @Inject constructor() : IGetExtraMenuItemPartialsUC {
-    override operator fun invoke(hostActivity: HostActivity) = emptyArray<MenuItemPartial>()
+open class GetExtraMenuItemPartialsUC @Inject constructor() {
+    open operator fun invoke(hostActivity: HostActivity) = emptyArray<MenuItemPartial>()
 }
--- app/src/main/java/com/tminus1010/budgetvalue/_core/LaunchImportUC.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/LaunchImportUC.kt
@@ -1,12 +1,11 @@
 package com.tminus1010.budgetvalue._core
 
 import android.content.Intent
-import com.tminus1010.budgetvalue._core.flavor_contracts.development_production.ILaunchImportUC
 import com.tminus1010.budgetvalue._core.ui.HostActivity
 import javax.inject.Inject
 
-open class LaunchImportUC @Inject constructor() : ILaunchImportUC {
-    override operator fun invoke(hostActivity: HostActivity) {
+open class LaunchImportUC @Inject constructor() {
+    open operator fun invoke(hostActivity: HostActivity) {
         Intent().apply { type = "*/*"; action = Intent.ACTION_GET_CONTENT }
             .let { Intent.createChooser(it, "Select transactions csv") }
             .also { hostActivity.activityResultLauncher.launch(it) }
