4d04896dfa76c74ef3c1f9d84e05c189585e6f62
Troy <troyswanson1010@gmail.com>
2021-04-14 00:28:15 -0500

    LifecycleImbuedAdapter`more abstraction

--- app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/ui/LifecycleImbuedAdapter.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/ui/LifecycleImbuedAdapter.kt
@@ -5,12 +5,11 @@ import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
 
 // Experimental
-abstract class LifecycleImbuedAdapter<VB: ViewBinding> (
+abstract class LifecycleImbuedAdapter<VH: RecyclerView.ViewHolder> (
     private val parentLifecycleOwner: LifecycleOwner
-): RecyclerView.Adapter<GenViewHolder2<VB>>() {
+): RecyclerView.Adapter<VH>() {
     init {
         parentLifecycleOwner.lifecycle.addObserver(object: LifecycleObserver {
             @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
@@ -21,30 +20,30 @@ abstract class LifecycleImbuedAdapter<VB: ViewBinding> (
         })
     }
 
-    abstract fun onBindViewHolder(holder: GenViewHolder2<VB>, position: Int, lifecycleOwner: LifecycleOwner)
+    abstract fun onBindViewHolder(holder: VH, position: Int, lifecycleOwner: LifecycleOwner)
 
     var lifecycleMap = hashMapOf<Int, ExposedLifecycleOwner>()
 
-    override fun onBindViewHolder(holder: GenViewHolder2<VB>, position: Int) {
+    override fun onBindViewHolder(holder: VH, position: Int) {
         if (lifecycleMap[position] != null) error("Shouldn't this have already been cleared..?")
         lifecycleMap[position] = ExposedLifecycleOwner()
         lifecycleMap[position]!!.onCreate()
         onBindViewHolder(holder, position, lifecycleMap[position]!!)
     }
 
-    override fun onViewRecycled(holder: GenViewHolder2<VB>) {
+    override fun onViewRecycled(holder: VH) {
         super.onViewRecycled(holder)
         lifecycleMap[holder.adapterPosition]!!.onDestroy()
         lifecycleMap.remove(holder.adapterPosition)
     }
 
-    override fun onViewAttachedToWindow(holder: GenViewHolder2<VB>) {
+    override fun onViewAttachedToWindow(holder: VH) {
         super.onViewAttachedToWindow(holder)
         lifecycleMap[holder.adapterPosition]!!.onStart()
         lifecycleMap[holder.adapterPosition]!!.onResume()
     }
 
-    override fun onViewDetachedFromWindow(holder: GenViewHolder2<VB>) {
+    override fun onViewDetachedFromWindow(holder: VH) {
         super.onViewDetachedFromWindow(holder)
         lifecycleMap[holder.adapterPosition]!!.onPause()
         lifecycleMap[holder.adapterPosition]!!.onStop()
