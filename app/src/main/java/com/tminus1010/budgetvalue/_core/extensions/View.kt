2ad67825387fe7dc9255d0cfcb95387475dd013f
Troy <troyswanson1010@gmail.com>
2021-04-17 18:15:57 -0500

    View.bind

--- app/src/main/java/com/tminus1010/budgetvalue/_core/extensions/View.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/extensions/View.kt
@@ -1,8 +1,22 @@
 package com.tminus1010.budgetvalue._core.extensions
 
 import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
+
+fun <V : View, T> V.bind(liveData: LiveData<T>, lifecycle: LifecycleOwner? = null, lambda: V.(T) -> Unit, ) {
+    val _lifecycle = lifecycle ?: findViewTreeLifecycleOwner()
+        ?: error("Could not find lifecycle. This might happen in Recyclerviews or other unattached views.\nEither attach the view if you can, or otherwise specify a lifecycle as argument.")
+    liveData.observe(_lifecycle) { lambda(it) }
+}
+
+fun TextView.bindText(liveData: LiveData<String>) {
+    bind(liveData) { easyText = it }
+}
 
 fun View.widthObservable(): Observable<Int> {
     return Observable.create<Int> { downstream ->
