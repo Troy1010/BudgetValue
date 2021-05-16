4956b1362763feefc269de2d46c06a8a4ab7693b
Troy <troyswanson1010@gmail.com>
2021-05-16 14:05:40 -0500

    cleanup

--- app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/ui/GenViewHolder.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/ui/GenViewHolder.kt
@@ -1,10 +1,6 @@
 package com.tminus1010.budgetvalue._core.middleware.ui
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
-
-class GenViewHolder(v: View) : RecyclerView.ViewHolder(v)
 class GenViewHolder2<T : ViewBinding>(val vb: T) : RecyclerView.ViewHolder(vb.root)
-class GenViewHolder3<T : ViewBinding>(val vb: T) : LifecycleViewHolder(vb.root)
