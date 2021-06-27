80a02ed8b9977bc1875d13faba2e0686dfcb9f1c
Troy <troyswanson1010@gmail.com>
2021-06-27 03:14:44 -0500

    SourceArrayList`not exposing as much mutability

--- app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/source_objects/SourceArrayList.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/source_objects/SourceArrayList.kt
@@ -1,47 +1,49 @@
 package com.tminus1010.budgetvalue._core.middleware.source_objects
 
-import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 
 class SourceArrayList<T> : ArrayList<T>() {
-    val observable = PublishSubject.create<ArrayList<T>>()
+    private val subject = BehaviorSubject.createDefault<List<T>>(emptyList())
+    val observable: Observable<List<T>> = subject
 
     override fun clear() {
         super.clear()
-        observable.onNext(this)
+        subject.onNext(this)
     }
 
     override fun addAll(elements: Collection<T>): Boolean {
         val x = super.addAll(elements)
-        observable.onNext(this)
+        subject.onNext(this)
         return x
     }
 
     override fun addAll(index: Int, elements: Collection<T>): Boolean {
         val x = super.addAll(index, elements)
-        observable.onNext(this)
+        subject.onNext(this)
         return x
     }
 
     override fun removeAll(elements: Collection<T>): Boolean {
         val x = super.removeAll(elements)
-        observable.onNext(this)
+        subject.onNext(this)
         return x
     }
 
     override fun add(element: T): Boolean {
         val x = super.add(element)
-        observable.onNext(this)
+        subject.onNext(this)
         return x
     }
 
     override fun add(index: Int, element: T) {
         super.add(index, element)
-        observable.onNext(this)
+        subject.onNext(this)
     }
 
     override fun removeAt(index: Int): T {
         val x = super.removeAt(index)
-        observable.onNext(this)
+        subject.onNext(this)
         return x
     }
 
@@ -52,19 +54,19 @@ class SourceArrayList<T> : ArrayList<T>() {
             } catch (e: NoSuchElementException) {
                 null
             }
-        observable.onNext(this)
+        subject.onNext(this)
         return x
     }
 
     override fun remove(element: T): Boolean {
         val x = super.remove(element)
-        observable.onNext(this)
+        subject.onNext(this)
         return x
     }
 
     override fun retainAll(elements: Collection<T>): Boolean {
         val x = super.retainAll(elements)
-        observable.onNext(this)
+        subject.onNext(this)
         return x
     }
 }
