06b32a14453c6d3b867ea5360a30dcd04073ec04
Troy <troyswanson1010@gmail.com>
2021-07-04 14:20:07 -0500

    formatting

--- app/src/main/java/com/tminus1010/budgetvalue/_core/ui/HostActivity.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/ui/HostActivity.kt
@@ -25,7 +25,7 @@ class HostActivity : AppCompatActivity() {
     private val vb by viewBinding(ActivityHostBinding::inflate)
     @Inject lateinit var getExtraMenuItemPartialsUC: GetExtraMenuItemPartialsUC
     @Inject lateinit var appInitDomain: AppInitDomain
-    private val transactionsVM by viewModels<TransactionsVM>()
+    private val transactionsVM: TransactionsVM by viewModels()
     val hostFrag by lazy { supportFragmentManager.findFragmentById(R.id.frag_nav_host) as HostFrag }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
