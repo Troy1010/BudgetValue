6c505508b245f98286cb212db8ef93785d6731a9
Troy <troyswanson1010@gmail.com>
2021-07-05 13:08:36 -0500

    .subscribeOn(Schedulers.io())

--- app/src/main/java/com/tminus1010/budgetvalue/_core/data/MainRepo.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/data/MainRepo.kt
@@ -32,58 +32,79 @@ class MainRepo @Inject constructor(
 ) : IMainRepo, ICategoriesRepo by categoriesRepo {
     override fun fetchAccounts(): Observable<List<Account>> =
         miscDAO.fetchAccounts().map { it.map { Account.fromDTO(it) } }
+            .subscribeOn(Schedulers.io())
 
     override fun update(account: Account): Completable =
         miscDAO.updateAccount(account.toDTO())
+            .subscribeOn(Schedulers.io())
 
     override fun push(account: Account): Completable =
         miscDAO.addAccount(account.toDTO())
+            .subscribeOn(Schedulers.io())
 
     override fun delete(account: Account): Completable =
         miscDAO.deleteAccount(account.toDTO())
+            .subscribeOn(Schedulers.io())
 
     override fun fetchAppInitBool(): Boolean =
         sharedPrefWrapper.fetchAppInitBool()
 
     override fun pushAppInitBool(appInitBool: Boolean) =
         sharedPrefWrapper.pushAppInitBool(appInitBool)
+            .subscribeOn(Schedulers.io())
 
     override fun pushActiveReconciliationCA(kv: Pair<Category, BigDecimal?>): Completable =
         sharedPrefWrapper.pushActiveReconciliationCA(Pair(kv.first.name, kv.second.toString()))
+            .subscribeOn(Schedulers.io())
 
     override val anchorDateOffset: Observable<Long> =
         sharedPrefWrapper.anchorDateOffset
+            .subscribeOn(Schedulers.io())
 
     override fun pushAnchorDateOffset(anchorDateOffset: Long?): Completable =
         sharedPrefWrapper.pushAnchorDateOffset(anchorDateOffset)
+            .subscribeOn(Schedulers.io())
 
     override val blockSize: Observable<Long> =
         sharedPrefWrapper.blockSize
+            .subscribeOn(Schedulers.io())
 
     override fun pushBlockSize(blockSize: Long?): Completable =
         sharedPrefWrapper.pushBlockSize(blockSize)
+            .subscribeOn(Schedulers.io())
 
-    override fun pushReconciliationCA(reconciliation: Reconciliation, category: Category, amount: BigDecimal?, ) =
+    override fun pushReconciliationCA(reconciliation: Reconciliation, category: Category, amount: BigDecimal?): Completable =
         reconciliation.categoryAmounts
             .toMutableMap()
-            .apply { if (amount==null) remove(category) else put(category, amount) }
-            .let { miscDAO.updateReconciliationCategoryAmounts(reconciliation.id, it.mapKeys { it.key.name }) }
+            .apply { if (amount == null) remove(category) else put(category, amount) }
+            .let {
+                miscDAO.updateReconciliationCategoryAmounts(
+                    reconciliation.id,
+                    it.mapKeys { it.key.name })
+                    .subscribeOn(Schedulers.io())
+            }
 
-    override fun clearReconciliations() = miscDAO.clearReconciliations()
+    override fun clearReconciliations() =
+        miscDAO.clearReconciliations()
+            .subscribeOn(Schedulers.io())
 
     override fun push(reconciliation: Reconciliation): Completable =
         miscDAO.add(reconciliation.toDTO(categoryAmountsConverter))
+            .subscribeOn(Schedulers.io())
 
     override fun delete(reconciliation: Reconciliation): Completable =
         miscDAO.delete(reconciliation.toDTO(categoryAmountsConverter))
+            .subscribeOn(Schedulers.io())
 
     override fun delete(plan: Plan): Completable =
         miscDAO.delete(plan.toDTO(categoryAmountsConverter))
+            .subscribeOn(Schedulers.io())
 
     override val reconciliations: Observable<List<Reconciliation>> =
         miscDAO.fetchReconciliations()
             .map { it.map { Reconciliation.fromDTO(it, categoryAmountsConverter) } }
             .replay(1).refCount()
+            .subscribeOn(Schedulers.io())
 
     override val activeReconciliationCAs: Observable<Map<Category, BigDecimal>> =
         sharedPrefWrapper.activeReconciliationCAs
@@ -123,9 +144,11 @@ class MainRepo @Inject constructor(
 
     override fun tryPush(transactions: List<Transaction>): Completable =
         miscDAO.tryAdd(transactions.map { it.toDTO(categoryAmountsConverter) })
+            .subscribeOn(Schedulers.io())
 
     override fun findTransactionsWithDescription(description: String): Single<List<Transaction>> =
         miscDAO.fetchTransactions(description).map { it.map { Transaction.fromDTO(it, categoryAmountsConverter) } }
+            .subscribeOn(Schedulers.io())
 
     override fun getTransaction(id: String): Single<Transaction> =
         miscDAO.getTransaction(id)
@@ -134,9 +157,11 @@ class MainRepo @Inject constructor(
 
     override val plans: Observable<List<Plan>> =
         miscDAO.fetchPlans().map { it.map { Plan.fromDTO(it, categoryAmountsConverter) } }
+            .subscribeOn(Schedulers.io())
 
     override fun pushPlan(plan: Plan): Completable =
         miscDAO.add(plan.toDTO(categoryAmountsConverter))
+            .subscribeOn(Schedulers.io())
 
     override fun updatePlanCA(plan: Plan, category: Category, amount: BigDecimal?): Completable =
         plan.categoryAmounts
