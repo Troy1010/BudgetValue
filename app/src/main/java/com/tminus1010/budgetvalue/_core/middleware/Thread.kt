0468d2d3f9a60e92468e8123fd71fb4ff8457178
Troy <troyswanson1010@gmail.com>
2021-07-05 13:53:50 -0500

    cleanup

--- app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/Thread.kt
+++
@@ -1,6 +0,0 @@
-package com.tminus1010.budgetvalue._core.middleware
-
-fun logt(prefix: Any? = "") {
-    val prefixRedef = if (prefix != "") "$prefix`" else prefix
-    logz("${prefixRedef}Current Thread:${Thread.currentThread().name}")
-}
