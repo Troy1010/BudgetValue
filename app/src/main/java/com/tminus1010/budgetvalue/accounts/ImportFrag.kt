5d42cc8497734e304ff923e2ca69ae1c27d5b0bb
Troy <troyswanson1010@gmail.com>
2021-07-07 00:21:09 -0500

    ImportFrag using recyclerview for buttons

--- app/src/main/java/com/tminus1010/budgetvalue/accounts/ImportFrag.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/accounts/ImportFrag.kt
@@ -1,27 +1,37 @@
 package com.tminus1010.budgetvalue.accounts
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.thekhaeng.recyclerviewmargin.LayoutMarginDecoration
 import com.tminus1010.budgetvalue.R
 import com.tminus1010.budgetvalue._core.LaunchImportUC
-import com.tminus1010.budgetvalue._core.middleware.ui.*
+import com.tminus1010.budgetvalue._core.middleware.ui.ButtonRVItem
+import com.tminus1010.budgetvalue._core.middleware.ui.GenViewHolder2
+import com.tminus1010.budgetvalue._core.middleware.ui.LifecycleRVAdapter
+import com.tminus1010.budgetvalue._core.middleware.ui.viewBinding
 import com.tminus1010.budgetvalue._core.ui.HostActivity
 import com.tminus1010.budgetvalue._core.ui.data_binding.bind
+import com.tminus1010.budgetvalue._core.ui.data_binding.bindButtonRVItem
 import com.tminus1010.budgetvalue.accounts.models.Account
 import com.tminus1010.budgetvalue.databinding.FragImportBinding
 import com.tminus1010.budgetvalue.databinding.ItemAccountBinding
+import com.tminus1010.budgetvalue.databinding.ItemButtonBinding
 import com.tminus1010.tmcommonkotlin.rx.extensions.observe
+import com.tminus1010.tmcommonkotlin.view.extensions.toPX
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class ImportFrag : Fragment(R.layout.frag_import) {
-    @Inject lateinit var launchImportUC: LaunchImportUC
+    @Inject
+    lateinit var launchImportUC: LaunchImportUC
     private val accountsVM: AccountsVM by activityViewModels()
     private val vb by viewBinding(FragImportBinding::bind)
     var accounts = emptyList<Account>()
@@ -30,12 +40,12 @@ class ImportFrag : Fragment(R.layout.frag_import) {
             field = value
             if (shouldNotifyDataSetChanged) vb.recyclerviewAccounts.adapter?.notifyDataSetChanged()
         }
+    var btns = emptyList<ButtonRVItem>()
+        set(value) { field = value.reversed(); vb.recyclerviewButtons.adapter?.notifyDataSetChanged() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // # Clicks
-        vb.btnImport.setOnClickListener { launchImportUC(requireActivity() as HostActivity) }
-        vb.btnAddAccount.setOnClickListener { accountsVM.addAccount() }
-        // # RecyclerView
+        // # Accounts RecyclerView
         accountsVM.accounts.observe(viewLifecycleOwner) { accounts = it }
         vb.recyclerviewAccounts.apply {
             layoutManager = LinearLayoutManager(requireActivity())
@@ -43,11 +53,37 @@ class ImportFrag : Fragment(R.layout.frag_import) {
                 override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
                     ItemAccountBinding.inflate(layoutInflater, parent, false)
                         .let { GenViewHolder2(it) }
+
                 override fun getItemCount() = accounts.size
                 override fun onBindViewHolder(holder: GenViewHolder2<ItemAccountBinding>, position: Int) {
                     holder.vb.bind(accounts[holder.adapterPosition], accountsVM)
                 }
             }
         }
+
+        // # Button RecyclerView
+        vb.recyclerviewButtons.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, true)
+        vb.recyclerviewButtons.addItemDecoration(LayoutMarginDecoration(8.toPX(requireContext())))
+        vb.recyclerviewButtons.adapter = object : LifecycleRVAdapter<GenViewHolder2<ItemButtonBinding>>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+                ItemButtonBinding.inflate(LayoutInflater.from(requireContext()), parent, false)
+                    .let { GenViewHolder2(it) }
+
+            override fun onViewAttachedToWindow(holder: GenViewHolder2<ItemButtonBinding>, lifecycle: LifecycleOwner) {
+                holder.vb.btnItem.bindButtonRVItem(lifecycle, btns[holder.adapterPosition])
+            }
+
+            override fun getItemCount() = btns.size
+        }
+        btns = listOfNotNull(
+            ButtonRVItem(
+                title = "Import",
+                onClick = { launchImportUC(requireActivity() as HostActivity) },
+            ),
+            ButtonRVItem(
+                title = "Add Account",
+                onClick = { accountsVM.addAccount() }
+            ),
+        )
     }
 }
