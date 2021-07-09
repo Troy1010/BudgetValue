8b5f01ea333647cc77c33a74599d5e673a6083e9
Troy <troyswanson1010@gmail.com>
2021-07-09 14:33:34 -0500

    no ICategoryAmountsConverter

--- app/src/main/java/com/tminus1010/budgetvalue/categories/CategoryAmountsConverter.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/categories/CategoryAmountsConverter.kt
@@ -11,13 +11,13 @@ import javax.inject.Inject
 class CategoryAmountsConverter @Inject constructor(
     private val categoryParser: ICategoryParser,
     private val moshi: Moshi
-) : ICategoryAmountsConverter {
-    override fun toCategoryAmounts(s: String?): Map<Category, BigDecimal> =
+) {
+    fun toCategoryAmounts(s: String?): Map<Category, BigDecimal> =
         if (s == null) emptyMap() else
             moshi.fromJson<Map<String, String>>(s)
                 .associate { categoryParser.parseCategory(it.key) to (it.value as Any).toString().toBigDecimal() }
 
-    override fun toJson(categoryAmounts: Map<Category, BigDecimal>): String =
+    fun toJson(categoryAmounts: Map<Category, BigDecimal>): String =
         categoryAmounts
             .associate { it.key.name to it.value.toString() }
             .let { moshi.toJson(it) }
--- app/src/main/java/com/tminus1010/budgetvalue/categories/ICategoryAmountsConverter.kt
+++
@@ -1,9 +0,0 @@
-package com.tminus1010.budgetvalue.categories
-
-import com.tminus1010.budgetvalue.categories.models.Category
-import java.math.BigDecimal
-
-interface ICategoryAmountsConverter {
-    fun toCategoryAmounts(s: String?): Map<Category, BigDecimal>
-    fun toJson(categoryAmounts: Map<Category, BigDecimal>): String
-}
