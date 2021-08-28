10837c46993238f9d3fe837e29839fd00e79500f
Troy <troyswanson1010@gmail.com>
2021-08-28 12:07:28 -0500

    calcActiveReconciliationDefaultAmount uses direct combineLatest args

--- app/src/main/java/com/tminus1010/budgetvalue/reconciliations/domain/ActiveReconciliationDefaultAmountUC.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/reconciliations/domain/ActiveReconciliationDefaultAmountUC.kt
@@ -1,12 +1,14 @@
 package com.tminus1010.budgetvalue.reconciliations.domain
 
 import androidx.annotation.VisibleForTesting
-import com.tminus1010.budgetvalue._core.middleware.Rx
 import com.tminus1010.budgetvalue._core.models.CategoryAmounts
 import com.tminus1010.budgetvalue.accounts.domain.AccountsDomain
 import com.tminus1010.budgetvalue.plans.data.PlansRepo
+import com.tminus1010.budgetvalue.plans.models.Plan
 import com.tminus1010.budgetvalue.reconciliations.data.ReconciliationsRepo
+import com.tminus1010.budgetvalue.reconciliations.models.Reconciliation
 import com.tminus1010.budgetvalue.transactions.domain.TransactionsDomain
+import com.tminus1010.budgetvalue.transactions.models.TransactionBlock
 import com.tminus1010.tmcommonkotlin.misc.extensions.sum
 import com.tminus1010.tmcommonkotlin.rx.nonLazy
 import com.tminus1010.tmcommonkotlin.rx.replayNonError
@@ -24,16 +26,14 @@ class ActiveReconciliationDefaultAmountUC @Inject constructor(
     accountsDomain: AccountsDomain,
 ) {
     private val defaultAmount =
-        Rx.combineLatest(
+        Observable.combineLatest(
             plansRepo.plans,
             reconciliationsRepo.reconciliations,
             transactionsDomain.transactionBlocks,
             accountsDomain.accountsTotal,
             reconciliationsRepo.activeReconciliationCAs,
+            ::calcActiveReconciliationDefaultAmount
         )
-            .map { (plans, reconciliations, transactionBlocks, accountsTotal, activeReconciliationCAs) ->
-                calcActiveReconciliationDefaultAmount(accountsTotal, (plans + reconciliations + transactionBlocks).map { it.totalAmount() }, activeReconciliationCAs)
-            }
             .replayNonError(1).nonLazy()
 
     operator fun invoke(): Observable<BigDecimal> = defaultAmount
@@ -43,7 +43,8 @@ class ActiveReconciliationDefaultAmountUC @Inject constructor(
          * Take a look at ManualCalculationsForTests for clarification about this.
          */
         @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-        fun calcActiveReconciliationDefaultAmount(accountsTotal: BigDecimal, historyTotalAmounts: Iterable<BigDecimal>, activeReconciliationCAs: CategoryAmounts): BigDecimal {
+        fun calcActiveReconciliationDefaultAmount(plans: List<Plan>, reconciliations: List<Reconciliation>, transactionBlocks: List<TransactionBlock>, accountsTotal: BigDecimal, activeReconciliationCAs: CategoryAmounts): BigDecimal {
+            val historyTotalAmounts = (plans + reconciliations + transactionBlocks).map { it.totalAmount() }
             return activeReconciliationCAs.defaultAmount(accountsTotal - historyTotalAmounts.sum())
         }
     }
--- app/src/test/java/com/tminus1010/budgetvalue/reconciliations/domain/ActiveReconciliationDefaultAmountUCTestRGR.kt
+++ app/src/test/java/com/tminus1010/budgetvalue/reconciliations/domain/ActiveReconciliationDefaultAmountUCTestRGR.kt
@@ -2,6 +2,11 @@ package com.tminus1010.budgetvalue.reconciliations.domain
 
 import com.tminus1010.budgetvalue._core.models.CategoryAmounts
 import com.tminus1010.budgetvalue._shared.app_init.AppInitDomain
+import com.tminus1010.budgetvalue.plans.models.Plan
+import com.tminus1010.budgetvalue.reconciliations.models.Reconciliation
+import com.tminus1010.budgetvalue.transactions.models.TransactionBlock
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.math.BigDecimal
@@ -11,21 +16,27 @@ class ActiveReconciliationDefaultAmountUCTestRGR {
     fun test() {
         // # Given
         val givenAccountsTotal = BigDecimal("500.00")
-        val givenHistoryDefaultAmounts = listOf(
-            BigDecimal("75"),
-            BigDecimal("217"),
-            BigDecimal("43"),
-            BigDecimal("93"),
-            BigDecimal("-110"),
-            BigDecimal("-16"),
+        val givenPlans = listOf<Plan>(
+            mockk { every { totalAmount() } returns BigDecimal("75") },
+        )
+        val givenReconciliations = listOf<Reconciliation>(
+            mockk { every { totalAmount() } returns BigDecimal("217") },
+            mockk { every { totalAmount() } returns BigDecimal("43") },
+            mockk { every { totalAmount() } returns BigDecimal("93") },
+        )
+        val givenTransactionBlocks = listOf<TransactionBlock>(
+            mockk { every { totalAmount() } returns BigDecimal("-110") },
+            mockk { every { totalAmount() } returns BigDecimal("-16") },
         )
         val givenActiveReconciliationCAs = CategoryAmounts(
             AppInitDomain.initCategories[0] to BigDecimal("9"),
         )
         // # When
         val actual = ActiveReconciliationDefaultAmountUC.calcActiveReconciliationDefaultAmount(
+            plans = givenPlans,
+            reconciliations = givenReconciliations,
+            transactionBlocks = givenTransactionBlocks,
             accountsTotal = givenAccountsTotal,
-            historyTotalAmounts = givenHistoryDefaultAmounts,
             activeReconciliationCAs = givenActiveReconciliationCAs,
         )
         // # Then
