a1a0fbaca8191a9198eb44a52b3a14ee8333ac08
Troy <troyswanson1010@gmail.com>
2021-08-28 23:22:01 -0500

    CreateFutureFrag`progress

--- app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/CreateFutureFrag.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/CreateFutureFrag.kt
@@ -3,19 +3,26 @@ package com.tminus1010.budgetvalue.replay_or_future
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import com.tminus1010.budgetvalue.R
 import com.tminus1010.budgetvalue._core.middleware.ui.viewBinding
 import com.tminus1010.budgetvalue.databinding.FragCreateFutureBinding
+import com.tminus1010.tmcommonkotlin.rx.extensions.observe
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class CreateFutureFrag : Fragment(R.layout.frag_create_future) {
     private val vb by viewBinding(FragCreateFutureBinding::bind)
+    val createFutureVM: CreateFutureVM by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        createFutureVM.categoryAmountFormulaVMItems
+        createFutureVM.fillCategory.observe(viewLifecycleOwner) {
+//            vb.
+        }
     }
 
     companion object {
