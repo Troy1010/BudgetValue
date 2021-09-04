aea8a5cb7d71aad83cd25696b883a3bee406d610
Troy <troyswanson1010@gmail.com>
2021-09-04 13:20:37 -0500

    comments, linting

--- app/src/main/java/com/tminus1010/budgetvalue/_core/extensions/Fragment.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/extensions/Fragment.kt
@@ -2,13 +2,13 @@ package com.tminus1010.budgetvalue._core.extensions
 
 import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
 import com.tminus1010.budgetvalue._core.middleware.ui.tmTableView3.ViewItemRecipe3_
 import kotlin.reflect.full.functions
 
 @Suppress("UNCHECKED_CAST")
 inline fun <reified VB : ViewBinding, D : Any?> Fragment.viewItemRecipe(noinline bind: (D, VB) -> Unit, d: D): ViewItemRecipe3_<VB, D> {
+    // TODO("this type of reflection will fail after obfuscation")
     val inflate = VB::class.functions.find { it.name == "inflate" }!! as (LayoutInflater) -> VB
     return ViewItemRecipe3_(requireContext(), inflate, bind, d)
 }
