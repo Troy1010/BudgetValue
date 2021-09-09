620322b25c65c34b16aee98a76d93eacf33e4954
Troy <troyswanson1010@gmail.com>
2021-09-09 00:31:09 -0500

    ReplaysFrag`navigates to Replay on click

--- app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/ReplaysFrag.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/ReplaysFrag.kt
@@ -1,19 +1,16 @@
 package com.tminus1010.budgetvalue.replay_or_future
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import com.tminus1010.budgetvalue.R
-import com.tminus1010.budgetvalue._core.extensions.easyText
-import com.tminus1010.budgetvalue._core.middleware.ui.tmTableView3.IViewItemRecipe3
-import com.tminus1010.budgetvalue._core.middleware.ui.tmTableView3.ViewItemRecipeFactory3
+import com.tminus1010.budgetvalue._core.middleware.ui.recipe_factories.itemTextViewRB
 import com.tminus1010.budgetvalue._core.middleware.ui.viewBinding
 import com.tminus1010.budgetvalue.databinding.FragReplaysBinding
-import com.tminus1010.budgetvalue.databinding.ItemTextViewBinding
-import com.tminus1010.budgetvalue.replay_or_future.models.IReplay
+import com.tminus1010.budgetvalue.replay_or_future.models.BasicReplay
+import com.tminus1010.budgetvalue.transactions.ui.ReplayFrag
 import com.tminus1010.tmcommonkotlin.rx.extensions.observe
 import com.tminus1010.tmcommonkotlin.view.extensions.nav
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,19 +26,16 @@ class ReplaysFrag : Fragment(R.layout.frag_replays) {
         //
         replaysVM.navUp.observe(viewLifecycleOwner) { nav.navigateUp() }
         // # TMTableView
-        val clickableTextViewRecipeFactory = ViewItemRecipeFactory3<ItemTextViewBinding, IReplay>(
-            { ItemTextViewBinding.inflate(LayoutInflater.from(requireContext())) },
-            { replay, vb, _ ->
-                vb.root.setOnClickListener { replaysVM.userAddSearchTextToReplay(replay) }
-                vb.textview.easyText = replay.name
-            }
-        )
         replaysVM.replays
             .observeOn(Schedulers.computation())
             .map { replays ->
                 replays.map { replay ->
-                    listOf<IViewItemRecipe3>(
-                        clickableTextViewRecipeFactory.createOne(replay),
+                    listOf(
+                        itemTextViewRB().create(replay.name) {
+                            // TODO("Duct-tape solution b/c ReplayFrag needs categorySelectionVM")
+                            nav.navigate(R.id.categorizeNestedGraph)
+                            ReplayFrag.navTo(nav, replay as BasicReplay)
+                        }
                     )
                 }
             }
