dfa98167a31f8013064261596878239ae1fb4125
Troy <troyswanson1010@gmail.com>
2021-09-11 13:29:39 -0500

    formatting

--- app/src/main/java/com/tminus1010/budgetvalue/plans/domain/ActivePlanDomain.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/plans/domain/ActivePlanDomain.kt
@@ -18,41 +18,43 @@ class ActivePlanDomain @Inject constructor(
     plansRepo: PlansRepo,
     datePeriodGetter: DatePeriodGetter,
 ) : ViewModel() {
-    val activePlan = plansRepo.plans
-        .flatMap {
-            // If the last plan is a valid active plan, use that. Otherwise, copy some of the last plan's properties if it exists or create a new one, and push it.
-            val lastPlan = it.lastOrNull()
-            if (lastPlan != null && lastPlan.localDatePeriod == datePeriodGetter.currentDatePeriod())
-                Observable.just(lastPlan)
-            else {
-                when {
-                    lastPlan != null ->
-                        Observable.just(
-                            Plan(
-                                datePeriodGetter.currentDatePeriod(),
-                                lastPlan.amount,
-                                lastPlan.categoryAmounts
+    val activePlan =
+        plansRepo.plans
+            .flatMap {
+                // If the last plan is a valid active plan, use that. Otherwise, copy some of the last plan's properties if it exists or create a new one, and push it.
+                val lastPlan = it.lastOrNull()
+                if (lastPlan != null && lastPlan.localDatePeriod == datePeriodGetter.currentDatePeriod())
+                    Observable.just(lastPlan)
+                else {
+                    when {
+                        lastPlan != null ->
+                            Observable.just(
+                                Plan(
+                                    datePeriodGetter.currentDatePeriod(),
+                                    lastPlan.amount,
+                                    lastPlan.categoryAmounts
+                                )
                             )
-                        )
-                    else ->
-                        Observable.just(
-                            Plan(
-                                datePeriodGetter.currentDatePeriod(),
-                                BigDecimal.ZERO,
-                                emptyMap()
+                        else ->
+                            Observable.just(
+                                Plan(
+                                    datePeriodGetter.currentDatePeriod(),
+                                    BigDecimal.ZERO,
+                                    emptyMap()
+                                )
                             )
-                        )
-                }.doOnNext { plansRepo.pushPlan(it).blockingAwait() }
+                    }.doOnNext { plansRepo.pushPlan(it).blockingAwait() }
+                }
             }
-        }
-        .replay(1).refCount()
+            .replay(1).refCount()
     val activePlanCAs =
         activePlan.map { it.categoryAmounts }
             .flatMapSourceHashMap(SourceHashMap(exitValue = BigDecimal.ZERO))
             { it.itemObservableMap }
             .replay(1).refCount()
-    val expectedIncome = activePlan.map { it.amount }
-        .distinctUntilChanged()
+    val expectedIncome =
+        activePlan.map { it.amount }
+            .distinctUntilChanged()
     val defaultAmount =
         Observable.combineLatest(expectedIncome, activePlanCAs.switchMap { it.values.total() })
         { expectedIncome, activePlanTotal ->
