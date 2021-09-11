49f6ef89dca4e4349a91529b3e076963547d58cd
Troy <troyswanson1010@gmail.com>
2021-09-11 13:27:02 -0500

    IsPlanEnabled`onChangeToTrue

--- app/src/main/java/com/tminus1010/budgetvalue/_shared/feature_flags/IsPlanEnabled.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_shared/feature_flags/IsPlanEnabled.kt
@@ -1,11 +1,26 @@
 package com.tminus1010.budgetvalue._shared.feature_flags
 
+import com.tminus1010.budgetvalue._core.extensions.isZero
+import com.tminus1010.budgetvalue.transactions.domain.TransactionsDomain
+import com.tminus1010.tmcommonkotlin.rx.extensions.pairwise
 import io.reactivex.rxjava3.core.Observable
 import javax.inject.Inject
 
-class IsPlanEnabled @Inject constructor() {
+class IsPlanEnabled @Inject constructor(
+    private val transactionsDomain: TransactionsDomain,
+) {
     operator fun invoke(): Observable<Boolean> {
-        return Observable.just(false)
+        return transactionsDomain.transactionBlocks
+            .map {
+                it
+                    .filter { it.defaultAmount.isZero }
+                    .size > 2
+            }
             .distinctUntilChanged()
     }
+
+    val onChangeToTrue = invoke()
+        .pairwise()
+        .filter { it.second }
+        .map { Unit }
 }
