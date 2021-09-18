9d2a5f8f43f8da50a27196512d8d48b09da37e7a
Troy <troyswanson1010@gmail.com>
2021-09-17 19:41:20 -0500

    Menu.items, Menu.unCheckAllMenuItems()

--- app/src/main/java/com/tminus1010/budgetvalue/_core/extensions/Menu.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/extensions/Menu.kt
@@ -1,6 +1,7 @@
 package com.tminus1010.budgetvalue._core.extensions
 
 import android.view.Menu
+import androidx.core.view.iterator
 import com.tminus1010.budgetvalue._core.middleware.ui.MenuVMItem
 
 fun Menu.add(vararg menuVMItems: MenuVMItem) {
@@ -9,4 +10,15 @@ fun Menu.add(vararg menuVMItems: MenuVMItem) {
 
 fun Menu.add(menuVMItems: List<MenuVMItem>) {
     menuVMItems.forEach { menuItemPartial -> add(menuItemPartial.title).setOnMenuItemClickListener { menuItemPartial.onClick(); true } }
+}
+
+val Menu.items get() = iterator().asSequence()
+
+fun Menu.unCheckAllMenuItems() {
+    items.forEach { item ->
+        if (item.hasSubMenu())
+            item.subMenu.unCheckAllMenuItems()
+        else
+            item.isChecked = false
+    }
 }
