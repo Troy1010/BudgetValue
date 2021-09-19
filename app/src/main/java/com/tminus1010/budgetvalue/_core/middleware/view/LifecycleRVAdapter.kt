7435588b49416484abf81b940460309d528703bc
Troy <troyswanson1010@gmail.com>
2021-09-18 21:32:59 -0500

    LifecycleRVAdapter2

--- app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/view/LifecycleRVAdapter.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/middleware/view/LifecycleRVAdapter.kt
@@ -3,6 +3,7 @@ package com.tminus1010.budgetvalue._core.middleware.view
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
 
+@Deprecated("Use LifecycleRVAdapter2, which automatically attaches lifecycle, and does not require a map")
 abstract class LifecycleRVAdapter<VH: RecyclerView.ViewHolder>: RecyclerView.Adapter<VH>() {
     abstract fun onViewAttachedToWindow(holder: VH, lifecycle: LifecycleOwner)
     override fun onBindViewHolder(holder: VH, position: Int) { }
