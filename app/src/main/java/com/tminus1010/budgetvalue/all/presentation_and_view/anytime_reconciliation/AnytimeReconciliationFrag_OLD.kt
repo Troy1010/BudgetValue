79eaec425f2367f08327712e4c62318ae166a8de
Troy <troyswanson1010@gmail.com>
2021-09-19 17:59:50 -0500

    !*!*! OLD

---
+++ app/src/main/java/com/tminus1010/budgetvalue/all/presentation_and_view/anytime_reconciliation/AnytimeReconciliationFrag_OLD.kt
@@ -0,0 +1,122 @@
+package com.tminus1010.budgetvalue.all.presentation_and_view.anytime_reconciliation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import com.tminus1010.budgetvalue.*
+import com.tminus1010.budgetvalue._core.extensions.bind
+import com.tminus1010.budgetvalue._core.extensions.easyText
+import com.tminus1010.budgetvalue._core.extensions.getColorByAttr
+import com.tminus1010.budgetvalue._core.middleware.Rx
+import com.tminus1010.budgetvalue._core.middleware.view.onDone
+import com.tminus1010.budgetvalue._core.middleware.view.recipe_factories.itemHeaderRF
+import com.tminus1010.budgetvalue._core.middleware.view.recipe_factories.itemTextViewRB
+import com.tminus1010.budgetvalue._core.middleware.view.recipe_factories.itemTitledDividerRB
+import com.tminus1010.budgetvalue._core.middleware.view.tmTableView3.ViewItemRecipeFactory3
+import com.tminus1010.budgetvalue._core.middleware.view.viewBinding
+import com.tminus1010.budgetvalue.all.presentation_and_view.import_z.AccountsVM
+import com.tminus1010.budgetvalue.budgeted.BudgetedVM
+import com.tminus1010.budgetvalue.categories.CategoriesVM
+import com.tminus1010.budgetvalue.categories.models.Category
+import com.tminus1010.budgetvalue.databinding.*
+import com.tminus1010.budgetvalue.plans.ActivePlanVM
+import com.tminus1010.budgetvalue.transactions.TransactionsMiscVM
+import com.tminus1010.tmcommonkotlin.core.extensions.reflectXY
+import com.tminus1010.tmcommonkotlin.misc.extensions.distinctUntilChangedWith
+import com.tminus1010.tmcommonkotlin.rx.extensions.observe
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.math.BigDecimal
+import java.util.concurrent.TimeUnit
+
+@AndroidEntryPoint
+class AnytimeReconciliationFrag_OLD : Fragment(R.layout.frag_reconcile) {
+    private val vb by viewBinding(FragReconcileBinding::bind)
+    private val anytimeReconciliationVM: AnytimeReconciliationVM by activityViewModels()
+    private val categoriesVM: CategoriesVM by activityViewModels()
+    private val activePlanVM: ActivePlanVM by activityViewModels()
+    private val transactionsMiscVM: TransactionsMiscVM by activityViewModels()
+    private val accountsVM: AccountsVM by activityViewModels()
+    private val budgetedVM: BudgetedVM by activityViewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // # Bind Incoming from Presentation layer
+        // ## State
+        vb.buttonsview.buttons = anytimeReconciliationVM.buttons
+        // ## TMTableView
+        val numberedHeaderRecipeFactory = ViewItemRecipeFactory3<ItemHeaderIncomeBinding, Pair<String, Observable<String>>>(
+            { ItemHeaderIncomeBinding.inflate(LayoutInflater.from(context)) },
+            { d, vb, lifecycle ->
+                vb.textviewHeader.text = d.first
+                d.second.observe(lifecycle) { vb.textviewNumber.text = it }
+            }
+        )
+        val reconcileCARecipeFactory = ViewItemRecipeFactory3<ItemMoneyEditTextBinding, Pair<Category, Observable<String>?>>(
+            { ItemMoneyEditTextBinding.inflate(LayoutInflater.from(context)) },
+            { (category, d), vb, lifecycle ->
+                vb.moneyedittext.onDone { anytimeReconciliationVM.pushActiveReconcileCA(category, it) }
+                if (d == null) return@ViewItemRecipeFactory3
+                d.observe(lifecycle) { vb.moneyedittext.easyText = it }
+            }
+        )
+        val budgetedRecipeFactory = ViewItemRecipeFactory3<ItemTextViewBinding, Observable<BigDecimal>?>(
+            { ItemTextViewBinding.inflate(LayoutInflater.from(requireContext())) },
+            { d, vb, lifecycle ->
+                if (d == null) return@ViewItemRecipeFactory3
+                vb.textview.bind(d, lifecycle) {
+                    easyText = it.toString()
+                    if (it < BigDecimal.ZERO)
+                        setTextColor(context.theme.getColorByAttr(R.attr.colorOnError))
+                    else
+                        setTextColor(context.theme.getColorByAttr(R.attr.colorOnBackground))
+                }
+            },
+        )
+        Rx.combineLatest(categoriesVM.userCategories, activePlanVM.activePlanCAs, transactionsMiscVM.currentSpendBlockCAs, anytimeReconciliationVM.activeReconcileCAsToShow, budgetedVM.categoryAmounts)
+            .observeOn(Schedulers.computation())
+            .debounce(100, TimeUnit.MILLISECONDS)
+            .map { (categories, activePlanCAs, currentSpendBlockCAs, activeReconciliationCAs, budgetedCA) ->
+                val recipeGrid = listOf(
+                    listOf(itemHeaderRF().create("Category"))
+                            + itemTextViewRB().create("Default")
+                            + categories.map { itemTextViewRB().create(it.name) },
+                    listOf(numberedHeaderRecipeFactory.createOne(Pair("Plan", activePlanVM.expectedIncome)))
+                            + itemTextViewRB().create(activePlanVM.defaultAmount)
+                            + categories.map { itemTextViewRB().create(activePlanCAs[it] ?: Observable.just("")) },
+                    listOf(itemHeaderRF().create("Actual"))
+                            + itemTextViewRB().create("")
+                            + categories.map { itemTextViewRB().create(currentSpendBlockCAs[it]?.toString() ?: "") },
+                    listOf(itemHeaderRF().create("Reconcile"))
+                            + itemTextViewRB().create(anytimeReconciliationVM.defaultAmount)
+                            + reconcileCARecipeFactory.createMany(categories.map { it to activeReconciliationCAs[it] }),
+                    listOf(numberedHeaderRecipeFactory.createOne(Pair("Budgeted", accountsVM.accountsTotal)))
+                            + itemTextViewRB().create(budgetedVM.defaultAmount)
+                            + budgetedRecipeFactory.createMany(categories.map { budgetedCA[it] })
+                ).reflectXY()
+                val dividerMap = categories
+                    .withIndex()
+                    .distinctUntilChangedWith(compareBy { it.value.type })
+                    .associate { it.index to itemTitledDividerRB().create(it.value.type.name) }
+                    .mapKeys { it.key + 2 } // header row, default row
+                Pair(recipeGrid, dividerMap)
+            }
+            .observe(viewLifecycleOwner) { (recipeGrid, dividerMap) ->
+                vb.tmTableView.initialize(
+                    recipeGrid = recipeGrid,
+                    shouldFitItemWidthsInsideTable = true,
+                    dividerMap = dividerMap,
+                    rowFreezeCount = 1,
+                )
+            }
+    }
+
+    companion object {
+        fun navTo(nav: NavController) {
+            nav.navigate(R.id.reconcileFrag)
+        }
+    }
+}
---
+++ app/src/main/java/com/tminus1010/budgetvalue/all/presentation_and_view/anytime_reconciliation/AnytimeReconciliationVM_OLD.kt
@@ -0,0 +1,61 @@
+package com.tminus1010.budgetvalue.all.presentation_and_view.anytime_reconciliation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.disposables
+import com.tminus1010.budgetvalue._core.extensions.flatMapSourceHashMap
+import com.tminus1010.budgetvalue._core.extensions.nonLazyCache
+import com.tminus1010.budgetvalue._core.extensions.toMoneyBigDecimal
+import com.tminus1010.budgetvalue._core.middleware.presentation.ButtonVMItem
+import com.tminus1010.budgetvalue._core.middleware.source_objects.SourceHashMap
+import com.tminus1010.budgetvalue.all.app.interactors.SaveActiveReconciliationInteractor
+import com.tminus1010.budgetvalue.categories.domain.CategoriesDomain
+import com.tminus1010.budgetvalue.categories.models.Category
+import com.tminus1010.budgetvalue.reconciliations.data.ReconciliationsRepo
+import com.tminus1010.budgetvalue.reconciliations.domain.ActiveReconciliationDefaultAmountUC
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Observable
+import java.math.BigDecimal
+import javax.inject.Inject
+
+@HiltViewModel
+class AnytimeReconciliationVM_OLD @Inject constructor(
+    private val reconciliationsRepo: ReconciliationsRepo,
+    categoriesDomain: CategoriesDomain,
+    activeReconciliationDefaultAmountUC: ActiveReconciliationDefaultAmountUC,
+    saveActiveReconciliationInteractor: SaveActiveReconciliationInteractor
+) : ViewModel() {
+    // # User Intents
+    fun pushActiveReconcileCA(category: Category, s: String) {
+        reconciliationsRepo.pushActiveReconciliationCA(category to s.toMoneyBigDecimal())
+            .subscribe()
+    }
+
+    // # Presentation Output
+    // ## Events
+    // ## State
+    private val activeReconcileCAs = // TODO("This should be an Interactor or something.")
+        Observable.combineLatest(
+            reconciliationsRepo.activeReconciliationCAs,
+            categoriesDomain.userCategories,
+        ) { activeReconcileCAs, activeCategories ->
+            // These extra zeros prevent refreshes on hidden additions/removals that happen when a value is set to 0.
+            activeCategories.associateWith { BigDecimal.ZERO }
+                .plus(activeReconcileCAs)
+        }
+            .nonLazyCache(disposables)
+    val activeReconcileCAsToShow: Observable<Map<Category, Observable<String>>> =
+        activeReconcileCAs
+            .flatMapSourceHashMap(SourceHashMap(exitValue = BigDecimal.ZERO)) { it.itemObservableMap }
+            .map { it.mapValues { it.value.map { it.toString() } } }
+            .nonLazyCache(disposables)
+    val defaultAmount: Observable<String> =
+        activeReconciliationDefaultAmountUC()
+            .map(BigDecimal::toString)
+    val buttons =
+        listOf(
+            ButtonVMItem(
+                title = "Save",
+                onClick = saveActiveReconciliationInteractor.saveActiveReconiliation::subscribe
+            )
+        )
+}
