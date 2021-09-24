9208faa7c7b22184666b440e207831ef5827f85e
Troy <troyswanson1010@gmail.com>
2021-09-23 22:25:16 -0500

    createPieData moved to ReviewVM

--- app/src/main/java/com/tminus1010/budgetvalue/all/presentation_and_view/review/ReviewFrag.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/all/presentation_and_view/review/ReviewFrag.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.tminus1010.budgetvalue.R
 import com.tminus1010.budgetvalue._core.extensions.bind
-import com.tminus1010.budgetvalue.all.presentation_and_view.services.createPieData
 import com.tminus1010.budgetvalue.databinding.FragReviewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +17,6 @@ class ReviewFrag : Fragment(R.layout.frag_review) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         vb = FragReviewBinding.bind(view)
-        vb.pieChart1.bind(reviewVM.chartData) { data = createPieData(it); invalidate() }
+        vb.pieChart1.bind(reviewVM.pieData) { data = it; invalidate() }
     }
 }
--- app/src/main/java/com/tminus1010/budgetvalue/all/presentation_and_view/review/ReviewVM.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/all/presentation_and_view/review/ReviewVM.kt
@@ -2,15 +2,17 @@ package com.tminus1010.budgetvalue.all.presentation_and_view.review
 
 import androidx.lifecycle.ViewModel
 import com.tminus1010.budgetvalue._core.models.CategoryAmounts
+import com.tminus1010.budgetvalue.all.presentation_and_view.services.createPieData
 import com.tminus1010.budgetvalue.transactions.data.TransactionsRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ReviewVM @Inject constructor(
-    private val transactionsRepo: TransactionsRepo
+    transactionsRepo: TransactionsRepo
 ) : ViewModel() {
-    val chartData =
+    val pieData =
         transactionsRepo.transactions
             .map { it.fold(CategoryAmounts()) { acc, transaction -> acc.addTogether(transaction.categoryAmounts) } }
+            .map(::createPieData)!!
 }
