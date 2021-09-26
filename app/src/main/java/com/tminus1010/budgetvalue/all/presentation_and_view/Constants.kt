d5d4127236b08c922e14831bb3a448a487f74c50
Troy <troyswanson1010@gmail.com>
2021-09-26 11:46:34 -0500

    SpinnerVMItem

--- app/src/main/java/com/tminus1010/budgetvalue/all/presentation_and_view/Constants.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/all/presentation_and_view/Constants.kt
@@ -1,3 +1,11 @@
 package com.tminus1010.budgetvalue.all.presentation_and_view
 
-enum class SelectableDuration { THIS_MONTH, TWO_MONTHS_COMBINED, ONE_MONTH_AGO, TWO_MONTHS_AGO, FOREVER }
+import java.util.*
+
+enum class SelectableDuration {
+    THIS_MONTH, TWO_MONTHS_COMBINED, ONE_MONTH_AGO, TWO_MONTHS_AGO, FOREVER;
+
+    override fun toString(): String {
+        return this.name.lowercase().capitalize(Locale.ROOT).replace("_", " ")
+    }
+}
