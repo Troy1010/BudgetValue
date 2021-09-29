29ed014d38c492b761869f20cd536288bee63592
Troy <troyswanson1010@gmail.com>
2021-09-28 21:40:11 -0500

    cleanup

--- app/src/main/java/com/tminus1010/budgetvalue/transactions/TransactionsMiscVM.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/transactions/TransactionsMiscVM.kt
@@ -5,20 +5,13 @@ import com.tminus1010.budgetvalue.categories.models.Category
 import com.tminus1010.budgetvalue.transactions.domain.TransactionsAppService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.core.Observable
-import java.io.InputStream
 import java.math.BigDecimal
 import javax.inject.Inject
 
 @HiltViewModel
 class TransactionsMiscVM @Inject constructor(
-    private val transactionsAppService: TransactionsAppService,
+    transactionsAppService: TransactionsAppService,
 ) : ViewModel() {
-    // # Input
-    fun userImportTransactions(inputStream: InputStream) {
-        transactionsAppService.importTransactions(inputStream)
-            .subscribe()
-    }
-
     // # Output
     val currentSpendBlockCAs: Observable<Map<Category, BigDecimal>> =
         transactionsAppService.currentSpendBlockCAs
