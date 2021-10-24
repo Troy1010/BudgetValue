b8f6dee8e7a5dccebb81cd83dc3f675f32690ca3
Troy <troyswanson1010@gmail.com>
2021-10-24 17:26:46 -0500

    ActivePlanRepo2`using default pattern, instead of init

--- app/src/main/java/com/tminus1010/budgetvalue/plans/data/ActivePlanRepo2.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/plans/data/ActivePlanRepo2.kt
@@ -6,7 +6,6 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.squareup.moshi.Moshi
-import com.tminus1010.budgetvalue._core.all.extensions.mapNotNull
 import com.tminus1010.budgetvalue._core.app.DatePeriodService
 import com.tminus1010.budgetvalue._core.data.dataStore
 import com.tminus1010.budgetvalue.categories.CategoryAmountsConverter
@@ -14,7 +13,6 @@ import com.tminus1010.budgetvalue.plans.data.model.PlanDTO
 import com.tminus1010.budgetvalue.plans.domain.Plan
 import com.tminus1010.tmcommonkotlin.misc.extensions.fromJson
 import com.tminus1010.tmcommonkotlin.misc.extensions.toJson
-import com.tminus1010.tmcommonkotlin.rx.extensions.value
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.rx3.asObservable
@@ -49,18 +47,15 @@ class ActivePlanRepo2 constructor(
 
     val activePlan =
         dataStore.data.asObservable()
-            .mapNotNull { moshi.fromJson<PlanDTO>(it[key])?.let { Plan.fromDTO(it, categoryAmountsConverter) } }
+            .map {
+                moshi.fromJson<PlanDTO>(it[key])
+                    ?.let { Plan.fromDTO(it, categoryAmountsConverter) }
+                    ?: Plan(
+                        datePeriodService.getDatePeriod(LocalDate.now()),
+                        BigDecimal.ZERO,
+                        mapOf()
+                    )
+            }
             .distinctUntilChanged()
             .replay(1).autoConnect()
-
-    init {
-        if (activePlan.value == null)
-            update(
-                Plan(
-                    datePeriodService.getDatePeriod(LocalDate.now()),
-                    BigDecimal.ZERO,
-                    mapOf()
-                )
-            )
-    }
 }
