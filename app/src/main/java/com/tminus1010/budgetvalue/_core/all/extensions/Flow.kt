8854c4b310ae0e7b7759f428e51c752704515f7a
Troy <troyswanson1010@gmail.com>
2021-11-06 19:08:48 -0500

    doLogx

--- app/src/main/java/com/tminus1010/budgetvalue/_core/all/extensions/Flow.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/all/extensions/Flow.kt
@@ -1,11 +1,19 @@
 package com.tminus1010.budgetvalue._core.all.extensions
 
+import com.tminus1010.tmcommonkotlin.core.logx
 import io.reactivex.rxjava3.core.Observable
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.rx3.asObservable
 
 
 fun <T : Any> Flow<T?>.asObservable2(): Observable<T> {
     return filterNotNull().asObservable()
+}
+
+inline fun <reified T> Flow<T>.doLogx(prefix: String? = null): Flow<T> {
+    return onEach { it.logx(prefix) }
+        .onCompletion { if (it == null) "Completed".logx(prefix) else logz("$prefix`Error:", it) }
 }
