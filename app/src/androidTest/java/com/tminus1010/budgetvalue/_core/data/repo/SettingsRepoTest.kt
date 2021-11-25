b110f60f5d7735c3f9b4392e733cdb76e3dafe3e
Troy <troyswanson1010@gmail.com>
2021-11-25 00:58:36 -0600

    SettingsRepoTest`using Hilt injection

--- app/src/androidTest/java/com/tminus1010/budgetvalue/_core/data/repo/SettingsRepoTest.kt
+++ app/src/androidTest/java/com/tminus1010/budgetvalue/_core/data/repo/SettingsRepoTest.kt
@@ -1,18 +1,40 @@
 package com.tminus1010.budgetvalue._core.data.repo
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import com.tminus1010.budgetvalue.FakeDataStore
+import com.tminus1010.budgetvalue._core.all.dependency_injection.DataStoreModule
+import com.tminus1010.budgetvalue._core.all.dependency_injection.DatabaseModule
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
 
+@UninstallModules(DataStoreModule::class)
 @HiltAndroidTest
 class SettingsRepoTest {
+    @BindValue
+    val fakeDataStore: DataStore<Preferences> = FakeDataStore()
+
+    @get:Rule
+    var hiltAndroidRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var settingsRepo: SettingsRepo
+
+    @Before
+    fun before() {
+        hiltAndroidRule.inject()
+    }
 
     @Test
     fun anchorDateOffset_default_push() {
-        // # Given
-        val settingsRepo = SettingsRepo(FakeDataStore())
         // # Then
         assertEquals(0, settingsRepo.anchorDateOffset.value)
         // # When
@@ -24,8 +46,6 @@ class SettingsRepoTest {
 
     @Test
     fun blockSize_default_push() {
-        // # Given
-        val settingsRepo = SettingsRepo(FakeDataStore())
         // # Then
         assertEquals(14, settingsRepo.blockSize.value)
         // # When
