535b1ec1bb4bf3b251ddd0383b4c28e4ae0ecc2f
Troy <troyswanson1010@gmail.com>
2021-12-05 19:11:26 -0600

    Extensions`easyCollect, easyEmit

--- app/src/main/java/com/tminus1010/budgetvalue/_core/all/extensions/Flow.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/all/extensions/Flow.kt
@@ -1,11 +1,13 @@
 package com.tminus1010.budgetvalue._core.all.extensions
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.tminus1010.tmcommonkotlin.core.logx
 import io.reactivex.rxjava3.core.Observable
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.rx3.asObservable
 
 
@@ -16,4 +18,13 @@ fun <T : Any> Flow<T?>.asObservable2(): Observable<T> {
 inline fun <reified T> Flow<T>.doLogx(prefix: String? = null): Flow<T> {
     return onEach { it.logx(prefix) }
         .onCompletion { if (it == null) "Completed".logx(prefix) else logz("$prefix`Error:", it) }
+}
+
+inline fun <reified T> Flow<T>.easyCollect(lifecycleOwner: LifecycleOwner, crossinline lambda: suspend (T) -> Unit) {
+    val flow = this
+    lifecycleOwner.lifecycleScope.launch {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collect(lambda)
+        }
+    }
 }
