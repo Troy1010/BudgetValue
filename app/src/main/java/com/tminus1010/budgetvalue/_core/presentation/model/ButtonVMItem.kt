261fdefa89e0edca7e7d88566ba6fd0f83539d53
Troy <troyswanson1010@gmail.com>
2021-12-18 17:52:28 -0600

    ButtonVMItem`IHasToViewItemRecipe

--- app/src/main/java/com/tminus1010/budgetvalue/_core/presentation/model/ButtonVMItem.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/presentation/model/ButtonVMItem.kt
@@ -1,8 +1,13 @@
 package com.tminus1010.budgetvalue._core.presentation.model
 
+import android.content.Context
 import android.widget.Button
 import com.tminus1010.budgetvalue._core.all.extensions.bind
 import com.tminus1010.budgetvalue._core.all.extensions.lifecycleOwner
+import com.tminus1010.budgetvalue._core.framework.view.tmTableView3.IViewItemRecipe3
+import com.tminus1010.budgetvalue._core.framework.view.tmTableView3.ViewItemRecipe3__
+import com.tminus1010.budgetvalue.budgeted.presentation.IHasToViewItemRecipe
+import com.tminus1010.budgetvalue.databinding.ItemButtonBinding
 import com.tminus1010.tmcommonkotlin.rx.extensions.observe
 import io.reactivex.rxjava3.core.Observable
 
@@ -12,7 +17,7 @@ data class ButtonVMItem(
     val isEnabled: Observable<Boolean>? = null,
     val onLongClick: (() -> Unit)? = null,
     val onClick: () -> Unit,
-) {
+) : IHasToViewItemRecipe {
     fun bind(button: Button) = button.apply {
         if (titleObservable != null)
             bind(titleObservable) { text = title }
@@ -23,5 +28,11 @@ data class ButtonVMItem(
             ?.also { setOnLongClickListener { it(); true } }
         this@ButtonVMItem.isEnabled?.observe(button.lifecycleOwner!!) { isEnabled = it }
             ?: run { isEnabled = true }
+    }
+
+    override fun toViewItemRecipe(context: Context): IViewItemRecipe3 {
+        return ViewItemRecipe3__(context, ItemButtonBinding::inflate) { vb ->
+            bind(vb.btnItem)
+        }
     }
 }
