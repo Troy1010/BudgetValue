5986bc6b848cf63a6d1bea7732b3a156fb5e83a5
Troy <troyswanson1010@gmail.com>
2022-01-23 20:22:01 -0600

    SharedPrefWrapper`cleanup

--- app/src/main/java/com/tminus1010/budgetvalue/_core/data/SharedPrefWrapper.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/data/SharedPrefWrapper.kt
@@ -1,14 +1,7 @@
 package com.tminus1010.budgetvalue._core.data
 
 import android.content.SharedPreferences
-import com.tminus1010.budgetvalue._core.data.MoshiProvider.moshi
-import com.tminus1010.tmcommonkotlin.misc.extensions.fromJson
-import com.tminus1010.tmcommonkotlin.misc.extensions.toJson
-import com.tminus1010.tmcommonkotlin.rx.extensions.value
 import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.subjects.PublishSubject
-import java.math.BigDecimal
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -18,83 +11,11 @@ class SharedPrefWrapper @Inject constructor(
 ) {
     companion object {
         enum class Key {
-            RECONCILE_CATEGORY_AMOUNTS,
-            ANCHOR_DATE_OFFSET,
-            BLOCK_SIZE,
             APP_INIT_BOOL,
         }
-
-        const val ANCHOR_DATE_OFFSET_DEFAULT: Long = 0
-        const val BLOCK_SIZE_DEFAULT: Long = 14
     }
 
     private val editor: SharedPreferences.Editor = sharedPreferences.edit()
-
-    // # ActiveReconciliation
-
-    private val activeReconciliationCAsPublisher = PublishSubject.create<Map<String, String>>()
-    val activeReconciliationCAs: Observable<Map<String, String>> =
-        activeReconciliationCAsPublisher
-            .startWithItem(moshi.fromJson(sharedPreferences.getString(Key.RECONCILE_CATEGORY_AMOUNTS.name, null) ?: "{}"))
-            .distinctUntilChanged()
-            .replay(1).autoConnect().also { it.subscribe { } }
-
-    fun pushActiveReconciliationCAs(categoryAmounts: Map<String, String>?): Completable {
-        categoryAmounts
-            ?.let { moshi.toJson(it) }
-            ?.also { editor.putString(Key.RECONCILE_CATEGORY_AMOUNTS.name, it) }
-            ?: editor.remove(Key.RECONCILE_CATEGORY_AMOUNTS.name)
-        return Completable.fromAction {
-            editor.commit()
-            activeReconciliationCAsPublisher.onNext(categoryAmounts ?: emptyMap())
-        }
-    }
-
-    fun pushActiveReconciliationCA(kv: Pair<String, String?>): Completable {
-        val (k, v) = kv
-        return activeReconciliationCAs.value!!
-            .toMutableMap()
-            .also { if (v == null || v == BigDecimal.ZERO.toString()) it.remove(k) else it[k] = v }
-            .let { pushActiveReconciliationCAs(it) }
-    }
-
-    fun clearActiveReconcileCAs() = pushActiveReconciliationCAs(null)
-
-    // # AnchorDateOffset
-
-    private val anchorDateOffsetPublisher = PublishSubject.create<Long>()
-    val anchorDateOffset: Observable<Long> =
-        anchorDateOffsetPublisher
-            .startWithItem(sharedPreferences.getLong(Key.ANCHOR_DATE_OFFSET.name, ANCHOR_DATE_OFFSET_DEFAULT))
-            .distinctUntilChanged()
-
-    fun pushAnchorDateOffset(anchorDateOffset: Long?): Completable {
-        anchorDateOffset
-            ?.also { editor.putString(Key.ANCHOR_DATE_OFFSET.name, it.toString()) }
-            ?: editor.remove(Key.ANCHOR_DATE_OFFSET.name)
-        return Completable.fromAction {
-            editor.commit()
-            anchorDateOffsetPublisher.onNext(anchorDateOffset ?: ANCHOR_DATE_OFFSET_DEFAULT)
-        }
-    }
-
-    // # BlockSize
-
-    private val blockSizePublisher = PublishSubject.create<Long>()
-    val blockSize: Observable<Long> =
-        blockSizePublisher
-            .startWithItem(sharedPreferences.getLong(Key.BLOCK_SIZE.name, BLOCK_SIZE_DEFAULT))
-            .distinctUntilChanged()
-
-    fun pushBlockSize(blockSize: Long?): Completable {
-        blockSize
-            ?.also { editor.putLong(Key.BLOCK_SIZE.name, it) }
-            ?: editor.remove(Key.BLOCK_SIZE.name)
-        return Completable.fromAction {
-            editor.commit()
-            blockSizePublisher.onNext(blockSize ?: BLOCK_SIZE_DEFAULT)
-        }
-    }
 
     // # AppInitBool
 
