6c5ec7bd33aad2817d093202bc001ad95eefb86f
Troy <troyswanson1010@gmail.com>
2022-01-29 14:27:21 -0600

    passing in adbAbsolutePath

--- buildSrc/src/main/java/com/tminus1010/budgetvalue/BudgetValuePlugin.kt
+++ buildSrc/src/main/java/com/tminus1010/budgetvalue/BudgetValuePlugin.kt
@@ -4,31 +4,14 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 open class BudgetValuePlugin : Plugin<Project> {
-    var x = "x"
+    open class BudgetValuePluginExtension {
+        var adbAbsolutePath: String? = null
+    }
+
     override fun apply(project: Project) {
+        val budgetValuePluginExtension = project.extensions.create("budgetValuePluginExtension", BudgetValuePluginExtension::class.java)
         project.afterEvaluate {
-            print(x)
-//            tasks.tryRegisterOrderedPair("clean", "assemble")
-//                .configure { group = "build" }
-//            tasks.tryRegisterOrderedPair("clean_assemble", "uninstallDebug")
-//                .configure { group = "install" }
-//            tasks.tryRegisterOrderedPair("clean_assemble_uninstallDebug", "installDebug")
-//                .configure { group = "install" }
-//            if (tasks.contains("publishToMavenLocal"))
-//                tasks.tryRegisterOrderedPair("assemble", "publishToMavenLocal")
-//                    .configure { group = "publishing" }
-//
-//
-//            tasks.register("launchApp", LaunchApp, android.getAdbExecutable().absolutePath)
-//                .configure { it.group = "adb" }
-//            tasks.register("launchDevEnv_Main", LaunchDevEnv_Main, android.getAdbExecutable().absolutePath)
-//                .configure { it.group = "adb" }
-//            tasks.register("quitApp", Quit, android.getAdbExecutable().absolutePath)
-//                .configure { it.group = "adb" }
-//            TaskContainerKt.tryRegisterOrderedPair(tasks, "quitApp", "launchApp")
-//                .configure { it.group = "adb" }
-//            TaskContainerKt.tryRegisterOrderedPair(tasks, "quitApp", "launchDevEnv_Main")
-//                .configure { it.group = "adb" }
+            print(budgetValuePluginExtension.adbAbsolutePath)
         }
     }
 }
