3f074bdabbc4c85c1a8248c85abdc87947dadc2f
Troy <troyswanson1010@gmail.com>
2022-01-30 18:00:52 -0600

    using mustRunAfter instead of tryRegisterOrderedPair

--- buildSrc/src/main/java/com/tminus1010/budgetvalue/BudgetValuePlugin.kt
+++ buildSrc/src/main/java/com/tminus1010/budgetvalue/BudgetValuePlugin.kt
@@ -2,7 +2,6 @@ package com.tminus1010.budgetvalue
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import tmextensions.tryRegisterOrderedPair
 
 open class BudgetValuePlugin : Plugin<Project> {
     open class Settings {
@@ -16,37 +15,32 @@ open class BudgetValuePlugin : Plugin<Project> {
     override fun apply(project: Project) {
         budgetValuePluginSettings = project.extensions.create("budgetValuePluginSettings", Settings::class.java)
         project.afterEvaluate {
-            // # easyRebuildAndLaunchApp, easyInstallAndLaunchApp
+            // # Register launchApp
             tasks.register("launchApp", LaunchApp::class.java, budgetValuePluginSettings.adbAbsolutePath)
-            tasks.tryRegisterOrderedPair("installDebug", "launchApp")
-            tasks.tryRegisterOrderedPair("clean", "installDebug_launchApp")
-            tasks.register("easyRebuildAndLaunchApp") {
-                description = "Launches slowly, but reliably"
-                group = "easy"
-                dependsOn(tasks.named("clean_installDebug_launchApp"))
-            }
+                .configure { setMustRunAfter(listOf(tasks.named("installDebug"))) }
             tasks.register("installAndLaunchApp") {
                 description = "Launches more quickly (if build cache is available), but less reliably"
                 group = "launch"
-                dependsOn(tasks.named("installDebug_launchApp"))
+                dependsOn(tasks.named("installDebug"), tasks.named("launchApp"))
             }
-            // easyRebuildAndLaunchDevEnvs, easyInstallAndLaunchDevEnvs
+            tasks.register("easyRebuildAndLaunchApp") {
+                description = "Launches slowly, but reliably"
+                group = "easy"
+                dependsOn(tasks.named("clean"), tasks.named("installDebug"), tasks.named("launchApp"))
+            }
+            // # Register launchDevEnvs
             listOf("DevEnv_Main", "DevEnv_UnlockedFeatures").forEach {
                 tasks.register("launch$it", LaunchDevEnv::class.java, budgetValuePluginSettings.adbAbsolutePath, it)
-                tasks.register("(installDebug,installDebugAndroidTest)_launch$it") {
-                    dependsOn(tasks.named("installDebug"), tasks.named("installDebugAndroidTest"))
-                    finalizedBy(tasks.named("launch$it"))
-                }
-                tasks.tryRegisterOrderedPair("clean", "(installDebug,installDebugAndroidTest)_launch$it")
-                tasks.register("easyRebuildAndLaunch$it") {
-                    description = "Launches slowly, but reliably. When successful, it will throw a timeout failure.. just ignore it."
-                    group = "easy"
-                    dependsOn(tasks.named("clean_(installDebug,installDebugAndroidTest)_launch$it"))
-                }
+                    .configure { setMustRunAfter(listOf(tasks.named("installDebug"), tasks.named("installDebugAndroidTest"))) }
                 tasks.register("installAndLaunch$it") {
                     description = "Launches quickly (if build cache is available), but less reliably. When successful, it will throw a timeout failure.. just ignore it."
                     group = "launch"
-                    dependsOn(tasks.named("(installDebug,installDebugAndroidTest)_launch$it"))
+                    dependsOn(tasks.named("installDebug"), tasks.named("installDebugAndroidTest"), tasks.named("launch$it"))
+                }
+                tasks.register("easyRebuildAndLaunch$it") {
+                    description = "Launches slowly, but reliably. When successful, it will throw a timeout failure.. just ignore it."
+                    group = "easy"
+                    dependsOn(tasks.named("clean"), tasks.named("installDebug"), tasks.named("installDebugAndroidTest"), tasks.named("launch$it"))
                 }
             }
         }
