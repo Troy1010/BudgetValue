99358cdd2ed8029334b55806dbfa79a51b346d4e
Troy <troyswanson1010@gmail.com>
2022-02-03 02:36:23 -0600

    BudgetValuePlugin`find DevEnv names

--- buildSrc/src/main/java/com/tminus1010/budgetvalue/BudgetValuePlugin.kt
+++ buildSrc/src/main/java/com/tminus1010/budgetvalue/BudgetValuePlugin.kt
@@ -20,26 +20,26 @@ open class BudgetValuePlugin : Plugin<Project> {
                 .configure { setMustRunAfter(listOf(tasks.named("installDebug"))) }
             tasks.register("installAndLaunchApp") {
                 description = "Launches more quickly (if build cache is available), but less reliably"
-                group = "launch"
+                group = "budgetvalue"
                 dependsOn(tasks.named("installDebug"), tasks.named("launchApp"))
             }
             tasks.register("rebuildAndLaunchApp") {
                 description = "Launches slowly, but reliably"
-                group = "launch"
+                group = "budgetvalue"
                 dependsOn(tasks.named("clean"), tasks.named("installDebug"), tasks.named("launchApp"))
             }
             // # Register launchDevEnvs
-            listOf("DevEnv_Main", "DevEnv_UnlockedFeatures").forEach {
+            layout.projectDirectory.dir("src/androidTest/java/com/tminus1010/budgetvalue/__devEnvs").asFileTree.filter { it.name.startsWith("DevEnv") }.map { it.name.dropLast(3) }.forEach {
                 tasks.register("launch$it", LaunchDevEnv::class.java, budgetValuePluginSettings.adbAbsolutePath, it)
                     .configure { setMustRunAfter(listOf(tasks.named("installDebug"), tasks.named("installDebugAndroidTest"))) }
                 tasks.register("installAndLaunch$it") {
                     description = "Launches quickly (if build cache is available), but less reliably. When successful, it will throw a timeout failure.. just ignore it."
-                    group = "launch"
+                    group = "budgetvalue"
                     dependsOn(tasks.named("installDebug"), tasks.named("installDebugAndroidTest"), tasks.named("launch$it"))
                 }
                 tasks.register("rebuildAndLaunch$it") {
                     description = "Launches slowly, but reliably. When successful, it will throw a timeout failure.. just ignore it."
-                    group = "launch"
+                    group = "budgetvalue"
                     dependsOn(tasks.named("clean"), tasks.named("installDebug"), tasks.named("installDebugAndroidTest"), tasks.named("launch$it"))
                 }
             }
