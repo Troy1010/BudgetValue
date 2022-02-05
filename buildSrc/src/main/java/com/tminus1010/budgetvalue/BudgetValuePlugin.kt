bf2e2acefae9831fb46b37c9b23f7783e4c1e045
Troy <troyswanson1010@gmail.com>
2022-02-05 02:20:53 -0600

    less .named()

--- buildSrc/src/main/java/com/tminus1010/budgetvalue/BudgetValuePlugin.kt
+++ buildSrc/src/main/java/com/tminus1010/budgetvalue/BudgetValuePlugin.kt
@@ -17,30 +17,30 @@ open class BudgetValuePlugin : Plugin<Project> {
         project.afterEvaluate {
             // # Register launchApp
             tasks.register("launchApp", LaunchApp::class.java, budgetValuePluginSettings.adbAbsolutePath)
-                .configure { setMustRunAfter(listOf(tasks.named("installDebug"))) }
+                .configure { setMustRunAfter(listOf("installDebug")) }
             tasks.register("installAndLaunchApp") {
                 description = "Launches more quickly (if build cache is available), but less reliably"
                 group = "budgetvalue"
-                dependsOn(tasks.named("installDebug"), tasks.named("launchApp"))
+                dependsOn("installDebug", "launchApp")
             }
             tasks.register("rebuildAndLaunchApp") {
                 description = "Launches slowly, but reliably"
                 group = "budgetvalue"
-                dependsOn(tasks.named("clean"), tasks.named("installDebug"), tasks.named("launchApp"))
+                dependsOn("clean", "installDebug", "launchApp")
             }
             // # Register launchDevEnvs
             layout.projectDirectory.dir("src/androidTest/java/com/tminus1010/budgetvalue/__devEnvs").asFileTree.filter { it.name.startsWith("DevEnv") }.map { it.name.dropLast(3) }.forEach {
                 tasks.register("launch$it", LaunchDevEnv::class.java, budgetValuePluginSettings.adbAbsolutePath, it)
-                    .configure { setMustRunAfter(listOf(tasks.named("installDebug"), tasks.named("installDebugAndroidTest"))) }
+                    .configure { setMustRunAfter(listOf("installDebug", "installDebugAndroidTest")) }
                 tasks.register("installAndLaunch$it") {
                     description = "Launches quickly (if build cache is available), but less reliably. When successful, it will throw a timeout failure.. just ignore it."
                     group = "budgetvalue"
-                    dependsOn(tasks.named("installDebug"), tasks.named("installDebugAndroidTest"), tasks.named("launch$it"))
+                    dependsOn("installDebug", "installDebugAndroidTest", "launch$it")
                 }
                 tasks.register("rebuildAndLaunch$it") {
                     description = "Launches slowly, but reliably. When successful, it will throw a timeout failure.. just ignore it."
                     group = "budgetvalue"
-                    dependsOn(tasks.named("clean"), tasks.named("installDebug"), tasks.named("installDebugAndroidTest"), tasks.named("launch$it"))
+                    dependsOn("clean", "installDebug", "installDebugAndroidTest", "launch$it")
                 }
             }
         }
