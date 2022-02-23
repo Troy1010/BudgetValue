7280ba379aa7a267d3ef13f27ab1b9888666fc5b
Troy <troyswanson1010@gmail.com>
2022-02-22 23:30:17 -0600

    formatting

--- app/src/main/java/com/tminus1010/budgetvalue/_core/presentation/view_model/HostVM.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/presentation/view_model/HostVM.kt
@@ -3,10 +3,10 @@ package com.tminus1010.budgetvalue._core.presentation.view_model
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.tminus1010.budgetvalue.R
-import com.tminus1010.budgetvalue._core.presentation.service.GetExtraMenuItemPartials
-import com.tminus1010.budgetvalue._core.presentation.model.MenuVMItem
 import com.tminus1010.budgetvalue._core.presentation.model.MenuPresentationModel
+import com.tminus1010.budgetvalue._core.presentation.model.MenuVMItem
 import com.tminus1010.budgetvalue._core.presentation.model.UnformattedString
+import com.tminus1010.budgetvalue._core.presentation.service.GetExtraMenuItemPartials
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import io.reactivex.rxjava3.subjects.PublishSubject
@@ -18,12 +18,15 @@ class HostVM @Inject constructor(
 ) : ViewModel() {
     // # Setup
     val nav = BehaviorSubject.create<NavController>()
-    //
+
+    // # Events
     val unCheckAllMenuItems = PublishSubject.create<Unit>()
     val navToHistory = PublishSubject.create<Unit>()
     val navToTransactions = PublishSubject.create<Unit>()
     val navToFutures = PublishSubject.create<Unit>()
     val navToReplays = PublishSubject.create<Unit>()
+
+    // # State
     val topMenuVMItems =
         MenuPresentationModel(
             MenuVMItem(
