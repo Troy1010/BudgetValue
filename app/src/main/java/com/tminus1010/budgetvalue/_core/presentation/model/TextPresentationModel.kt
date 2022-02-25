166df3fcbf03ae5051d468ae07cc01cce3a5582e
Troy <troyswanson1010@gmail.com>
2022-02-24 21:32:35 -0600

    TextPresentationModel.text2

--- app/src/main/java/com/tminus1010/budgetvalue/_core/presentation/model/TextPresentationModel.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_core/presentation/model/TextPresentationModel.kt
@@ -6,12 +6,15 @@ import com.tminus1010.budgetvalue._core.framework.view.tmTableView3.ViewItemReci
 import com.tminus1010.budgetvalue.budgeted.presentation.IHasToViewItemRecipe
 import com.tminus1010.budgetvalue.databinding.ItemHeaderBinding
 import com.tminus1010.budgetvalue.databinding.ItemTextViewBinding
+import com.tminus1010.tmcommonkotlin.misc.extensions.bind
 import com.tminus1010.tmcommonkotlin.view.extensions.toPX
+import kotlinx.coroutines.flow.Flow
 
 // TODO: add functionality to TextVMItem, and use that instead. Figure out how it can be most easily unit-tested
 data class TextPresentationModel(
     val style: Style = Style.ONE,
     val text1: String? = null,
+    val text2: Flow<String?>? = null,
     val onClick: (() -> Unit)? = null,
     val onLongClick: (() -> Unit)? = null,
     val menuPresentationModel: MenuPresentationModel? = null,
@@ -22,6 +25,7 @@ data class TextPresentationModel(
         return if (style == Style.HEADER)
             ViewItemRecipe3__(context, ItemHeaderBinding::inflate) { vb ->
                 vb.root.text = text1
+                if (text2 != null) vb.root.bind(text2) { text = it }
                 onClick?.also { onClick -> vb.root.setOnClickListener { onClick() } }
                 onLongClick?.also { onLongClick -> vb.root.setOnLongClickListener { onLongClick(); true } }
                 menuPresentationModel?.bind(vb.root)
@@ -33,6 +37,7 @@ data class TextPresentationModel(
                     vb.root.requestLayout()
                 }
                 vb.root.text = text1
+                if (text2 != null) vb.root.bind(text2) { text = it }
                 onClick?.also { onClick -> vb.root.setOnClickListener { onClick() } }
                 onLongClick?.also { onLongClick -> vb.root.setOnLongClickListener { onLongClick(); true } }
                 menuPresentationModel?.bind(vb.root)
