fb28dbb532e2846e7a9eadfcda408e24e5646eee
Troy <troyswanson1010@gmail.com>
2022-02-24 22:55:52 -0600

    CreateFuture2VM`developing

--- app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/presentation/CreateFuture2VM.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/presentation/CreateFuture2VM.kt
@@ -3,14 +3,13 @@ package com.tminus1010.budgetvalue.replay_or_future.presentation
 import androidx.lifecycle.ViewModel
 import com.tminus1010.budgetvalue._core.all.extensions.onNext
 import com.tminus1010.budgetvalue._core.all.extensions.toMoneyBigDecimal
-import com.tminus1010.budgetvalue._core.presentation.model.ButtonVMItem
-import com.tminus1010.budgetvalue._core.presentation.model.CheckboxVMItem
-import com.tminus1010.budgetvalue._core.presentation.model.MoneyEditVMItem
-import com.tminus1010.budgetvalue._core.presentation.model.TextPresentationModel
+import com.tminus1010.budgetvalue._core.presentation.model.*
+import com.tminus1010.budgetvalue.transactions.presentation.model.SearchType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import java.math.BigDecimal
 import javax.inject.Inject
 
@@ -31,37 +30,47 @@ class CreateFuture2VM @Inject constructor(
         isPermanent.onNext(b)
     }
 
+    fun userSetSearchType(searchType: SearchType) {
+        this.searchType.onNext(searchType)
+    }
+
+    fun userSetDescription(s: String) {
+        description.onNext(s)
+    }
+
     // # Internal
     val totalGuess = MutableStateFlow(BigDecimal.TEN)
     val isPermanent = MutableStateFlow(false)
+    val searchType = MutableStateFlow(SearchType.DESCRIPTION)
+    val description = MutableStateFlow<String?>(null)
 
     // # Events
     val navUp = MutableSharedFlow<Unit>()
 
     // # State
     val otherInput =
-        flowOf(
+        searchType.map { searchType ->
             listOfNotNull(
                 listOf(
                     TextPresentationModel(text1 = "Total Guess"),
                     MoneyEditVMItem(text1 = totalGuess.value.toString(), onDone = { userSetTotalGuess(it) }),
                 ),
-//                listOf(
-//                    itemTextViewRB().create(createFutureVM.searchTypeHeader),
-//                    itemSpinnerRF().create(SearchType.values(), createFutureVM.searchType.value, createFutureVM::userSetSearchType),
-//                ),
-//                if (listOf(SearchType.DESCRIPTION_AND_TOTAL, SearchType.DESCRIPTION).any { it == searchType })
-//                    listOf(
-//                        itemTextViewRB().create(createFutureVM.searchDescriptionHeader),
-//                        itemEditTextRF().create(createFutureVM.searchDescription, createFutureVM::userSetSearchDescription, createFutureVM.searchDescriptionMenuVMItems),
-//                    )
-//                else null,
+                listOf(
+                    TextPresentationModel(text1 = "Search Type"),
+                    SpinnerVMItem(SearchType.values(), searchType, onNewItem = { userSetSearchType(it) }),
+                ),
+                if (listOf(SearchType.DESCRIPTION_AND_TOTAL, SearchType.DESCRIPTION).any { it == searchType })
+                    listOf(
+                        TextPresentationModel(text1 = "Description"),
+                        EditTextVMItem(text = description.value, onDone = { userSetDescription(it) }),
+                    )
+                else null,
                 listOf(
                     TextPresentationModel(text1 = "Is Permanent"),
                     CheckboxVMItem(isPermanent.value, onCheckChanged = { userSetIsPermanent(it) }),
                 ),
             )
-        )
+        }
     val recipeGrid =
         flowOf(
             listOf(
@@ -69,7 +78,7 @@ class CreateFuture2VM @Inject constructor(
                     TextPresentationModel(TextPresentationModel.Style.HEADER, "Category"),
                     TextPresentationModel(TextPresentationModel.Style.HEADER, "Amount"),
                     TextPresentationModel(TextPresentationModel.Style.HEADER, "Fill"),
-                )
+                ),
             )
         )
     val buttons =
@@ -77,7 +86,7 @@ class CreateFuture2VM @Inject constructor(
             listOfNotNull(
                 ButtonVMItem(
                     title = "Submit",
-                    onClick = { userSubmit() }
+                    onClick = { userSubmit() },
                 ),
             )
         )
