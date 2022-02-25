daf42ced3b23cb499838b7ad6cd18985038749e5
Troy <troyswanson1010@gmail.com>
2022-02-24 22:08:53 -0600

    developing CreateFuture2VM

--- app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/presentation/CreateFuture2VM.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/presentation/CreateFuture2VM.kt
@@ -1,23 +1,84 @@
 package com.tminus1010.budgetvalue.replay_or_future.presentation
 
 import androidx.lifecycle.ViewModel
+import com.tminus1010.budgetvalue._core.all.extensions.onNext
+import com.tminus1010.budgetvalue._core.all.extensions.toMoneyBigDecimal
+import com.tminus1010.budgetvalue._core.presentation.model.ButtonVMItem
+import com.tminus1010.budgetvalue._core.presentation.model.CheckboxVMItem
+import com.tminus1010.budgetvalue._core.presentation.model.MoneyEditVMItem
 import com.tminus1010.budgetvalue._core.presentation.model.TextPresentationModel
-import com.tminus1010.budgetvalue._core.presentation.model.TextVMItem
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.reactivex.rxjava3.core.Observable
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOf
+import java.math.BigDecimal
 import javax.inject.Inject
 
 @HiltViewModel
 class CreateFuture2VM @Inject constructor(
 ) : ViewModel() {
+    // # User Intents
+    fun userSubmit() {
+        TODO()
+        navUp.onNext()
+    }
+
+    fun userSetTotalGuess(s: String) {
+        totalGuess.onNext(s.toMoneyBigDecimal())
+    }
+
+    fun userSetIsPermanent(b: Boolean) {
+        isPermanent.onNext(b)
+    }
+
+    // # Internal
+    val totalGuess = MutableStateFlow(BigDecimal.TEN)
+    val isPermanent = MutableStateFlow(false)
+
+    // # Events
+    val navUp = MutableSharedFlow<Unit>()
+
+    // # State
+    val otherInput =
+        flowOf(
+            listOfNotNull(
+                listOf(
+                    TextPresentationModel(text1 = "Total Guess"),
+                    MoneyEditVMItem(text1 = totalGuess.value.toString(), onDone = { userSetTotalGuess(it) }),
+                ),
+//                listOf(
+//                    itemTextViewRB().create(createFutureVM.searchTypeHeader),
+//                    itemSpinnerRF().create(SearchType.values(), createFutureVM.searchType.value, createFutureVM::userSetSearchType),
+//                ),
+//                if (listOf(SearchType.DESCRIPTION_AND_TOTAL, SearchType.DESCRIPTION).any { it == searchType })
+//                    listOf(
+//                        itemTextViewRB().create(createFutureVM.searchDescriptionHeader),
+//                        itemEditTextRF().create(createFutureVM.searchDescription, createFutureVM::userSetSearchDescription, createFutureVM.searchDescriptionMenuVMItems),
+//                    )
+//                else null,
+                listOf(
+                    TextPresentationModel(text1 = "Is Permanent"),
+                    CheckboxVMItem(isPermanent.value, onCheckChanged = { userSetIsPermanent(it) }),
+                ),
+            )
+        )
     val recipeGrid =
-        Observable.just(
+        flowOf(
             listOf(
                 listOf(
                     TextPresentationModel(TextPresentationModel.Style.HEADER, "Category"),
                     TextPresentationModel(TextPresentationModel.Style.HEADER, "Amount"),
                     TextPresentationModel(TextPresentationModel.Style.HEADER, "Fill"),
                 )
+            )
+        )
+    val buttons =
+        flowOf(
+            listOfNotNull(
+                ButtonVMItem(
+                    title = "Submit",
+                    onClick = { userSubmit() }
+                ),
             )
         )
 }
--- app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/view/CreateFuture2Frag.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/replay_or_future/view/CreateFuture2Frag.kt
@@ -9,7 +9,9 @@ import com.tminus1010.budgetvalue.R
 import com.tminus1010.budgetvalue._core.framework.view.viewBinding
 import com.tminus1010.budgetvalue.databinding.FragCreateFuture2Binding
 import com.tminus1010.budgetvalue.replay_or_future.presentation.CreateFuture2VM
+import com.tminus1010.tmcommonkotlin.coroutines.extensions.observe
 import com.tminus1010.tmcommonkotlin.misc.extensions.bind
+import com.tminus1010.tmcommonkotlin.view.extensions.nav
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -20,13 +22,22 @@ class CreateFuture2Frag : Fragment(R.layout.frag_create_future_2) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // # Events
+        createFuture2VM.navUp.observe(viewLifecycleOwner) { nav.navigateUp() }
         // # State
+        vb.tmTableViewOtherInput.bind(createFuture2VM.otherInput) {
+            initialize(
+                recipeGrid = it.map { it.map { it.toViewItemRecipe(requireContext()) } },
+                shouldFitItemWidthsInsideTable = true
+            )
+        }
         vb.tmTableViewCategoryAmounts.bind(createFuture2VM.recipeGrid) {
             initialize(
                 recipeGrid = it.map { it.map { it.toViewItemRecipe(requireContext()) } },
                 shouldFitItemWidthsInsideTable = true,
             )
         }
+        vb.buttonsview.bind(createFuture2VM.buttons) { buttons = it }
     }
 
     companion object {
