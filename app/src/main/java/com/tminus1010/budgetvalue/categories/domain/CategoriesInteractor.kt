d81e8a948688cb49308d36a593c406d9ef0d712f
Troy <troyswanson1010@gmail.com>
2022-03-20 02:25:22 -0500

    CategoriesInteractor`improved

--- app/src/main/java/com/tminus1010/budgetvalue/categories/domain/CategoriesInteractor.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/categories/domain/CategoriesInteractor.kt
@@ -1,13 +1,11 @@
 package com.tminus1010.budgetvalue.categories.domain
 
-import com.tminus1010.budgetvalue._core.categoryComparator
 import com.tminus1010.budgetvalue.categories.data.CategoriesRepo
 import com.tminus1010.budgetvalue.categories.models.Category
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.rx3.asObservable
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -17,14 +15,16 @@ class CategoriesInteractor @Inject constructor(
 ) {
     fun parseCategory(categoryName: String): Category {
         if (categoryName == Category.DEFAULT.name) error("Should never have to parse \"${Category.DEFAULT.name}\"")
-        return nameToCategoryMap.blockingFirst()[categoryName]
+        return userCategoryMap.value[categoryName]
             ?: Category.UNRECOGNIZED.also { logz("Warning: returning category Unrecognized for unrecognized name:$categoryName") }
     }
 
-    val userCategories = categoriesRepo.userCategories.map { it.sortedWith(categoryComparator) }.stateIn(GlobalScope, SharingStarted.Eagerly, emptyList())
+    val userCategories =
+        categoriesRepo.userCategories
+            .stateIn(GlobalScope, SharingStarted.Eagerly, emptyList())
 
-    private val nameToCategoryMap =
-        userCategories.asObservable()
-            .map { it.associateBy { it.name } as HashMap<String, Category> }
-            .replay(1).apply { connect() }
+    private val userCategoryMap =
+        userCategories
+            .map { it.associate { it.name to it } }
+            .stateIn(GlobalScope, SharingStarted.Eagerly, emptyMap())
 }
