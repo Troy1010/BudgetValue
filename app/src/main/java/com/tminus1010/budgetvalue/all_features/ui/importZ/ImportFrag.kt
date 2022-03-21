ec1b03951177f18fcf2e5ba8958669f147cfe4bd
Troy <troyswanson1010@gmail.com>
2022-03-20 20:06:08 -0500

    ImportVM iteration

--- app/src/main/java/com/tminus1010/budgetvalue/all_features/ui/importZ/ImportFrag.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/all_features/ui/importZ/ImportFrag.kt
@@ -13,51 +13,46 @@ import com.tminus1010.budgetvalue.all_features.all_layers.extensions.onClick
 import com.tminus1010.budgetvalue.all_features.framework.view.GenViewHolder2
 import com.tminus1010.budgetvalue.all_features.framework.view.onDone
 import com.tminus1010.budgetvalue.all_features.framework.view.viewBinding
-import com.tminus1010.budgetvalue.all_features.ui.all_features.model.AccountPresentationModel
 import com.tminus1010.budgetvalue.databinding.FragImportBinding
 import com.tminus1010.budgetvalue.databinding.ItemAccountBinding
-import com.tminus1010.tmcommonkotlin.rx.extensions.observe
+import com.tminus1010.tmcommonkotlin.misc.extensions.bind
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ImportFrag : Fragment(R.layout.frag_import) {
-    private val importVM by activityViewModels<ImportVM>()
+    private val viewModel by activityViewModels<ImportVM>()
     private val vb by viewBinding(FragImportBinding::bind)
-    var accounts = emptyList<AccountPresentationModel>()
-        set(value) {
-            field = value; vb.recyclerviewAccounts.adapter?.notifyDataSetChanged()
-        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // # Buttons
-        vb.buttonsview.buttons = importVM.buttons
-        // # Accounts
-        importVM.accountVMItemList.observe(viewLifecycleOwner) { accounts = it }
+        // # State
+        vb.buttonsview.bind(viewModel.buttons) { buttons = it }
         vb.recyclerviewAccounts.layoutManager = LinearLayoutManager(requireActivity())
-        vb.recyclerviewAccounts.adapter = object : RecyclerView.Adapter<GenViewHolder2<ItemAccountBinding>>() {
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-                GenViewHolder2(ItemAccountBinding.inflate(layoutInflater, parent, false))
+        vb.recyclerviewAccounts.bind(viewModel.accountVMItemList) { accountVMItems ->
+            adapter = object : RecyclerView.Adapter<GenViewHolder2<ItemAccountBinding>>() {
+                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+                    GenViewHolder2(ItemAccountBinding.inflate(layoutInflater, parent, false))
 
-            override fun getItemCount() = accounts.size
-            override fun onBindViewHolder(holder: GenViewHolder2<ItemAccountBinding>, position: Int) {
-                val vb = holder.vb
-                val accountVMItem = accounts[position]
-                vb.edittextName.easyText2 = accountVMItem.title
-                vb.edittextName.onDone {
-                    // TODO("There should be a better way to avoid NO_POSITION error.")
-                    if (holder.adapterPosition == RecyclerView.NO_POSITION) return@onDone
-                    val accountVMItem = accounts[holder.adapterPosition]
-                    accountVMItem.userSetTitle(it)
+                override fun getItemCount() = accountVMItems.size
+                override fun onBindViewHolder(holder: GenViewHolder2<ItemAccountBinding>, position: Int) {
+                    val vb = holder.vb
+                    val accountVMItem = accountVMItems[position]
+                    vb.edittextName.easyText2 = accountVMItem.title
+                    vb.edittextName.onDone {
+                        // TODO("There should be a better way to avoid NO_POSITION error.")
+                        if (holder.adapterPosition == RecyclerView.NO_POSITION) return@onDone
+                        val accountVMItem = accountVMItems[holder.adapterPosition]
+                        accountVMItem.userSetTitle(it)
+                    }
+                    vb.edittextAmount.easyText2 = accountVMItem.amount
+                    vb.edittextAmount.onDone {
+                        // TODO("There should be a better way to avoid NO_POSITION error.")
+                        if (holder.adapterPosition == RecyclerView.NO_POSITION) return@onDone
+                        val accountVMItem = accountVMItems[holder.adapterPosition]
+                        accountVMItem.userSetAmount(it)
+                    }
+                    vb.btnDeleteAccount.onClick { accountVMItem.userDeleteAccount() }
                 }
-                vb.edittextAmount.easyText2 = accountVMItem.amount
-                vb.edittextAmount.onDone {
-                    // TODO("There should be a better way to avoid NO_POSITION error.")
-                    if (holder.adapterPosition == RecyclerView.NO_POSITION) return@onDone
-                    val accountVMItem = accounts[holder.adapterPosition]
-                    accountVMItem.userSetAmount(it)
-                }
-                vb.btnDeleteAccount.onClick { accountVMItem.userDeleteAccount() }
             }
         }
     }
--- app/src/main/java/com/tminus1010/budgetvalue/all_features/ui/importZ/ImportVM.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/all_features/ui/importZ/ImportVM.kt
@@ -2,15 +2,16 @@ package com.tminus1010.budgetvalue.all_features.ui.importZ
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.disposables
-import com.tminus1010.budgetvalue.all_features.ui.all_features.model.AccountsPresentationModel
-import com.tminus1010.budgetvalue.all_features.ui.all_features.model.ButtonVMItem
+import com.tminus1010.budgetvalue.all_features.all_layers.extensions.invoke
 import com.tminus1010.budgetvalue.all_features.data.repo.AccountsRepo
 import com.tminus1010.budgetvalue.all_features.domain.accounts.Account
-import com.tminus1010.budgetvalue.all_features.all_layers.extensions.invoke
+import com.tminus1010.budgetvalue.all_features.ui.all_features.model.AccountsPresentationModel
+import com.tminus1010.budgetvalue.all_features.ui.all_features.model.ButtonVMItem
 import com.tminus1010.tmcommonkotlin.rx.nonLazy
 import com.tminus1010.tmcommonkotlin.rx.replayNonError
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.subjects.PublishSubject
+import kotlinx.coroutines.flow.flowOf
 import java.math.BigDecimal
 import javax.inject.Inject
 
@@ -28,14 +29,16 @@ class ImportVM @Inject constructor(
             .replayNonError(1)
             .nonLazy(disposables)
     val buttons =
-        listOfNotNull(
-            ButtonVMItem(
-                title = "Import",
-                onClick = navToSelectFile::invoke
-            ),
-            ButtonVMItem(
-                title = "Add Account",
-                onClick = { accountsRepo.add(Account("", BigDecimal.ZERO)).subscribe() }
-            ),
+        flowOf(
+            listOfNotNull(
+                ButtonVMItem(
+                    title = "Import",
+                    onClick = navToSelectFile::invoke
+                ),
+                ButtonVMItem(
+                    title = "Add Account",
+                    onClick = { accountsRepo.add(Account("", BigDecimal.ZERO)).subscribe() }
+                ),
+            )
         )
 }
