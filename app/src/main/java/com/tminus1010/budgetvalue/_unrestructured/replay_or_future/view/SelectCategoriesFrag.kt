5176c71db8146db4cb6e1814513972a5ac9d14cd
Troy <troyswanson1010@gmail.com>
2022-03-24 00:47:31 -0500

    cleanup, formatting

--- app/src/main/java/com/tminus1010/budgetvalue/_unrestructured/replay_or_future/view/SelectCategoriesFrag.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/_unrestructured/replay_or_future/view/SelectCategoriesFrag.kt
@@ -10,12 +10,12 @@ import androidx.navigation.NavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.thekhaeng.recyclerviewmargin.LayoutMarginDecoration
 import com.tminus1010.budgetvalue.R
+import com.tminus1010.budgetvalue._unrestructured.replay_or_future.presentation.SelectCategoriesVM
+import com.tminus1010.budgetvalue.databinding.FragSelectCategoriesBinding
+import com.tminus1010.budgetvalue.databinding.ItemCategoryBtnBinding
 import com.tminus1010.budgetvalue.framework.view.GenViewHolder2
 import com.tminus1010.budgetvalue.framework.view.LifecycleRVAdapter2
 import com.tminus1010.budgetvalue.framework.view.viewBinding
-import com.tminus1010.budgetvalue.databinding.FragSelectCategoriesBinding
-import com.tminus1010.budgetvalue.databinding.ItemCategoryBtnBinding
-import com.tminus1010.budgetvalue._unrestructured.replay_or_future.presentation.SelectCategoriesVM
 import com.tminus1010.tmcommonkotlin.coroutines.extensions.observe
 import com.tminus1010.tmcommonkotlin.misc.extensions.bind
 import com.tminus1010.tmcommonkotlin.view.extensions.nav
@@ -31,15 +31,14 @@ class SelectCategoriesFrag : Fragment(R.layout.frag_select_categories) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // # Setup
-        val spanSize = if (requireContext().resources.configuration.fontScale <= 1.0) 3 else 2
-        vb.recyclerviewCategories.addItemDecoration(LayoutMarginDecoration(spanSize, 8.toPX(requireContext())))
-        vb.recyclerviewCategories.layoutManager =
-            GridLayoutManager(requireActivity(), spanSize, GridLayoutManager.VERTICAL, false)
         // # Events
         selectCategoriesVM.navUp.observe(viewLifecycleOwner) { nav.navigateUp() }
         // # State
         vb.buttonsview.bind(selectCategoriesVM.buttons) { buttons = it }
+        val spanSize = if (requireContext().resources.configuration.fontScale <= 1.0) 3 else 2
+        vb.recyclerviewCategories.addItemDecoration(LayoutMarginDecoration(spanSize, 8.toPX(requireContext())))
+        vb.recyclerviewCategories.layoutManager =
+            GridLayoutManager(requireActivity(), spanSize, GridLayoutManager.VERTICAL, false)
         vb.recyclerviewCategories.bind(selectCategoriesVM.categoryButtonVMItems.map { it.map { it.toViewItemRecipe(requireContext()) } }) { viewItemRecipes ->
             adapter = object : LifecycleRVAdapter2<GenViewHolder2<ItemCategoryBtnBinding>>() {
                 override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
