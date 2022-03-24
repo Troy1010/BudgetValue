80f1c3b0e5635f31777c9c721a2b2902c4a4a9eb
Troy <troyswanson1010@gmail.com>
2022-03-24 01:04:22 -0500

    MockImportTransactionActivity`using buttonsview

--- app/src/main/java/com/tminus1010/budgetvalue/MockImportSelectionActivity.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/MockImportSelectionActivity.kt
@@ -2,17 +2,12 @@ package com.tminus1010.budgetvalue
 
 import android.content.res.AssetManager
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.tminus1010.budgetvalue.framework.view.Toaster
-import com.tminus1010.budgetvalue.framework.view.GenViewHolder2
-import com.tminus1010.budgetvalue.databinding.ActivityMockImportSelectionBinding
-import com.tminus1010.budgetvalue.databinding.ItemButtonBinding
 import com.tminus1010.budgetvalue.data.service.ImportTransactions
+import com.tminus1010.budgetvalue.databinding.ActivityMockImportSelectionBinding
+import com.tminus1010.budgetvalue.ui.all_features.model.ButtonVMItem
+import com.tminus1010.tmcommonkotlin.view.extensions.easyToast
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -25,37 +20,24 @@ class MockImportSelectionActivity : AppCompatActivity() {
     lateinit var assets2: AssetManager
 
     @Inject
-    lateinit var toaster: Toaster
-
-    @Inject
     lateinit var importTransactions: ImportTransactions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(vb.root)
-        // # RecyclerView
-        val transactionPathNames =
-            assets2.list("transactions")
+        vb.buttonsview.buttons =
+            (assets2.list("transactions")
                 ?.map { "transactions/$it" }
-                ?: emptyList()
-        vb.recyclerviewSelectMockImport.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true)
-        vb.recyclerviewSelectMockImport.adapter =
-            object : RecyclerView.Adapter<GenViewHolder2<ItemButtonBinding>>() {
-                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-                    ItemButtonBinding.inflate(LayoutInflater.from(this@MockImportSelectionActivity), parent, false)
-                        .let { GenViewHolder2(it) }
-
-                override fun onBindViewHolder(holder: GenViewHolder2<ItemButtonBinding>, position: Int) {
-                    holder.vb.btnItem.text = "Import Transactions ${holder.adapterPosition}"
-                    holder.vb.btnItem.setOnClickListener {
-                        importTransactions(assets2.open(transactionPathNames[holder.adapterPosition]).buffered()).subscribe()
-                        toaster.toast(R.string.import_successful)
-                        finish()
-                    }
+                ?: emptyList())
+                .withIndex().map { (i, s) ->
+                    ButtonVMItem(
+                        title = "Import Transaction $i",
+                        onClick = {
+                            importTransactions(assets2.open(s).buffered()).subscribe()
+                            application.easyToast(getString(R.string.import_successful))
+                            finish()
+                        }
+                    )
                 }
-
-                override fun getItemCount() = transactionPathNames.size
-            }
     }
 }
