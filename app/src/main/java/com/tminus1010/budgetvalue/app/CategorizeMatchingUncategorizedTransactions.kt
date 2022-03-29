3f3d927a916f6ac174905e5c1435ff7b384096a4
Troy <troyswanson1010@gmail.com>
2022-03-28 21:34:53 -0500

    bugfix redo

--- app/src/main/java/com/tminus1010/budgetvalue/app/CategorizeMatchingUncategorizedTransactions.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/app/CategorizeMatchingUncategorizedTransactions.kt
@@ -1,17 +1,15 @@
 package com.tminus1010.budgetvalue.app
 
 import com.tminus1010.budgetvalue.domain.Transaction
-import com.tminus1010.budgetvalue.data.TransactionsRepo
 import javax.inject.Inject
 
 class CategorizeMatchingUncategorizedTransactions @Inject constructor(
     private val transactionsInteractor: TransactionsInteractor,
-    private val transactionsRepo: TransactionsRepo,
 ) {
     suspend operator fun invoke(isMatch: (Transaction) -> Boolean, categorize: (Transaction) -> Transaction): Int {
         return transactionsInteractor.uncategorizedSpends.value
             .filter(isMatch)
-            .onEach { transactionsRepo.update2(categorize(it)) }
+            .onEach { transactionsInteractor.push(categorize(it)) }
             .fold(0) { acc, v -> acc + 1 }
     }
 }
