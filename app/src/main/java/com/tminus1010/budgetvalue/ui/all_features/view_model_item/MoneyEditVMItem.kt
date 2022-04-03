df8c916af386ce55b5189717e8b5c838b6ca586a
Troy <troyswanson1010@gmail.com>
2022-04-02 23:27:25 -0500

    MoneyEditText`ignore exact matches

--- app/src/main/java/com/tminus1010/budgetvalue/ui/all_features/view_model_item/MoneyEditVMItem.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/ui/all_features/view_model_item/MoneyEditVMItem.kt
@@ -1,13 +1,13 @@
 package com.tminus1010.budgetvalue.ui.all_features.view_model_item
 
 import android.content.Context
-import com.tminus1010.budgetvalue.all_layers.extensions.easyText2
+import com.tminus1010.budgetvalue.all_layers.extensions.easyText3
+import com.tminus1010.budgetvalue.databinding.ItemMoneyEditTextBinding
 import com.tminus1010.budgetvalue.framework.android.onDone
+import com.tminus1010.tmcommonkotlin.misc.extensions.bind
+import com.tminus1010.tmcommonkotlin.misc.tmTableView.IHasToViewItemRecipe
 import com.tminus1010.tmcommonkotlin.misc.tmTableView.IViewItemRecipe3
 import com.tminus1010.tmcommonkotlin.misc.tmTableView.ViewItemRecipe3
-import com.tminus1010.tmcommonkotlin.misc.tmTableView.IHasToViewItemRecipe
-import com.tminus1010.budgetvalue.databinding.ItemMoneyEditTextBinding
-import com.tminus1010.tmcommonkotlin.misc.extensions.bind
 import kotlinx.coroutines.flow.Flow
 
 class MoneyEditVMItem(
@@ -17,8 +17,8 @@ class MoneyEditVMItem(
 ) : IHasToViewItemRecipe {
     override fun toViewItemRecipe(context: Context): IViewItemRecipe3 {
         return ViewItemRecipe3(context, ItemMoneyEditTextBinding::inflate) { vb ->
-            if (text1 != null) vb.moneyedittext.easyText2 = text1
-            if (text2 != null) vb.moneyedittext.bind(text2) { easyText2 = it }
+            if (text1 != null) vb.moneyedittext.easyText3 = text1
+            if (text2 != null) vb.moneyedittext.bind(text2) { if (text.toString() != it) easyText3 = it }
             vb.moneyedittext.onDone(onDone)
         }
     }
