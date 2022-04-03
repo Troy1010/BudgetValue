3e88c9aa876157ca7325c00537ea676fbebea019
Troy <troyswanson1010@gmail.com>
2022-04-03 00:49:55 -0500

    TransactionsAggregate`sort by description, use sorting in spends

--- app/src/main/java/com/tminus1010/budgetvalue/domain/TransactionsAggregate.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/domain/TransactionsAggregate.kt
@@ -3,8 +3,8 @@ package com.tminus1010.budgetvalue.domain
 class TransactionsAggregate(
     transactions: List<Transaction>,
 ) {
-    val transactions = transactions.sortedByDescending(Transaction::date)
-    val spends by lazy { transactions.filter(Transaction::isSpend) }
+    val transactions = transactions.sortedBy { it.description }.sortedByDescending(Transaction::date)
+    val spends by lazy { this.transactions.filter(Transaction::isSpend) }
     val mostRecentSpend by lazy { spends.firstOrNull() }
     val oldestSpend by lazy { spends.lastOrNull() }
     val mostRecentUncategorizedSpend by lazy { spends.firstOrNull(Transaction::isUncategorized) }
