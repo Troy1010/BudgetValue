303386dc2aaa56911a6f15d47e0d6a68736ff9b8
Troy <troyswanson1010@gmail.com>
2022-04-07 02:02:02 -0500

    bugfix for latest gradle upgrades

--- app/src/main/java/com/tminus1010/budgetvalue/all_layers/BaseApp.kt
+++ app/src/main/java/com/tminus1010/budgetvalue/all_layers/BaseApp.kt
@@ -1,8 +1,10 @@
 package com.tminus1010.budgetvalue.all_layers
 
 import android.app.Application
+import androidx.annotation.Keep
 import io.reactivex.rxjava3.plugins.RxJavaPlugins
 
+@Keep
 open class BaseApp : Application() {
     override fun onCreate() {
         logz("!*!*! START")
