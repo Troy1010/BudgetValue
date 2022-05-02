90f09e9d82c594344f495d463b7e93efb8dc6c62
Troy <troyswanson1010@gmail.com>
2022-05-02 03:31:35 -0500

    match entire line

--- app/src/main/java/com/tminus1010/buva/ui/receipt_categorization_imagetotext/ReceiptCategorizationImageToTextVM.kt
+++ app/src/main/java/com/tminus1010/buva/ui/receipt_categorization_imagetotext/ReceiptCategorizationImageToTextVM.kt
@@ -1,10 +1,8 @@
 package com.tminus1010.buva.ui.receipt_categorization_imagetotext
 
-import android.graphics.Color
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.ClickableSpan
-import android.text.style.ForegroundColorSpan
 import android.view.View
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
@@ -63,25 +61,16 @@ class ReceiptCategorizationImageToTextVM @Inject constructor(
             ?.let { Regex("""[0-9]+\s*\.\s*[0-9]\s*[0-9]""").replace(it) { it.value.filter { !it.isWhitespace() } } }
             ?.replace("\n\n", "\n")
             .let { SpannableString(it) }
-            .apply {
-                /**
-                 * Only match last number of: CHZ IT HOT 12.42 13.99
-                 */
-                Regex("""(.+?)\s?([0-9]+\.[0-9]{2})(?!.*[0-9]+\.[0-9]{2})""").findAll(this).forEach {
-                    setSpan(
+            .also { spannableString ->
+                Regex("""(.*)($|\n)""").findAll(spannableString).forEach {
+                    spannableString.setSpan(
                         object : ClickableSpan() {
                             override fun onClick(v: View) {
-                                userAddLine("${it.groupValues[1]} ${it.groupValues[2]}")
+                                userAddLine(it.groupValues[1])
                             }
                         },
                         it.groups[1]!!.range.first,
                         it.groups[1]!!.range.last + 1,
-                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
-                    )
-                    setSpan(
-                        ForegroundColorSpan(Color.rgb(192, 0, 0)),
-                        it.groups[2]!!.range.first,
-                        it.groups[2]!!.range.last + 1,
                         Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
                     )
                 }
