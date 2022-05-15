8a9db044d2f9bbbf102c18b059f6242251d2c737
Troy <troyswanson1010@gmail.com>
2022-05-14 23:49:03 -0500

    Errors iteration

--- app/src/main/java/com/tminus1010/buva/ui/errors/Errors.kt
+++ app/src/main/java/com/tminus1010/buva/ui/errors/Errors.kt
@@ -1,18 +1,20 @@
 package com.tminus1010.buva.ui.errors
 
-import com.tminus1010.buva.all_layers.extensions.onNext
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.plus
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class Errors private constructor(private val errors: MutableSharedFlow<Throwable>) : MutableSharedFlow<Throwable> by errors {
+class Errors private constructor(
+    private val errorSubject: MutableSharedFlow<Throwable>,
+    val coroutineExceptionHandler: CoroutineExceptionHandler = CoroutineExceptionHandler { _, e -> runBlocking { errorSubject.emit(e) } },
+) : MutableSharedFlow<Throwable> by errorSubject, CoroutineExceptionHandler by coroutineExceptionHandler {
     @Inject
     constructor() : this(MutableSharedFlow<Throwable>())
 
-    val coroutineExceptionHandler = CoroutineExceptionHandler { _, e -> errors.onNext(e) }
     val globalScope = GlobalScope + coroutineExceptionHandler
 }
