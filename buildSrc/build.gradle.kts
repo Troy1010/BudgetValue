27b5835f2c068846ab0bcb0aca509cb3c4d06896
Troy <troyswanson1010@gmail.com>
2022-05-15 12:58:05 -0500

    gradle migration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.1.3")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20") // migration to 1.4.31 gave warning: Unsupported Kotlin Version
+    implementation("com.android.tools.build:gradle:7.2.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
     implementation(gradleApi())
     implementation(localGroovy())
     //noinspection GradleDynamicVersion
