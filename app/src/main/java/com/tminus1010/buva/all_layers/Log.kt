8cd0ee2fa2d1065ec72ab1d24c9743d809f0d92c
Troy <troyswanson1010@gmail.com>
2022-10-16 14:51:08 -0500

    logc

---
+++ app/src/main/java/com/tminus1010/buva/all_layers/Log.kt
@@ -0,0 +1,21 @@
+package com.tminus1010.buva.all_layers
+
+import android.util.Log
+import com.tminus1010.tmcommonkotlin.core.isNonInstrumentationTest
+
+
+inline fun <reified T> T.logc(prefix: Any? = null, lambda: (x: T) -> Any?): T {
+    return this.apply {
+        val prefixLogStr = prefix?.let { "$prefix`" } ?: ""
+        if (isNonInstrumentationTest)
+            when (this) {
+                is Throwable -> println("TM`Error:${this.message}")
+                else -> println("TM`${prefixLogStr}${lambda(this)}")
+            }
+        else
+            when (this) {
+                is Throwable -> Log.e("TMLog", "TM`${prefixLogStr}Error:", this)
+                else -> Log.d("TMLog", "TM`${prefixLogStr}${lambda(this)}")
+            }
+    }
+}
