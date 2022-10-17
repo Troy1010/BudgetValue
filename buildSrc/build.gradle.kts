6799055dcc53d65fc01dc795b162fcbd336595ab
Troy <troyswanson1010@gmail.com>
2022-10-17 17:39:55 -0500

    auto gradle migration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.2.0")
+    implementation("com.android.tools.build:gradle:7.3.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
     implementation(gradleApi())
     implementation(localGroovy())
