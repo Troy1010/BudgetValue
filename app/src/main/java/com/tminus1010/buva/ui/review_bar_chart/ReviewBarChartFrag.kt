08faedf0f96252bba41873227d9e2321a357995e
Troy <troyswanson1010@gmail.com>
2023-03-04 04:39:01 -0600

    barData from VM

--- app/src/main/java/com/tminus1010/buva/ui/review_bar_chart/ReviewBarChartFrag.kt
+++ app/src/main/java/com/tminus1010/buva/ui/review_bar_chart/ReviewBarChartFrag.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
 import com.tminus1010.buva.R
 import com.tminus1010.buva.databinding.FragReviewBarChartBinding
+import com.tminus1010.tmcommonkotlin.customviews.extensions.bind
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,6 +17,6 @@ class ReviewBarChartFrag : Fragment(R.layout.frag_review_bar_chart) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         vb = FragReviewBarChartBinding.bind(view)
-        vb.barChart1.data = BarData(BarDataSet(listOf(BarEntry(1.0f, 1.0f), BarEntry(2.0f, 2.0f), BarEntry(3.0f, 3.0f)), "some data"))
+        vb.barChart1.bind(reviewPieChartVM.barData) { data = it }
     }
 }
--- app/src/main/java/com/tminus1010/buva/ui/review_bar_chart/ReviewBarChartVM.kt
+++ app/src/main/java/com/tminus1010/buva/ui/review_bar_chart/ReviewBarChartVM.kt
@@ -1,11 +1,21 @@
 package com.tminus1010.buva.ui.review_bar_chart
 
 import androidx.lifecycle.ViewModel
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.tminus1010.buva.app.TransactionsInteractor
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA")
 @HiltViewModel
 class ReviewBarChartVM @Inject constructor(
+    transactionsInteractor: TransactionsInteractor,
 ) : ViewModel() {
+    val barData =
+        flowOf(
+            BarData(BarDataSet(listOf(BarEntry(1.0f, 1.0f), BarEntry(2.0f, 2.0f), BarEntry(3.0f, 3.0f)), "some data"))
+        )
 }
