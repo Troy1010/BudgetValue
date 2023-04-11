b3fcca89c5e873655d8e4be7e36c07ad5da5324b
Troy <troyswanson1010@gmail.com>
2023-04-11 02:30:36 -0500

    refactor

--- app/src/main/java/com/tminus1010/buva/app/ActiveReconciliationInteractor.kt
+++ app/src/main/java/com/tminus1010/buva/app/ActiveReconciliationInteractor.kt
@@ -1,10 +1,12 @@
 package com.tminus1010.buva.app
 
+import com.tminus1010.buva.all_layers.extensions.easyEquals
 import com.tminus1010.buva.data.AccountsRepo
 import com.tminus1010.buva.data.ActivePlanRepo
 import com.tminus1010.buva.data.ActiveReconciliationRepo
 import com.tminus1010.buva.data.ReconciliationsRepo
 import com.tminus1010.buva.domain.*
+import com.tminus1010.tmcommonkotlin.misc.extensions.sum
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -80,6 +82,47 @@ class ActiveReconciliationInteractor @Inject constructor(
                         )
                     }
             }
+        }
+            .shareIn(GlobalScope, SharingStarted.Eagerly, 1)
+
+    val budgeted =
+        combine(activeReconciliationCAsAndTotal, reconciliationsToDoInteractor.currentReconciliationToDo, reconciliationsRepo.reconciliations, transactionsInteractor.transactionBlocks)
+        { activeReconciliation, currentReconciliationToDo, reconciliations, transactionBlocks ->
+            val relevantReconciliations =
+                when (currentReconciliationToDo) {
+                    is ReconciliationToDo.PlanZ ->
+                        reconciliations.filter { it.date < currentReconciliationToDo.transactionBlock.datePeriod!!.endDate }
+                    is ReconciliationToDo.Accounts ->
+                        reconciliations.filter { it.date < currentReconciliationToDo.date }
+                    else -> reconciliations
+                }
+            val relevantTransactionBlocks =
+                when (currentReconciliationToDo) {
+                    is ReconciliationToDo.PlanZ ->
+                        transactionBlocks.filter { it.datePeriod!!.startDate < currentReconciliationToDo.transactionBlock.datePeriod!!.endDate }
+                    is ReconciliationToDo.Accounts ->
+                        transactionBlocks.filter { it.datePeriod!!.startDate < currentReconciliationToDo.date }
+                    else -> transactionBlocks
+                }
+            CategoryAmountsAndTotalWithValidation(
+                categoryAmounts = CategoryAmounts.addTogether(
+                    activeReconciliation.categoryAmounts,
+                    *relevantReconciliations.map { it.categoryAmounts }.toTypedArray(),
+                    *relevantTransactionBlocks.map { it.categoryAmounts }.toTypedArray(),
+                ),
+                total = activeReconciliation.total
+                    .plus(relevantReconciliations.map { it.total }.sum())
+                    .plus(relevantTransactionBlocks.map { it.total }.sum()),
+                caValidation = {
+                    when (currentReconciliationToDo) {
+                        is ReconciliationToDo.Accounts,
+                        is ReconciliationToDo.Anytime,
+                        -> (it ?: BigDecimal.ZERO) >= BigDecimal.ZERO
+                        else -> true
+                    }
+                },
+                defaultAmountValidation = { (it ?: BigDecimal.ZERO).easyEquals(BigDecimal.ZERO) }
+            )
         }
             .shareIn(GlobalScope, SharingStarted.Eagerly, 1)
 
--- app/src/main/java/com/tminus1010/buva/ui/budget/reconciliation/accounts_reconciliation/AccountsReconciliationVM.kt
+++ app/src/main/java/com/tminus1010/buva/ui/budget/reconciliation/accounts_reconciliation/AccountsReconciliationVM.kt
@@ -3,7 +3,6 @@ package com.tminus1010.buva.ui.budget.reconciliation.accounts_reconciliation
 import androidx.lifecycle.ViewModel
 import com.tminus1010.buva.all_layers.extensions.toMoneyBigDecimal
 import com.tminus1010.buva.app.ActiveReconciliationInteractor
-import com.tminus1010.buva.app.BudgetedForActiveReconciliationInteractor
 import com.tminus1010.buva.app.UserCategories
 import com.tminus1010.buva.data.ActiveReconciliationRepo
 import com.tminus1010.buva.domain.Category
@@ -18,7 +17,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AccountsReconciliationVM @Inject constructor(
-    budgetedForActiveReconciliationInteractor: BudgetedForActiveReconciliationInteractor,
     private val activeReconciliationInteractor: ActiveReconciliationInteractor,
     private val activeReconciliationRepo: ActiveReconciliationRepo,
     userCategories: UserCategories,
@@ -42,30 +40,30 @@ class AccountsReconciliationVM @Inject constructor(
 
     // # State
     val reconciliationTableView =
-        combine(userCategories.flow, activeReconciliationInteractor.activeReconciliationCAsAndTotal, budgetedForActiveReconciliationInteractor.categoryAmountsAndTotal)
-        { categories, activeReconciliation, budgetedWithActiveReconciliation ->
+        combine(userCategories.flow, activeReconciliationInteractor.activeReconciliationCAsAndTotal, activeReconciliationInteractor.budgeted)
+        { categories, activeReconciliation, budgeted ->
             TableViewVMItem(
                 recipeGrid = listOf(
                     listOf(
                         HeaderPresentationModel("Categories"),
                         HeaderPresentationModel("Reconcile"),
-                        BudgetHeaderPresentationModel("Budgeted", budgetedWithActiveReconciliation.total.toString()),
+                        BudgetHeaderPresentationModel("Budgeted", budgeted.total.toString()),
                     ),
                     listOf(
                         TextVMItem("Total"),
                         TextVMItem(activeReconciliation.total.toString()),
-                        AmountPresentationModel(budgetedWithActiveReconciliation.total),
+                        AmountPresentationModel(budgeted.total),
                     ),
                     listOf(
                         TextVMItem("Default"),
                         TextVMItem(activeReconciliation.defaultAmount.toString()),
-                        AmountPresentationModel(budgetedWithActiveReconciliation.defaultAmount, checkIfValid = { budgetedWithActiveReconciliation.isDefaultAmountValid }),
+                        AmountPresentationModel(budgeted.defaultAmount, checkIfValid = { budgeted.isDefaultAmountValid }),
                     ),
                     *categories.map { category ->
                         listOf(
                             TextVMItem(category.name),
                             CategoryAmountPresentationModel(category, activeReconciliation.categoryAmounts[category], ::userSetCategoryAmount, menuVMItems = MenuVMItems(MenuVMItem("Fill into category", onClick = { userDumpIntoCategory(category) }))),
-                            AmountPresentationModel(budgetedWithActiveReconciliation.categoryAmounts[category], checkIfValid = { budgetedWithActiveReconciliation.isValid(category) }),
+                            AmountPresentationModel(budgeted.categoryAmounts[category], checkIfValid = { budgeted.isValid(category) }),
                         )
                     }.toTypedArray(),
                 ),
