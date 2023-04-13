3afc85a6cb7e9125f751cb08b85eaf2edc7c7940
Troy <troyswanson1010@gmail.com>
2023-04-13 01:31:57 -0500

    SelectedBudgetHostPage

--- app/src/main/java/com/tminus1010/buva/ui/budget/BudgetHostVM.kt
+++ app/src/main/java/com/tminus1010/buva/ui/budget/BudgetHostVM.kt
@@ -4,29 +4,31 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.tminus1010.buva.R
 import com.tminus1010.buva.all_layers.KEY1
-import com.tminus1010.buva.all_layers.extensions.onNext
+import com.tminus1010.buva.data.SelectedBudgetHostPage
 import com.tminus1010.buva.ui.budget.budget.BudgetFrag
 import com.tminus1010.buva.ui.budget.plan.PlanFrag
 import com.tminus1010.buva.ui.budget.reconciliation.ReconciliationHostFrag
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class BudgetHostVM @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
+    private val selectedBudgetHostPage: SelectedBudgetHostPage,
 ) : ViewModel() {
     // # User Intent
     fun userSelectMenuItem(id: Int) {
-        selectedItemId.onNext(id)
+        selectedBudgetHostPage.set(id)
     }
 
     // # Private
-    private val subNavId = savedStateHandle.get<Int>(KEY1) ?: R.id.budgetFrag
+    init {
+        savedStateHandle.get<Int>(KEY1)?.also { selectedBudgetHostPage.set(it) }
+    }
 
     // # State
-    val selectedItemId = MutableStateFlow(value = subNavId)
+    val selectedItemId = selectedBudgetHostPage.flow
     val fragFactory =
         selectedItemId.map {
             when (it) {
