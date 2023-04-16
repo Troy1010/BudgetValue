6b0ac38f8d1852da7c82e83ec1b8f74353db9513
Troy <troyswanson1010@gmail.com>
2023-04-16 02:15:44 -0500

    bugfix

--- app/src/main/java/com/tminus1010/buva/app/IsReadyToReconcile.kt
+++ app/src/main/java/com/tminus1010/buva/app/IsReadyToReconcile.kt
@@ -1,20 +1,19 @@
 package com.tminus1010.buva.app
 
 import com.tminus1010.buva.all_layers.extensions.isZero
-import com.tminus1010.buva.data.ActivePlanRepo
 import com.tminus1010.buva.domain.ReconciliationStrategyGroup
 import com.tminus1010.buva.domain.ResetStrategy
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class IsReadyToReconcile @Inject constructor(
-    private val activePlanRepo: ActivePlanRepo,
+    private val userCategories: UserCategories,
 ) {
     class PlanIsInvalidException : Exception()
 
     suspend fun check() {
         if (
-            activePlanRepo.activePlan.first().categoryAmounts.keys.any { category ->
+            userCategories.flow.first().any { category ->
                 category.reconciliationStrategyGroup is ReconciliationStrategyGroup.Reservoir
                         && (category.reconciliationStrategyGroup.resetStrategy as? ResetStrategy.Basic)?.let { it.budgetedMax?.isZero ?: false } ?: false
             }
