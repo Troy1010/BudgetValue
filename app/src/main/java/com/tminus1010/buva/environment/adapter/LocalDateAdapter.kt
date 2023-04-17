c0ef2d5b0c706a5264b736cdc0c795f04a85442a
Troy <troyswanson1010@gmail.com>
2023-04-17 00:56:56 -0500

    LocalDateAdapter simplification

--- app/src/main/java/com/tminus1010/buva/environment/adapter/LocalDateAdapter.kt
+++ app/src/main/java/com/tminus1010/buva/environment/adapter/LocalDateAdapter.kt
@@ -9,13 +9,10 @@ object LocalDateAdapter {
     private val dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
 
     @ToJson
-    fun toJson(x: LocalDate?): String =
-        x?.format(dateFormatter).toString()
+    fun toJson(x: LocalDate): String =
+        x.format(dateFormatter)
 
     @FromJson
-    fun fromJson(s: String): LocalDate? =
-        when (s) {
-            "null" -> null
-            else -> s.let { LocalDate.parse(s, dateFormatter) }
-        }
+    fun fromJson(s: String): LocalDate =
+        LocalDate.parse(s, dateFormatter)
 }
