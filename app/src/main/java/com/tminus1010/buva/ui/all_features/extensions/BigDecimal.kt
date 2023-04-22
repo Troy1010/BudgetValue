c59accec488830b37fcc580c102d74b32adc1e3e
Troy <troyswanson1010@gmail.com>
2023-04-21 23:47:44 -0500

    BigDecimal.toDisplayStr for null

--- app/src/main/java/com/tminus1010/buva/ui/all_features/extensions/BigDecimal.kt
+++ app/src/main/java/com/tminus1010/buva/ui/all_features/extensions/BigDecimal.kt
@@ -3,8 +3,9 @@ package com.tminus1010.buva.ui.all_features.extensions
 import com.tminus1010.buva.all_layers.extensions.easyEquals
 import java.math.BigDecimal
 
-val BigDecimal.displayStr
-    get() = when {
+fun BigDecimal?.toDisplayStr() =
+    when {
+        this == null -> null
         this.setScale(0).easyEquals(this) -> this.setScale(0).toPlainString()
         else -> this.toPlainString()
     }
--- app/src/main/java/com/tminus1010/buva/ui/all_features/view_model_item/AmountPresentationModel.kt
+++ app/src/main/java/com/tminus1010/buva/ui/all_features/view_model_item/AmountPresentationModel.kt
@@ -7,6 +7,7 @@ import com.tminus1010.buva.databinding.ItemEditTextBinding
 import com.tminus1010.buva.databinding.ItemTextViewBinding
 import com.tminus1010.buva.domain.ValidationResult
 import com.tminus1010.buva.ui.all_features.extensions.toColor
+import com.tminus1010.buva.ui.all_features.extensions.toDisplayStr
 import com.tminus1010.tmcommonkotlin.customviews.IViewItemRecipe3
 import com.tminus1010.tmcommonkotlin.customviews.ViewItemRecipe3
 import com.tminus1010.tmcommonkotlin.customviews.ViewItemRecipeFactory
@@ -18,20 +19,20 @@ class AmountPresentationModel(
     private val onNewAmount: ((BigDecimal?) -> Unit)? = null,
     private val menuVMItems: MenuVMItems? = null,
 ) : ViewItemRecipeFactory {
-    val s get() = bigDecimal?.toString()
+    val s get() = bigDecimal.toDisplayStr()
     val validationResult get() = validation(bigDecimal ?: BigDecimal.ZERO)
     override fun toViewItemRecipe(context: Context): IViewItemRecipe3 {
         return if (onNewAmount != null)
             ViewItemRecipe3(context, ItemEditTextBinding::inflate) { vb ->
                 menuVMItems?.bind(vb.root)
-                vb.edittext.setText(s)
+                vb.edittext.setText(bigDecimal.toDisplayStr())
                 vb.edittext.setTextColor(validationResult.toColor(context))// TODO: It seems like the color will be wrong if onDone changes the value..?
                 vb.edittext.onDone { onNewAmount!!(it.ifEmpty { null }?.toMoneyBigDecimal()) }
             }
         else
             ViewItemRecipe3(context, ItemTextViewBinding::inflate) { vb ->
                 menuVMItems?.bind(vb.root)
-                vb.textview.text = s
+                vb.textview.text = bigDecimal.toDisplayStr()
                 vb.textview.setTextColor(validationResult.toColor(context))// TODO: It seems like the color will be wrong if onDone changes the value..?
             }
     }
