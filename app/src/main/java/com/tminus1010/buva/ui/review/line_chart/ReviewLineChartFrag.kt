bbe3361c0d207f79d862a5320fdc706009342d5d
Troy <troyswanson1010@gmail.com>
2023-04-30 15:27:56 -0500

    ReviewLineChart shows savings in past

--- app/src/main/java/com/tminus1010/buva/ui/review/line_chart/ReviewLineChartFrag.kt
+++ app/src/main/java/com/tminus1010/buva/ui/review/line_chart/ReviewLineChartFrag.kt
@@ -3,6 +3,7 @@ package com.tminus1010.buva.ui.review_spend_bar_chart
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.tminus1010.buva.R
 import com.tminus1010.buva.databinding.FragReviewLineChartBinding
@@ -11,7 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ReviewLineChartFrag : Fragment(R.layout.frag_review_line_chart) {
     lateinit var vb: FragReviewLineChartBinding
-    val reviewLineChartVM by viewModels<ReviewLineChartVM>()
+    val reviewLineChartVM by activityViewModels<ReviewLineChartVM>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/tminus1010/buva/ui/review/line_chart/ReviewLineChartVM.kt
+++ app/src/main/java/com/tminus1010/buva/ui/review/line_chart/ReviewLineChartVM.kt
@@ -2,37 +2,50 @@ package com.tminus1010.buva.ui.review_spend_bar_chart
 
 import android.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tminus1010.buva.app.BudgetedInteractor
 import com.tminus1010.buva.app.TransactionsInteractor
+import com.tminus1010.buva.app.UserCategories
 import com.tminus1010.buva.data.AccountsRepo
 import com.tminus1010.buva.data.ReconciliationsRepo
 import com.tminus1010.buva.domain.GuessPastUtil
-import com.tminus1010.buva.domain.MiscUtil
 import com.tminus1010.buva.ui.all_features.view_model_item.LineChartVMItem
 import com.tminus1010.tmcommonkotlin.core.extensions.toDisplayStr
 import com.tminus1010.tmcommonkotlin.tuple.tuple
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.shareIn
 import javax.inject.Inject
 import kotlin.math.absoluteValue
 
 @HiltViewModel
 class ReviewLineChartVM @Inject constructor(
-    transactionsInteractor: TransactionsInteractor,
-    accountsRepo: AccountsRepo,
-    reconciliationsRepo: ReconciliationsRepo,
+    private val transactionsInteractor: TransactionsInteractor,
+    private val accountsRepo: AccountsRepo,
+    private val reconciliationsRepo: ReconciliationsRepo,
+    private val userCategories: UserCategories,
+    private val budgetedInteractor: BudgetedInteractor,
 ) : ViewModel() {
     private val mapLabelToValues =
-        transactionsInteractor.transactionBlocks.map { transactionBlocks ->
+        combine(userCategories.flow, transactionsInteractor.transactionBlocks)
+        { userCategories, transactionBlocks ->
+            val savingsCategory = userCategories.find { it.name == "Savings" } // TODO: Should be an easier way to get Settings.
             transactionBlocks
                 .sortedBy { it.datePeriod?.startDate }
                 .associate {
-                    Pair(
+                    tuple(
                         it.datePeriod!!.startDate.toDisplayStr(),
-                        listOf(
+                        listOfNotNull(
                             tuple(
                                 Color.CYAN,
-                                GuessPastUtil.guessAccountsTotalInPast(it.datePeriod.endDate, accountsRepo.accountsAggregate.first(), transactionsInteractor.transactionBlocks.first(), reconciliationsRepo.reconciliations.first()).toFloat()
+                                GuessPastUtil.guessAccountsTotalInPast(
+                                    it.datePeriod.endDate,
+                                    accountsRepo.accountsAggregate.first(),
+                                    transactionsInteractor.transactionBlocks.first(),
+                                    reconciliationsRepo.reconciliations.first(),
+                                ).toFloat()
                             ),
                             tuple(
                                 Color.RED,
@@ -42,14 +55,27 @@ class ReviewLineChartVM @Inject constructor(
                                 Color.GREEN,
                                 it.incomeBlock.total.toFloat().absoluteValue
                             ),
+                            if (savingsCategory != null)
+                                tuple(
+                                    Color.MAGENTA,
+                                    GuessPastUtil.budgettedAmountInPast(
+                                        savingsCategory,
+                                        it.datePeriod.endDate,
+                                        budgetedInteractor.budgeted.first().categoryAmounts,
+                                        transactionsInteractor.transactionBlocks.first(),
+                                        reconciliationsRepo.reconciliations.first(),
+                                    ).toFloat()
+                                )
+                            else null,
 //                            tuple(
-//                                Color.MAGENTA,
+//                                Color.YELLOW,
 //                                it.total.toFloat()
 //                            ),
                         ),
                     )
                 }
         }
+            .shareIn(viewModelScope, SharingStarted.Eagerly, 1)
     val lineChartVMItem =
         LineChartVMItem(
             mapLabelToValues = mapLabelToValues,
