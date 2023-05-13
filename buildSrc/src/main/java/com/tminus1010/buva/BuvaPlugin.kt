98239149d26abc9b8850c0f005887470bfaecad7
Troy <troyswanson1010@gmail.com>
2023-05-12 23:46:54 -0500

    rename launch tasks

--- buildSrc/src/main/java/com/tminus1010/buva/BuvaPlugin.kt
+++ buildSrc/src/main/java/com/tminus1010/buva/BuvaPlugin.kt
@@ -16,31 +16,29 @@ open class BuvaPlugin : Plugin<Project> {
         budgetValuePluginSettings = project.extensions.create("budgetValuePluginSettings", Settings::class.java)
         project.afterEvaluate {
             // # Register launchApp
-            tasks.register("launchApp", LaunchApp::class.java, budgetValuePluginSettings.adbAbsolutePath)
+            tasks.register("launch_app", LaunchApp::class.java, budgetValuePluginSettings.adbAbsolutePath)
                 .configure { setMustRunAfter(listOf("installDebug")) }
-            tasks.register("installLaunchApp") {
-                description = "Launches more quickly (if build cache is available), but less reliably"
+            tasks.register("launch_app_speed5") {
                 group = "budgetvalue"
-                dependsOn("installDebug", "launchApp")
+                dependsOn("installDebug", "launch_app")
             }
-            tasks.register("easySlowLaunchApp") {
-                description = "Launches slowly, but reliably"
+            tasks.register("launch_app_speed3") {
                 group = "budgetvalue"
-                dependsOn("clean", "installDebug", "launchApp")
+                dependsOn("clean", "installDebug", "launch_app")
             }
             // # Register launchDevEnvs
             layout.projectDirectory.dir("src/androidTest/java/com/tminus1010/buva/devEnvs").asFileTree.filter { it.name.startsWith("DevEnv") }.map { it.name.dropLast(3) }.forEach {
-                tasks.register("launch$it", LaunchDevEnv::class.java, budgetValuePluginSettings.adbAbsolutePath, it)
+                tasks.register("launch_$it", LaunchDevEnv::class.java, budgetValuePluginSettings.adbAbsolutePath, it)
                     .configure { setMustRunAfter(listOf("installDebug", "installDebugAndroidTest")) }
-                tasks.register("installLaunch$it") {
+                tasks.register("launch_${it}_speed5") {
                     description = "Launches quickly (if build cache is available), but less reliably. When successful, it will throw a timeout failure.. just ignore it."
                     group = "budgetvalue"
-                    dependsOn("installDebug", "installDebugAndroidTest", "launch$it")
+                    dependsOn("installDebug", "installDebugAndroidTest", "launch_$it")
                 }
-                tasks.register("easySlowLaunch$it") {
+                tasks.register("launch_${it}_speed3") {
                     description = "Launches slowly, but reliably. When successful, it will throw a timeout failure.. just ignore it."
                     group = "budgetvalue"
-                    dependsOn("clean", "installDebug", "installDebugAndroidTest", "launch$it")
+                    dependsOn("clean", "installDebug", "installDebugAndroidTest", "launch_$it")
                 }
             }
         }
